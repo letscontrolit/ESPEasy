47379348f2c6bbb374f769a75b043ba53110c8bb
battika <battika@gmail.com>
2016-05-15 13:39:48 +0200

    Fixed documentation error and a typo

--- _C001.ino
+++ _C001.ino
@@ -147,7 +147,7 @@ boolean CPlugin_001(byte function, struct EventStruct *event, String& string)
           addLog(LOG_LEVEL_DEBUG_MORE, log);
           if (line.substring(0, 15) == "HTTP/1.1 200 OK")
           {
-            strcpy_P(log, PSTR("HTTP : Succes!"));
+            strcpy_P(log, PSTR("HTTP : Success"));
             addLog(LOG_LEVEL_DEBUG, log);
             success = true;
           }
