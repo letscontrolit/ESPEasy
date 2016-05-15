55a4c76ef4ea886904f488793ad9afa85fe855e6
battika <battika@gmail.com>
2016-05-15 13:35:16 +0200

    By default FEATURE_ADC_VCC is off

--- ESPEasy.ino
+++ ESPEasy.ino
@@ -109,7 +109,7 @@
 // Enable FEATURE_ADC_VCC to measure supply voltage using the analog pin
 // Please note that the TOUT pin has to be disconnected in this mode
 // Use the "Analog Input" device to read the VCC value
-#define FEATURE_ADC_VCC                  true
+#define FEATURE_ADC_VCC                  false
 
 // ********************************************************************************
 //   DO NOT CHANGE ANYTHING BELOW THIS LINE
