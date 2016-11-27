2bcf328d1bfec661b24307e8ddcd2e9ff7861da6
Aurelien.Hannoteaux <aurelien.hannoteaux@braincube.com>
2016-11-27 19:13:12 +0100

    Add MQ2 gaz sensor plugin

---
+++ _P036_MQ2.ino
@@ -0,0 +1,217 @@
+//#######################################################################################################
+//#################################### Plugin 036: MQ2 sensors ###############################################
+//#######################################################################################################
+
+#define PLUGIN_036
+#define PLUGIN_ID_036         36
+#define PLUGIN_NAME_036       "MQ2 smoke sensor"
+#define PLUGIN_VALUENAME1_036 "LPG"
+#define PLUGIN_VALUENAME2_036 "CO"
+#define PLUGIN_VALUENAME3_036 "SMOKE"
+
+/************************Hardware Related Macros************************************/
+#define         RL_VALUE                     (5)     //define the load resistance on the board, in kilo ohms
+#define         RO_CLEAN_AIR_FACTOR          (9.83)  //RO_CLEAR_AIR_FACTOR=(Sensor resistance in clean air)/RO,
+                                                     //which is derived from the chart in datasheet
+
+/***********************Software Related Macros************************************/
+#define         CALIBARAION_SAMPLE_TIMES     (50)    //define how many samples you are going to take in the calibration phase
+#define         CALIBRATION_SAMPLE_INTERVAL  (500)   //define the time interal(in milisecond) between each samples in the
+                                                     //cablibration phase
+#define         READ_SAMPLE_INTERVAL         (20)    //define how many samples you are going to take in normal operation
+#define         READ_SAMPLE_TIMES            (5)     //define the time interal(in milisecond) between each samples in
+                                                     //normal operation
+
+/**********************Application Related Macros**********************************/
+#define         GAS_LPG                      (0)
+#define         GAS_CO                       (1)
+#define         GAS_SMOKE                    (2)
+
+/*****************************Globals***********************************************/
+float           LPGCurve[3]  =  {2.3,0.21,-0.47};   //two points are taken from the curve.
+                                                    //with these two points, a line is formed which is "approximately equivalent"
+                                                    //to the original curve.
+                                                    //data format:{ x, y, slope}; point1: (lg200, 0.21), point2: (lg10000, -0.59)
+float           COCurve[3]  =  {2.3,0.72,-0.34};    //two points are taken from the curve.
+                                                    //with these two points, a line is formed which is "approximately equivalent"
+                                                    //to the original curve.
+                                                    //data format:{ x, y, slope}; point1: (lg200, 0.72), point2: (lg10000,  0.15)
+float           SmokeCurve[3] ={2.3,0.53,-0.44};    //two points are taken from the curve.
+                                                    //with these two points, a line is formed which is "approximately equivalent"
+                                                    //to the original curve.
+                                                    //data format:{ x, y, slope}; point1: (lg200, 0.53), point2: (lg10000,  -0.22)
+float           Ro           =  10;                 //Ro is initialized to 10 kilo ohms
+
+
+
+boolean Plugin_034(byte function, struct EventStruct *event, String& string)
+{
+  boolean success = false;
+
+  switch (function)
+  {
+
+    case PLUGIN_DEVICE_ADD:
+      {
+        Device[++deviceCount].Number = PLUGIN_ID_036;
+        Device[deviceCount].Type = DEVICE_TYPE_ANALOG;
+        Device[deviceCount].VType = SENSOR_TYPE_TRIPLE;
+        Device[deviceCount].Ports = 0;
+        Device[deviceCount].PullUpOption = false;
+        Device[deviceCount].InverseLogicOption = false;
+        Device[deviceCount].FormulaOption = true;
+        Device[deviceCount].ValueCount = 3;
+        Device[deviceCount].SendDataOption = true;
+        Device[deviceCount].TimerOption = true;
+        Device[deviceCount].GlobalSyncOption = true;
+        break;
+      }
+
+    case PLUGIN_GET_DEVICENAME:
+      {
+        string = F(PLUGIN_NAME_036);
+        break;
+      }
+
+    case PLUGIN_GET_DEVICEVALUENAMES:
+      {
+        strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[0], PSTR(PLUGIN_VALUENAME1_036));
+        strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[1], PSTR(PLUGIN_VALUENAME2_036));
+        strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[2], PSTR(PLUGIN_VALUENAME3_036));
+        break;
+      }
+
+    case PLUGIN_INIT:
+      {
+        String log = F("MQ2 Calibrating...put it in fresh air");
+        addLog(LOG_LEVEL_INFO,log);
+        Ro = MQCalibration();                       //Calibrating the sensor. Please make sure the sensor is in clean air
+                                                          //when you perform the calibration
+        log = F("MQ2 Calibration is done...");
+        addLog(LOG_LEVEL_INFO,log);
+        log = F("Ro=");
+        log += Ro;
+        log += F("kohm");
+        addLog(LOG_LEVEL_INFO,log);
+      }
+
+    case PLUGIN_READ:
+      {
+        //Read LPG value
+        int value = MQGetGasPercentage(MQRead()/Ro,GAS_LPG);
+        UserVar[event->BaseVarIndex] = (float)value;
+        String log = F("MQ2  : LPG value: ");
+        log += value;
+        addLog(LOG_LEVEL_INFO,log);
+        //read CO value
+        value = MQGetGasPercentage(MQRead()/Ro,GAS_CO);
+        UserVar[event->BaseVarIndex +1] = (float)value;
+        log = F("MQ2  : CO value: ");
+        log += value;
+        addLog(LOG_LEVEL_INFO,log);
+        //Read SMOKE
+        value = MQGetGasPercentage(MQRead()/Ro,GAS_SMOKE);
+        UserVar[event->BaseVarIndex +2] = (float)value;
+        log = F("MQ2  : SMOKE value: ");
+        log += value;
+        addLog(LOG_LEVEL_INFO,log);
+        //affect result
+        success = true;
+        break;
+      }
+  }
+  return success;
+}
+
+/****************** MQResistanceCalculation ****************************************
+Input:   raw_adc - raw value read from adc, which represents the voltage
+Output:  the calculated sensor resistance
+Remarks: The sensor and the load resistor forms a voltage divider. Given the voltage
+         across the load resistor and its resistance, the resistance of the sensor
+         could be derived.
+************************************************************************************/
+float MQResistanceCalculation(int raw_adc)
+{
+  return ( ((float)RL_VALUE*(1023-raw_adc)/raw_adc));
+}
+
+/***************************** MQCalibration ****************************************
+Input:   mq_pin - analog channel
+Output:  Ro of the sensor
+Remarks: This function assumes that the sensor is in clean air. It use
+         MQResistanceCalculation to calculates the sensor resistance in clean air
+         and then divides it with RO_CLEAN_AIR_FACTOR. RO_CLEAN_AIR_FACTOR is about
+         10, which differs slightly between different sensors.
+************************************************************************************/
+float MQCalibration()
+{
+  int i;
+  float val=0;
+
+  for (i=0;i<CALIBARAION_SAMPLE_TIMES;i++) {            //take multiple samples
+    val += MQResistanceCalculation(analogRead(A0));
+    delay(CALIBRATION_SAMPLE_INTERVAL);
+  }
+  val = val/CALIBARAION_SAMPLE_TIMES;                   //calculate the average value
+
+  val = val/RO_CLEAN_AIR_FACTOR;                        //divided by RO_CLEAN_AIR_FACTOR yields the Ro
+                                                        //according to the chart in the datasheet
+
+  return val;
+}
+/*****************************  MQRead *********************************************
+Input:   mq_pin - analog channel
+Output:  Rs of the sensor
+Remarks: This function use MQResistanceCalculation to caculate the sensor resistenc (Rs).
+         The Rs changes as the sensor is in the different consentration of the target
+         gas. The sample times and the time interval between samples could be configured
+         by changing the definition of the macros.
+************************************************************************************/
+float MQRead()
+{
+  int i;
+  float rs=0;
+
+  for (i=0;i<READ_SAMPLE_TIMES;i++) {
+    rs += MQResistanceCalculation(analogRead(A0));
+    delay(READ_SAMPLE_INTERVAL);
+  }
+
+  rs = rs/READ_SAMPLE_TIMES;
+
+  return rs;
+}
+
+/*****************************  MQGetGasPercentage **********************************
+Input:   rs_ro_ratio - Rs divided by Ro
+         gas_id      - target gas type
+Output:  ppm of the target gas
+Remarks: This function passes different curves to the MQGetPercentage function which
+         calculates the ppm (parts per million) of the target gas.
+************************************************************************************/
+int MQGetGasPercentage(float rs_ro_ratio, int gas_id)
+{
+  if ( gas_id == GAS_LPG ) {
+     return MQGetPercentage(rs_ro_ratio,LPGCurve);
+  } else if ( gas_id == GAS_CO ) {
+     return MQGetPercentage(rs_ro_ratio,COCurve);
+  } else if ( gas_id == GAS_SMOKE ) {
+     return MQGetPercentage(rs_ro_ratio,SmokeCurve);
+  }
+
+  return 0;
+}
+
+/*****************************  MQGetPercentage **********************************
+Input:   rs_ro_ratio - Rs divided by Ro
+         pcurve      - pointer to the curve of the target gas
+Output:  ppm of the target gas
+Remarks: By using the slope and a point of the line. The x(logarithmic value of ppm)
+         of the line could be derived if y(rs_ro_ratio) is provided. As it is a
+         logarithmic coordinate, power of 10 is used to convert the result to non-logarithmic
+         value.
+************************************************************************************/
+int  MQGetPercentage(float rs_ro_ratio, float *pcurve)
+{
+  return (pow(10,( ((log(rs_ro_ratio)-pcurve[1])/pcurve[2]) + pcurve[0])));
+}
