45ad68cd6100d903d34fdfea08d5bf0e3d2f286c
krikk <krikkit@gmx.net>
2017-03-16 21:58:03 +0100

    motorshield library should be a submodule, enhanced the motorshield plugin... (#162)
    * added missing sensor types: LONG & WIND
    
    * did a bit of code cleanup, like you suggested... removed a lot of code, and works as before! :)
    
    * fixed typo
    
    * added date to the time displays... changed password fields to type=password
    
    * motorshield library should be a submodule, enhanced the motorshield plugin, with better command parsing, but still work to do..
    
    * fully implemented steppers and DCMotors

--- src/_P048_Motorshield_v2.ino
+++ src/_P048_Motorshield_v2.ino
@@ -5,7 +5,7 @@
 // Adafruit Motorshield v2
 // like this one: https://www.adafruit.com/products/1438
 // based on this library: https://github.com/adafruit/Adafruit_Motor_Shield_V2_Library
-// Currently only DC Motors Part is implemented, Steppers and Servos are missing!!!
+// Currently DC Motors and Steppers are implemented, Servos are missing!!!
 
 #ifdef PLUGIN_BUILD_DEV
 
@@ -17,10 +17,9 @@
 #define PLUGIN_VALUENAME1_048 "MotorShield v2"
 
 uint8_t Plugin_048_MotorShield_address = 0x60;
-byte Plugin_048_MotorType = 0;
-byte Plugin_048_MotorNumber = 1;
-byte Plugin_048_MotorSpeed = 255;
 
+int Plugin_048_MotorStepsPerRevolution = 200;
+int Plugin_048_StepperSpeed = 10;
 
 boolean Plugin_048(byte function, struct EventStruct *event, String& string) {
 	boolean success = false;
@@ -31,181 +30,266 @@ boolean Plugin_048(byte function, struct EventStruct *event, String& string) {
 
 	switch (function) {
 
-	case PLUGIN_DEVICE_ADD: {
-		Device[++deviceCount].Number = PLUGIN_ID_048;
-		Device[deviceCount].Type = DEVICE_TYPE_I2C;
-		Device[deviceCount].VType = SENSOR_TYPE_SINGLE;
-		Device[deviceCount].Ports = 0;
-		Device[deviceCount].PullUpOption = false;
-		Device[deviceCount].InverseLogicOption = false;
-		Device[deviceCount].FormulaOption = false;
-		Device[deviceCount].ValueCount = 0;
-		Device[deviceCount].SendDataOption = false;
-		Device[deviceCount].TimerOption = true;
-		break;
-	}
-
-	case PLUGIN_GET_DEVICENAME: {
-		string = F(PLUGIN_NAME_048);
-		break;
-	}
-
-	case PLUGIN_GET_DEVICEVALUENAMES: {
-		strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[0],
-				PSTR(PLUGIN_VALUENAME1_048));
-		break;
-	}
-
-	case PLUGIN_WEBFORM_LOAD: {
-
-		string +=
-				F("<TR><TD>I2C Address (Hex): <TD><input type='text' title='Set i2c Address of sensor' name='");
-		string += F("plugin_048_adr' value='0x");
-		string += String(Settings.TaskDevicePluginConfig[event->TaskIndex][0],HEX);
-		string += F("'>");
-
-		byte choice2 = Settings.TaskDevicePluginConfig[event->TaskIndex][1];
-		String options2[3];
-		options2[0] = F("DCMotor");
-		options2[1] = F("Stepper [NOT IMPLEMETED]");
-		options2[2] = F("Servo [NOT IMPLEMETED]");
-		int optionValues2[3];
-		optionValues2[0] = 0;
-		optionValues2[1] = 1;
-		optionValues2[2] = 2;
-		string += F("<TR><TD>Motor Type:<TD><select name='plugin_048_motortype'>");
-		for (byte x = 0; x < 3; x++) {
-			string += F("<option value='");
-			string += optionValues2[x];
-			string += "'";
-			if (choice2 == optionValues2[x])
-				string += F(" selected");
-			string += ">";
-			string += options2[x];
-			string += F("</option>");
+		case PLUGIN_DEVICE_ADD: {
+			Device[++deviceCount].Number = PLUGIN_ID_048;
+			Device[deviceCount].Type = DEVICE_TYPE_I2C;
+			Device[deviceCount].VType = SENSOR_TYPE_SINGLE;
+			Device[deviceCount].Ports = 0;
+			Device[deviceCount].PullUpOption = false;
+			Device[deviceCount].InverseLogicOption = false;
+			Device[deviceCount].FormulaOption = false;
+			Device[deviceCount].ValueCount = 0;
+			Device[deviceCount].SendDataOption = false;
+			Device[deviceCount].TimerOption = false;
+			break;
 		}
-		string += F("</select>");
 
-		byte choice3 = Settings.TaskDevicePluginConfig[event->TaskIndex][3];
-		String options3[4];
-		options3[0] = F("1");
-		options3[1] = F("2");
-		options3[2] = F("3");
-		options3[3] = F("4");
-		int optionValues3[4];
-		optionValues3[0] = 1;
-		optionValues3[1] = 2;
-		optionValues3[2] = 3;
-		optionValues3[3] = 4;
-		string += F("<TR><TD>Motor Number:<TD><select name='plugin_048_motornumber'>");
-		for (byte x = 0; x < 4; x++) {
-			string += F("<option value='");
-			string += optionValues3[x];
-			string += "'";
-			if (choice3 == optionValues3[x])
-				string += F(" selected");
-			string += ">";
-			string += options3[x];
-			string += F("</option>");
+		case PLUGIN_GET_DEVICENAME: {
+			string = F(PLUGIN_NAME_048);
+			break;
 		}
-		string += F("</select>");
 
-		success = true;
-		break;
-	}
+		case PLUGIN_GET_DEVICEVALUENAMES: {
+			strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[0],
+					PSTR(PLUGIN_VALUENAME1_048));
+			break;
+		}
 
-	case PLUGIN_WEBFORM_SAVE: {
-		String plugin1 = WebServer.arg(F("plugin_048_adr"));
-		Settings.TaskDevicePluginConfig[event->TaskIndex][0] = (int) strtol(plugin1.c_str(), 0, 16);
-		String plugin2 = WebServer.arg(F("plugin_048_motortype"));
-		Settings.TaskDevicePluginConfig[event->TaskIndex][1] = plugin2.toInt();
-		String plugin4 = WebServer.arg(F("plugin_048_motornumber"));
-		Settings.TaskDevicePluginConfig[event->TaskIndex][3] = plugin4.toInt();
-		success = true;
-		break;
-	}
+		case PLUGIN_WEBFORM_LOAD: {
 
-	case PLUGIN_INIT: {
-		Plugin_048_MotorShield_address = Settings.TaskDevicePluginConfig[event->TaskIndex][0];
-		Plugin_048_MotorType = Settings.TaskDevicePluginConfig[event->TaskIndex][1];
-		Plugin_048_MotorNumber = Settings.TaskDevicePluginConfig[event->TaskIndex][3];
+			string +=
+			F("<TR><TD>I2C Address (Hex): <TD><input type='text' title='Set i2c Address of sensor' name='");
+			string += F("plugin_048_adr' value='0x");
+			string += String(Settings.TaskDevicePluginConfig[event->TaskIndex][0],HEX);
+			string += F("'>");
 
-		success = true;
-		break;
-	}
+			string +=
+			F("<TR><TD>Stepper: steps per revolution: <TD><input type='text' title='Set steps per revolution for steppers' name='");
+			string += F("plugin_048_MotorStepsPerRevolution' value='");
+			string += String(Settings.TaskDevicePluginConfig[event->TaskIndex][1]);
+			string += F("'>");
+
+			string +=
+			F("<TR><TD>Stepper speed (rpm): <TD><input type='text' title='Set speed of the stepper motor rotation in RPM' name='");
+			string += F("plugin_048_StepperSpeed' value='");
+			string += String(Settings.TaskDevicePluginConfig[event->TaskIndex][2]);
+			string += F("'>");
 
 
-	case PLUGIN_READ: {
-
-		success = false;
-		break;
-	}
-
-	case PLUGIN_WRITE: {
-
-		String tmpString = string;
-		int argIndex = tmpString.indexOf(',');
-		if (argIndex)
-			tmpString = tmpString.substring(0, argIndex);
-
-		if (tmpString.equalsIgnoreCase(F("MotorShieldCMD"))) {
-
-			// Create the motor shield object with the default I2C address
-			AFMS = Adafruit_MotorShield(Plugin_048_MotorShield_address);
-
-			if (Plugin_048_MotorType == 0) {
-				myMotor = AFMS.getMotor(Plugin_048_MotorNumber);
-			}
-			if (Plugin_048_MotorType == 1) {
-				// TODO remove hardcoded 200 steps per revolution (1.8 degree)
-				myStepper = AFMS.getStepper(200,Plugin_048_MotorNumber);
-			}
-			if (Plugin_048_MotorType == 2) {
-				// TODO Servo
-			}
-
-			String log = F("MotorShield: Address: 0x");
-			log += String(Plugin_048_MotorShield_address,HEX);
-			log += F(" Motortype: ");
-			log += String(Plugin_048_MotorNumber);
-			log += F(" Motor#: ");
-			log += String(Plugin_048_MotorNumber);
-			addLog(LOG_LEVEL_DEBUG, log);
-
-			AFMS.begin();  // create with the default frequency 1.6KHz
 
 			success = true;
-			argIndex = string.indexOf(',');
-			tmpString = string.substring(argIndex + 1);
-
-			String cmdString = tmpString.substring(0,tmpString.indexOf(','));
-
-			if (cmdString.equalsIgnoreCase(F("Forward"))) {
-				if (event->Par2 >= 0 && event->Par2 <= 255) {
-					Plugin_048_MotorSpeed = event->Par2;
-					addLog(LOG_LEVEL_INFO, "DCMotor->Forward Speed: " + String(Plugin_048_MotorSpeed));
-					myMotor->setSpeed(Plugin_048_MotorSpeed);
-					myMotor->run(FORWARD);
-				}
-			}
-			else if (cmdString.equalsIgnoreCase(F("Backward"))) {
-				if (event->Par2 >= 0 && event->Par2 <= 255) {
-					Plugin_048_MotorSpeed = event->Par2;
-					addLog(LOG_LEVEL_INFO, "DCMotor->Backward Speed: " + String(Plugin_048_MotorSpeed));
-					myMotor->setSpeed(Plugin_048_MotorSpeed);
-					myMotor->run(BACKWARD);
-				}
-			}
-			else if (tmpString.equalsIgnoreCase(F("Release"))) {
-				addLog(LOG_LEVEL_INFO, "DCMotor->Release");
-				myMotor->run(RELEASE);
-			}
+			break;
 		}
-		break;
-	}
+
+		case PLUGIN_WEBFORM_SAVE: {
+			String plugin1 = WebServer.arg(F("plugin_048_adr"));
+			Settings.TaskDevicePluginConfig[event->TaskIndex][0] = (int) strtol(plugin1.c_str(), 0, 16);
+			String plugin2 = WebServer.arg(F("plugin_048_MotorStepsPerRevolution"));
+			Settings.TaskDevicePluginConfig[event->TaskIndex][1] = plugin2.toInt();
+			String plugin3 = WebServer.arg(F("plugin_048_StepperSpeed"));
+			Settings.TaskDevicePluginConfig[event->TaskIndex][2] = plugin3.toInt();
+			success = true;
+			break;
+		}
+
+		case PLUGIN_INIT: {
+			Plugin_048_MotorShield_address = Settings.TaskDevicePluginConfig[event->TaskIndex][0];
+			Plugin_048_MotorStepsPerRevolution = Settings.TaskDevicePluginConfig[event->TaskIndex][1];
+			Plugin_048_StepperSpeed = Settings.TaskDevicePluginConfig[event->TaskIndex][2];
+
+			success = true;
+			break;
+		}
+
+		case PLUGIN_READ: {
+
+			success = false;
+			break;
+		}
+
+		case PLUGIN_WRITE: {
+
+			String tmpString = string;
+
+			String cmd = parseString(tmpString, 1);
+			String param1 = parseString(tmpString, 2);
+			String param2 = parseString(tmpString, 3);
+			String param3 = parseString(tmpString, 4);
+			String param4 = parseString(tmpString, 5);
+			String param5 = parseString(tmpString, 6);
+//			String log = "Debug parsesting: ";
+//			log += tmpString;
+//			log += " cmd: ";
+//			log += cmd;
+//			log += " param1: ";
+//			log += param1;
+//			log += " param2: ";
+//			log += param2;
+//			log += " param3: ";
+//			log += param3;
+//			log += " param4: ";
+//			log += param4;
+//			log += " param5: ";
+//			log += param5;
+//			addLog(LOG_LEVEL_DEBUG_MORE, log);
+
+			// Commands:
+			// MotorShieldCMD,<DCMotor>,<Motornumber>,<Forward/Backward/Release>,<Speed>
+
+			if (cmd.equalsIgnoreCase(F("MotorShieldCMD")))
+			{
+
+				// Create the motor shield object with the default I2C address
+				AFMS = Adafruit_MotorShield(Plugin_048_MotorShield_address);
+				String log = F("MotorShield: Address: 0x");
+				log += String(Plugin_048_MotorShield_address,HEX);
+				addLog(LOG_LEVEL_DEBUG, log);
+
+				if (param1.equalsIgnoreCase(F("DCMotor"))) {
+					if (param2.toInt() > 0 && param2.toInt() < 5)
+					{
+						myMotor = AFMS.getMotor(param2.toInt());
+						if (param3.equalsIgnoreCase(F("Forward")))
+						{
+							byte speed = 255;
+							if (param4.toInt() >= 0 && param4.toInt() <= 255)
+								speed = param4.toInt();
+							AFMS.begin();
+							addLog(LOG_LEVEL_INFO, "DCMotor" + param2 + "->Forward Speed: " + String(speed));
+							myMotor->setSpeed(speed);
+							myMotor->run(FORWARD);
+							success = true;
+						}
+						if (param3.equalsIgnoreCase(F("Backward")))
+						{
+							byte speed = 255;
+							if (param4.toInt() >= 0 && param4.toInt() <= 255)
+								speed = param4.toInt();
+							AFMS.begin();
+							addLog(LOG_LEVEL_INFO, "DCMotor" + param2 + "->Backward Speed: " + String(speed));
+							myMotor->setSpeed(speed);
+							myMotor->run(BACKWARD);
+							success = true;
+						}
+						if (param3.equalsIgnoreCase(F("Release")))
+						{
+							AFMS.begin();
+							addLog(LOG_LEVEL_INFO, "DCMotor" + param2 + "->Release");
+							myMotor->run(RELEASE);
+							success = true;
+						}
+					}
+				}
+
+				// MotorShieldCMD,<Stepper>,<Motornumber>,<Forward/Backward/Release>,<Steps>,<SINGLE/DOUBLE/INTERLEAVE/MICROSTEP>
+				if (param1.equalsIgnoreCase(F("Stepper")))
+				{
+					// Stepper# is which port it is connected to. If you're using M1 and M2, its port 1.
+					// If you're using M3 and M4 indicate port 2
+					if (param2.toInt() > 0 && param2.toInt() < 3)
+					{
+						myStepper = AFMS.getStepper(Plugin_048_MotorStepsPerRevolution, param2.toInt());
+						myStepper->setSpeed(Plugin_048_StepperSpeed);
+						String log = F("MotorShield: StepsPerRevolution: ");
+						log += String(Plugin_048_MotorStepsPerRevolution);
+						log += F(" Stepperspeed: ");
+						log += String(Plugin_048_StepperSpeed);
+						addLog(LOG_LEVEL_DEBUG_MORE, log);
+
+						if (param3.equalsIgnoreCase(F("Forward")))
+						{
+							int steps = 0;
+							if (param4.toInt())
+							{
+								steps = param4.toInt();
+								if (param5.equalsIgnoreCase(F("SINGLE")))
+								{
+									AFMS.begin();
+									addLog(LOG_LEVEL_INFO, "Stepper" + param2 + "->Forward Steps: " + steps + " SINGLE");
+									myStepper->step(steps, FORWARD, SINGLE);
+									success = true;
+								}
+								if (param5.equalsIgnoreCase(F("DOUBLE")))
+								{
+									AFMS.begin();
+									addLog(LOG_LEVEL_INFO, "Stepper" + param2 + "->Forward Steps: " + steps + " DOUBLE");
+									myStepper->step(steps, FORWARD, DOUBLE);
+									success = true;
+								}
+								if (param5.equalsIgnoreCase(F("INTERLEAVE")))
+								{
+									AFMS.begin();
+									addLog(LOG_LEVEL_INFO, "Stepper" + param2 + "->Forward Steps: " + steps + " INTERLEAVE");
+									myStepper->step(steps, FORWARD, INTERLEAVE);
+									success = true;
+								}
+								if (param5.equalsIgnoreCase(F("MICROSTEP")))
+								{
+									AFMS.begin();
+									addLog(LOG_LEVEL_INFO, "Stepper" + param2 + "->Forward Steps: " + steps + " MICROSTEP");
+									myStepper->step(steps, FORWARD, MICROSTEP);
+									success = true;
+								}
+							}
+						}
+
+						if (param3.equalsIgnoreCase(F("Backward")))
+						{
+							int steps = 0;
+							if (param4.toInt())
+							{
+								steps = param4.toInt();
+								if (param5.equalsIgnoreCase(F("SINGLE")))
+								{
+									AFMS.begin();
+									addLog(LOG_LEVEL_INFO, "Stepper" + param2 + "->Backward Steps: " + steps + " SINGLE");
+									myStepper->step(steps, BACKWARD, SINGLE);
+									success = true;
+								}
+								if (param5.equalsIgnoreCase(F("DOUBLE")))
+								{
+									AFMS.begin();
+									addLog(LOG_LEVEL_INFO, "Stepper" + param2 + "->Backward Steps: " + steps + " DOUBLE");
+									myStepper->step(steps, BACKWARD, DOUBLE);
+									success = true;
+								}
+								if (param5.equalsIgnoreCase(F("INTERLEAVE")))
+								{
+									AFMS.begin();
+									addLog(LOG_LEVEL_INFO, "Stepper" + param2 + "->Backward Steps: " + steps + " INTERLEAVE");
+									myStepper->step(steps, BACKWARD, INTERLEAVE);
+									success = true;
+								}
+								if (param5.equalsIgnoreCase(F("MICROSTEP")))
+								{
+									AFMS.begin();
+									addLog(LOG_LEVEL_INFO, "Stepper" + param2 + "->Backward Steps: " + steps + " MICROSTEP");
+									myStepper->step(steps, BACKWARD, MICROSTEP);
+									success = true;
+								}
+
+							}
+						}
+
+						if (param3.equalsIgnoreCase(F("Release")))
+						{
+							AFMS.begin();
+							addLog(LOG_LEVEL_INFO, "Stepper" + param2 + "->Release.");
+							myStepper->release();
+							success = true;
+						}
+
+					}
+				}
+
+			}
+
+			break;
+		}
 
 	}
 	return success;
 }
+
 
 #endif
