e44464f53288368579530e36050762882bd16406
Edwin Eefting <edwin@datux.nl>
2017-04-02 01:38:27 +0200

    temporary disabled IR plugins, because of iram1_0_seg issues

--- src/_P035_IRTX.ino
+++ src/_P035_IRTX.ino
@@ -1,6 +1,7 @@
 //#######################################################################################################
 //#################################### Plugin 035: Output IR ############################################
 //#######################################################################################################
+#ifdef PLUGIN_BUILD_BROKEN
 
 #include <IRremoteESP8266.h>
 IRsend *Plugin_035_irSender;
@@ -170,7 +171,7 @@ boolean Plugin_035(byte function, struct EventStruct *event, String& string)
             if (IrType.equalsIgnoreCase("RC5")) Plugin_035_irSender->sendRC5(IrCode, IrBits);
             if (IrType.equalsIgnoreCase("RC6")) Plugin_035_irSender->sendRC6(IrCode, IrBits);
             if (IrType.equalsIgnoreCase("SAMSUNG")) Plugin_035_irSender->sendSAMSUNG(IrCode, IrBits);
-            if (IrType.equalsIgnoreCase("SONY")) Plugin_035_irSender->sendSony(IrCode, IrBits, 2);
+            if (IrType.equalsIgnoreCase("SONY")) Plugin_035_irSender->sendSony(IrCode, IrBits);
             if (IrType.equalsIgnoreCase("PANASONIC")) Plugin_035_irSender->sendPanasonic(IrBits, IrCode);
           }
 
@@ -191,3 +192,4 @@ boolean Plugin_035(byte function, struct EventStruct *event, String& string)
   return success;
 }
 
+#endif
