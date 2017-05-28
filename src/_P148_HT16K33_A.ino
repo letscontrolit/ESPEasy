732aefa43efd9173f3c4467822dec80b19418a0e
JK <jkdesign@me.com>
2017-05-28 10:22:18 +0200

    work release

--- src/_P148_HT16K33_A.ino
+++ src/_P148_HT16K33_A.ino
@@ -1,49 +1,19 @@
-//######################################## DMX512 TX ####################################################
+//######################################## HT16K33 ####################################################
 //#######################################################################################################
 
-// ESPEasy Plugin to control DMX-512 Devices (DMX 512/1990; DIN 56930-2) like Dimmer-Packs, LED-Bars, Moving-Heads, Event-Lighting
+// ESPEasy Plugin to control LED matrix chip HT16K33
 // written by Jochen Krapf (jk@nerd2nerd.org)
 
 // List of commands:
-// (1) DMX,<param>
+// (1) ???,<param>
 // (2) DMX,<param>,<param>,<param>, ...
 
 // List of DMX params:
 // (a) <value>
 //     DMX-value (0...255) to write to the next channel address (1...512) starting with 1
-// (b) <channel>=<value>
-//     DMX-value (0...255) to write to the given channel address (1...512).
-// (c) <channel>=<value>,<value>,<value>,...
-//     List of DMX-values (0...255) to write beginning with the given channel address (1...512).
-// (d) "OFF"
-//     Set DMX-values of all channels to 0.
-// (e) "ON"
-//     Set DMX-values of all channels to 255.
-// (f) "LOG"
-//     Print DMX-values of all channels to log output.
 
 // Examples:
 // DMX,123"   Set channel 1 to value 123
-// DMX,123,22,33,44"   Set channel 1 to value 123, channel 2 to 22, channel 3 to33, channel 4 to 44
-// DMX,5=123"   Set channel 5 to value 123
-// DMX,5=123,22,33,44"   Set channel 5 to value 123, channel 6 to 22, channel 7 to33, channel 8 to 44
-// DMX,5=123,8=44"   Set channel 5 to value 123, channel 8 to 44
-// DMX,OFF"   Pitch Black
-
-// Transeiver:
-// SN75176 or MAX485 or LT1785 or ...
-// Pin 5: GND
-// Pin 2, 3, 5: +5V
-// Pin 4: to ESP D4
-// Pin 6: DMX+ (hot)
-// Pin 7: DMX- (cold)
-
-// XLR Plug:
-// Pin 1: GND, Shield
-// Pin 2: DMX- (cold)
-// Pin 3: DMX+ (hot)
-
-// Note: The ESP serial FIFO has size of 128 byte. Therefore it is rcommented to use DMX buffer sizes below 128
 
 
 //#include <*.h>   //no lib needed
@@ -90,13 +60,9 @@ boolean Plugin_148(byte function, struct EventStruct *event, String& string)
     case PLUGIN_WEBFORM_LOAD:
       {
         byte addr = Settings.TaskDevicePluginConfig[event->TaskIndex][0];
-        /*
-        String options[2];
-        options[0] = F("0x76 - default settings (SDO Low)");
-        options[1] = F("0x77 - alternate settings (SDO HIGH)");
-        */
-        int optionValues[2] = { 0x76, 0x77 };
-        addFormSelectorI2C(string, F("i2c_addr"), 2, optionValues, addr);
+
+        int optionValues[8] = { 0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77 };
+        addFormSelectorI2C(string, F("i2c_addr"), 8, optionValues, addr);
 
 
 
@@ -112,9 +78,6 @@ boolean Plugin_148(byte function, struct EventStruct *event, String& string)
       {
         Settings.TaskDevicePluginConfig[event->TaskIndex][0] = getFormItemInt(F("i2c_addr"));
 
-        Settings.TaskDevicePin1[event->TaskIndex] = 2;
-        if (Settings.Pin_status_led == 2)
-          Settings.Pin_status_led = -1;
         Plugin_148_DMXSize = getFormItemInt(F("channels"));
         Limit (Plugin_148_DMXSize, 1, 512);
         Settings.TaskDevicePluginConfig[event->TaskIndex][0] = Plugin_148_DMXSize;
@@ -233,23 +196,7 @@ boolean Plugin_148(byte function, struct EventStruct *event, String& string)
       {
         if (Plugin_148_DMXBuffer)
         {
-          int sendPin = 2;   //TX1 fix to GPIO2 (D4) == onboard LED
 
-          //empty serial from prev. transmit
-          Serial1.flush();
-
-          //send break
-          Serial1.end();
-          pinMode(sendPin, OUTPUT);
-          digitalWrite(sendPin, LOW);
-          delayMicroseconds(120);   //88µs ... inf
-          digitalWrite(sendPin, HIGH);
-          delayMicroseconds(12);   //8µs ... 1s
-
-          //send DMX data
-          Serial1.begin(250000, SERIAL_8N2);
-          Serial1.write(0);   //start byte
-          Serial1.write(Plugin_148_DMXBuffer, Plugin_148_DMXSize);
         }
         break;
       }
