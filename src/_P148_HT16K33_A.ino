816f430fbe68fee3ddd1fc36236cac515c289238
JK <jkdesign@me.com>
2017-05-28 23:53:07 +0200

    work release

--- src/_P148_HT16K33_A.ino
+++ src/_P148_HT16K33_A.ino
@@ -27,6 +27,110 @@
 byte* Plugin_148_DMXBuffer = 0;
 int Plugin_148_DMXSize = 32;
 
+class CHT16K33 {
+ public:
+  CHT16K33(void) {};
+
+  void Init(uint8_t addr)
+  {
+    _addr = addr;
+
+    // System Setup Register
+    Wire.beginTransmission(_addr);
+    Wire.write(0x21);  // oscillator on
+    Wire.endTransmission();
+
+    // Display Setup Register
+    Wire.beginTransmission(_addr);
+    Wire.write(0x81);  // blink off; display on
+    Wire.endTransmission();
+
+    SetBrightness(15);
+    Clear();
+  };
+
+  void SetBrightness(uint8_t b)
+  {
+    if (b > 15)
+      b = 15;
+    // Digital Dimming Data Input
+    Wire.beginTransmission(_addr);
+    Wire.write(0xE0 | b);  // brightness
+    Wire.endTransmission();
+  };
+
+  void Transmit(void)
+  {
+    // Display Memory
+    Wire.beginTransmission(_addr);
+    Wire.write(0); // start data at address 0
+    for (byte i=0; i<8; i++)
+    {
+      Wire.write(_buffer[i] & 0xFF);
+      Wire.write(_buffer[i] >> 8);
+    }
+    Wire.endTransmission();
+  };
+
+  void Keys(void)
+  {
+    // Display Memory
+    Wire.beginTransmission(_addr);
+    Wire.write(0x40); // start data at address 0x40
+    for (byte i=0; i<3; i++)
+    {
+      _keys[i] = Wire.read() | (Wire.read() << 8);
+    }
+    Wire.endTransmission();
+  };
+
+  void Clear(void)
+  {
+    for (byte i=0; i<8; i++)
+      _buffer[i] = 0;
+  };
+
+  void SetRow(uint8_t row, uint16_t data)
+  {
+    if (row < 8)
+      _buffer[row] = data;
+  };
+
+  uint16_t GetRow(uint8_t row)
+  {
+    if (row < 8)
+      return _buffer[row];
+    else
+      return 0;
+  };
+
+  uint8_t _addr;
+  uint16_t _buffer[8];
+  uint16_t _keys[3];
+};
+
+CHT16K33* Plugin_148_M = NULL;
+
+PROGMEM static const uint8_t diits[] =
+{
+	0x3F, /* 0 */
+	0x06, /* 1 */
+	0x5B, /* 2 */
+	0x4F, /* 3 */
+	0x66, /* 4 */
+	0x6D, /* 5 */
+	0x7D, /* 6 */
+	0x07, /* 7 */
+	0x7F, /* 8 */
+	0x6F, /* 9 */
+	0x77, /* a */
+	0x7C, /* b */
+	0x39, /* C */
+	0x5E, /* d */
+	0x79, /* E */
+	0x71, /* F */
+};
+
 
 boolean Plugin_148(byte function, struct EventStruct *event, String& string)
 {
@@ -79,21 +183,20 @@ boolean Plugin_148(byte function, struct EventStruct *event, String& string)
         Settings.TaskDevicePluginConfig[event->TaskIndex][0] = getFormItemInt(F("i2c_addr"));
 
         Plugin_148_DMXSize = getFormItemInt(F("channels"));
-        Limit (Plugin_148_DMXSize, 1, 512);
-        Settings.TaskDevicePluginConfig[event->TaskIndex][0] = Plugin_148_DMXSize;
+        //Limit (Plugin_148_DMXSize, 1, 512);
+        //Settings.TaskDevicePluginConfig[event->TaskIndex][0] = Plugin_148_DMXSize;
         success = true;
         break;
       }
 
     case PLUGIN_INIT:
       {
-        Settings.TaskDevicePin1[event->TaskIndex] = 2;   //TX1 fix to GPIO2 (D4) == onboard LED
-        Plugin_148_DMXSize = Settings.TaskDevicePluginConfig[event->TaskIndex][0];
+        byte addr = Settings.TaskDevicePluginConfig[event->TaskIndex][0];
 
-        if (Plugin_148_DMXBuffer)
-          delete [] Plugin_148_DMXBuffer;
-        Plugin_148_DMXBuffer = new byte[Plugin_148_DMXSize];
-        memset(Plugin_148_DMXBuffer, 0, Plugin_148_DMXSize);
+        if (!Plugin_148_M)
+          Plugin_148_M = new CHT16K33;
+
+        Plugin_148_M->Init(addr);
 
         success = true;
         break;
@@ -101,16 +204,39 @@ boolean Plugin_148(byte function, struct EventStruct *event, String& string)
 
     case PLUGIN_WRITE:
       {
+        if (!Plugin_148_M)
+          return false;
+
         string.toLowerCase();
         String command = parseString(string, 1);
 
-        if (command == F("dmx"))
+        if (command == F("num"))
+        {
+          String number = parseString(string, 2);
+          byte row = 0;
+
+          Plugin_148_M->Clear();
+          while (number[row] && row < 8)
+          {
+            int value = number[row];
+            if (value >= 'A')
+              value -= 'A' - 10;
+            else
+              value -= '0';
+            value = diits[value & 0xF];
+            Plugin_148_M->SetRow(row, value);
+            row++;
+          }
+          Plugin_148_M->Transmit();
+        }
+
+        else if (command == F("mx") || command == F("seg7"))
         {
           String param;
           String paramKey;
           String paramVal;
           byte paramIdx = 2;
-          int channel = 1;
+          int row = 0;
           int value = 0;
 
           string.replace("  ", " ");
@@ -126,11 +252,11 @@ boolean Plugin_148(byte function, struct EventStruct *event, String& string)
 
               if (param == F("log"))
               {
-                String log = F("DMX  : ");
-                for (int i = 0; i < Plugin_148_DMXSize; i++)
+                String log = F("MX   : ");
+                for (int i = 0; i < 8; i++)
                 {
-                  log += Plugin_148_DMXBuffer[i];
-                  log += F(", ");
+                  log += String(Plugin_148_M->GetRow(i), 16);
+                  log += F("h, ");
                 }
                 addLog(LOG_LEVEL_INFO, log);
                 success = true;
@@ -138,32 +264,25 @@ boolean Plugin_148(byte function, struct EventStruct *event, String& string)
 
               else if (param == F("test"))
               {
-                for (int i = 0; i < Plugin_148_DMXSize; i++)
-                  //Plugin_148_DMXBuffer[i] = i+1;
-                  Plugin_148_DMXBuffer[i] = rand()&255;
+                for (int i = 0; i < 8; i++)
+                  Plugin_148_M->SetRow(i, 1 << i);
                 success = true;
               }
 
-              else if (param == F("on"))
+              else if (param == F("clear"))
               {
-                memset(Plugin_148_DMXBuffer, 255, Plugin_148_DMXSize);
-                success = true;
-              }
-
-              else if (param == F("off"))
-              {
-                memset(Plugin_148_DMXBuffer, 0, Plugin_148_DMXSize);
+                Plugin_148_M->Clear();
                 success = true;
               }
 
               else
               {
                 int index = param.indexOf('=');
-                if (index > 0)   //syntax: "<channel>=<value>"
+                if (index > 0)   //syntax: "<row>=<value>"
                 {
                   paramKey = param.substring(0, index);
                   paramVal = param.substring(index+1);
-                  channel = paramKey.toInt();
+                  row = paramKey.toInt();
                 }
                 else   //syntax: "<value>"
                 {
@@ -171,11 +290,19 @@ boolean Plugin_148(byte function, struct EventStruct *event, String& string)
                 }
 
                 value = paramVal.toInt();
-                Limit (value, 0, 255);
 
-                if (channel > 0 && channel <= Plugin_148_DMXSize)
-                  Plugin_148_DMXBuffer[channel-1] = value;
-                channel++;
+                if (command == F("seg7"))
+                {
+                  if (value < 16)
+                    value = diits[value];
+                  else
+                    value = 0;
+                }
+
+                if (row >= 0 && row < 8)
+                  Plugin_148_M->SetRow(row, value);
+                success = true;
+                row++;
               }
 
               param = parseString(string, paramIdx++);
@@ -186,6 +313,8 @@ boolean Plugin_148(byte function, struct EventStruct *event, String& string)
             //??? no params
           }
 
+          if (success)
+            Plugin_148_M->Transmit();
           success = true;
         }
 
