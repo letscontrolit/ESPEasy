de4517f614ecd706b83d1dac805e8b7b269e8aa6
mvdbro <mvdbro@zonnet.nl>
2017-06-11 09:39:10 +0200

    Fix incorrect ADC GPIO display

--- src/_P037_MQTTImport.ino
+++ src/_P037_MQTTImport.ino
@@ -39,7 +39,7 @@ boolean Plugin_037(byte function, struct EventStruct *event, String& string)
     case PLUGIN_DEVICE_ADD:
       {
         Device[++deviceCount].Number = PLUGIN_ID_037;
-        Device[deviceCount].Type = SENSOR_TYPE_SWITCH;
+        Device[deviceCount].Type = DEVICE_TYPE_DUMMY;
         Device[deviceCount].VType = SENSOR_TYPE_SINGLE;     // This means it has a single pin
         Device[deviceCount].Ports = 0;
         Device[deviceCount].PullUpOption = false;
