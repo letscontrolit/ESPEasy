10e33de01899957c6da51aa0d745eab9f502a957
JK <jkdesign@me.com>
2017-06-22 13:20:13 +0200

    Fine tuning of CSS and menu

--- src/WebServer.ino
+++ src/WebServer.ino
@@ -5,35 +5,38 @@
 static const char pgDefaultCSS[] PROGMEM = {
   //color sheme: #07D #D50 #DB0 #A0D
   "* {font-family:sans-serif; font-size:12pt;}"
-  "h1 {font-size:16pt; color:#D50; margin:8px 0 0 0; font-weight:bold;}"
-  "h2 {font-size:12pt; margin:8px -4px 0 -4px; padding:6px; background-color:#444; color:#FFF; font-weight:bold;}"
+  "h1 {font-size:16pt; color:#D50; margin:8px 0; font-weight:bold;}"
+  "h2 {font-size:12pt; margin:0 -4px; padding:6px; background-color:#444; color:#FFF; font-weight:bold;}"
   "h3 {font-size:12pt; margin:16px -4px 0 -4px; padding:4px; background-color:#EEE; color:#444; font-weight:bold;}"
-  "h6 {font-size:10pt; color:#D50; text-align:center;}"
+  "h6 {font-size:10pt; color:#D50;}"
   //buttons
   ".button {margin:4px; padding:4px 16px; background-color:#07D; color:#FFF; text-decoration:none; border-radius:4px}"
   ".button.link {}"
   ".button.help {padding:2px 4px; border:solid 1px #FFF; border-radius:50%}"
   ".button:hover {background:#369;}"
   //tables
-  "th {padding:6px; background-color:#444; color:#FFF; font-weight:bold;}"
+  "th {padding:6px; background-color:#444; color:#FFF; border-color:#888; font-weight:bold;}"
   "td {padding:4px;}"
   "tr {padding:4px;}"
-  "table {color:black;}"
-
+  "table {color:#000;}"
+  //inside a form
+  ".note {color:#444; font-style:italic}"
+  //header with title and menu
+  ".headermenu {position:fixed; top:0; left:0; right:0; height:64px; padding:8px 12px; background-color:#F8F8F8; border-bottom: 1px solid #DDD;}"
+  ".bodymenu {margin-top:96px;}"
+  //menu
+  ".menubar {position:inherit; top:44px;}"
+  ".menu {float:left; height:20px; padding: 4px 16px 8px 16px; color:#444; white-space:nowrap; border:solid transparent; border-width: 4px 1px 1px; border-radius: 4px 4px 0 0; text-decoration: none;}"
+  ".menu.active {color:#000; background-color:#FFF; border-color:#D50 #DDD #FFF;}"
+  ".menu:hover {color:#000; background:#DEF;}"
+  //symbols for enabled
+  ".on {color:green;}"
+  ".off {color:red;}"
+  //others
   ".div_l {float:left;}"
   ".div_r {float:right; margin:2px; padding:1px 10px; border-radius:4px; background-color:#FD0; color:#06B;}"
   ".div_br {clear:both;}"
-  ".note {color:#444; font-style:italic}"
-  ".active {text-decoration:underline;}"
-  ".on {color:green;}"
-  ".off {color:red;}"
-  //header with title and menu
-  ".header {margin:-8px -8px 16px -4px; padding:8px; background-color:#F8F8F8;}"
-  //menu
-  ".menubar {position:relative; margin:8px -8px -8px -8px; padding:20px 8px; border-bottom: 1px solid #DDD;}"
-  ".menu {float:left; height:20px; margin-top:-16px; padding: 4px 16px 8px 16px; color:#444; white-space:nowrap; border:solid transparent; border-width: 4px 1px 1px; border-radius: 4px 4px 0 0; text-decoration: none;}"
-  ".menu.active {color:#000; background-color:#FFF; border-color:#D50 #DDD #FFF;}"
-  ".menu:hover {background:#DEF;}"
+  //".active {text-decoration:underline;}"
   "\0"
 };
 
@@ -145,20 +148,23 @@ void getWebPageTemplateDefault(const String& tmplName, String& tmpl)
   else   //all other template names e.g. TmplStd
   {
     tmpl += F(
-              "<!DOCTYPE html>"
-              "<head>"
-              "<title>{{name}}</title>"
-              "{{js}}"
-              "{{css}}"
-              "</head>"
-              "<body>"
-              "<div class='header'>"
-              "<h1>ESP Easy Mega: {{name}} {{logo}}</h1>"
-              "{{menu}}</div>"
-              "{{error}}"
-              "{{content}}"
-              "<BR><h6>Powered by www.letscontrolit.com</h6>"
-              "</body>"
+      "<!DOCTYPE html><html lang='en'>"
+      "<head>"
+        "<title>{{name}}</title>"
+        //"{{js}}"
+        "{{css}}"
+      "</head>"
+      "<body class='bodymenu'>"
+        "<header class='headermenu'>"
+          "<h1>ESP Easy Mega: {{name}} {{logo}}</h1>"
+          "{{menu}}"
+        "</header>"
+        "{{error}}"
+        "{{content}}"
+        "<footer>"
+          "<h6>Powered by www.letscontrolit.com</h6>"
+        "</footer>"
+      "</body></html>"
             );
   }
 }
@@ -191,6 +197,7 @@ void sendWebPageChunkedEnd(String& log)
 {
   log += F(" [0]");
   WebServer.sendContent("");
+  WebServer.client().flush();
   WebServer.client().stop();   // Stop is needed because we sent no content length
 }
 
