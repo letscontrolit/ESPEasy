728ef430983393061a9cbef9a4825d9060af4923
JK <jkdesign@me.com>
2017-06-26 15:57:16 +0200

    Check plugin-array size on init - issue #119

--- src/__CPlugin.ino
+++ src/__CPlugin.ino
@@ -2,6 +2,19 @@
 // Initialize all Controller CPlugins that where defined earlier
 // and initialize the function call pointer into the CCPlugin array
 //********************************************************************************
+
+static const char ADDCPLUGIN_ERROR[] PROGMEM = "System: Error - To much C-Plugins";
+
+#define ADDCPLUGIN(NNN) \
+  if (x < CPLUGIN_MAX) \
+  { \
+    CPlugin_id[x] = CPLUGIN_ID_##NNN; \
+    CPlugin_ptr[x++] = &CPlugin_##NNN; \
+  } \
+  else \
+    addLog(LOG_LEVEL_ERROR, FPSTR(ADDCPLUGIN_ERROR));
+
+
 void CPluginInit(void)
 {
   byte x;
@@ -16,103 +29,103 @@ void CPluginInit(void)
   x = 0;
 
 #ifdef CPLUGIN_001
-  CPlugin_id[x] = 1; CPlugin_ptr[x++] = &CPlugin_001;
+  ADDCPLUGIN(001)
 #endif
 
 #ifdef CPLUGIN_002
-  CPlugin_id[x] = 2; CPlugin_ptr[x++] = &CPlugin_002;
+  ADDCPLUGIN(002)
 #endif
 
 #ifdef CPLUGIN_003
-  CPlugin_id[x] = 3; CPlugin_ptr[x++] = &CPlugin_003;
+  ADDCPLUGIN(003)
 #endif
 
 #ifdef CPLUGIN_004
-  CPlugin_id[x] = 4; CPlugin_ptr[x++] = &CPlugin_004;
+  ADDCPLUGIN(004)
 #endif
 
 #ifdef CPLUGIN_005
-  CPlugin_id[x] = 5; CPlugin_ptr[x++] = &CPlugin_005;
+  ADDCPLUGIN(005)
 #endif
 
 #ifdef CPLUGIN_006
-  CPlugin_id[x] = 6; CPlugin_ptr[x++] = &CPlugin_006;
+  ADDCPLUGIN(006)
 #endif
 
 #ifdef CPLUGIN_007
-  CPlugin_id[x] = 7; CPlugin_ptr[x++] = &CPlugin_007;
+  ADDCPLUGIN(007)
 #endif
 
 #ifdef CPLUGIN_008
-  CPlugin_id[x] = 8; CPlugin_ptr[x++] = &CPlugin_008;
+  ADDCPLUGIN(008)
 #endif
 
 #ifdef CPLUGIN_009
-  CPlugin_id[x] = 9; CPlugin_ptr[x++] = &CPlugin_009;
+  ADDCPLUGIN(009)
 #endif
 
 #ifdef CPLUGIN_010
-  CPlugin_id[x] = 10; CPlugin_ptr[x++] = &CPlugin_010;
+  ADDCPLUGIN(010)
 #endif
 
 #ifdef CPLUGIN_011
-  CPlugin_id[x] = 11; CPlugin_ptr[x++] = &CPlugin_011;
+  ADDCPLUGIN(011)
 #endif
 
 #ifdef CPLUGIN_012
-  CPlugin_id[x] = 12; CPlugin_ptr[x++] = &CPlugin_012;
+  ADDCPLUGIN(012)
 #endif
 
 #ifdef CPLUGIN_013
-  CPlugin_id[x] = 13; CPlugin_ptr[x++] = &CPlugin_013;
+  ADDCPLUGIN(013)
 #endif
 
 #ifdef CPLUGIN_014
-  CPlugin_id[x] = 14; CPlugin_ptr[x++] = &CPlugin_014;
+  ADDCPLUGIN(014)
 #endif
 
 #ifdef CPLUGIN_015
-  CPlugin_id[x] = 15; CPlugin_ptr[x++] = &CPlugin_015;
+  ADDCPLUGIN(015)
 #endif
 
 #ifdef CPLUGIN_016
-  CPlugin_id[x] = 16; CPlugin_ptr[x++] = &CPlugin_016;
+  ADDCPLUGIN(016)
 #endif
 
 #ifdef CPLUGIN_017
-  CPlugin_id[x] = 17; CPlugin_ptr[x++] = &CPlugin_017;
+  ADDCPLUGIN(017)
 #endif
 
 #ifdef CPLUGIN_018
-  CPlugin_id[x] = 18; CPlugin_ptr[x++] = &CPlugin_018;
+  ADDCPLUGIN(018)
 #endif
 
 #ifdef CPLUGIN_019
-  CPlugin_id[x] = 19; CPlugin_ptr[x++] = &CPlugin_019;
+  ADDCPLUGIN(019)
 #endif
 
 #ifdef CPLUGIN_020
-  CPlugin_id[x] = 20; CPlugin_ptr[x++] = &CPlugin_020;
+  ADDCPLUGIN(020)
 #endif
 
 #ifdef CPLUGIN_021
-  CPlugin_id[x] = 21; CPlugin_ptr[x++] = &CPlugin_021;
+  ADDCPLUGIN(021)
 #endif
 
 #ifdef CPLUGIN_022
-  CPlugin_id[x] = 22; CPlugin_ptr[x++] = &CPlugin_022;
+  ADDCPLUGIN(022)
 #endif
 
 #ifdef CPLUGIN_023
-  CPlugin_id[x] = 23; CPlugin_ptr[x++] = &CPlugin_023;
+  ADDCPLUGIN(023)
 #endif
 
 #ifdef CPLUGIN_024
-  CPlugin_id[x] = 24; CPlugin_ptr[x++] = &CPlugin_024;
+  ADDCPLUGIN(024)
 #endif
 
 #ifdef CPLUGIN_025
-  CPlugin_id[x] = 25; CPlugin_ptr[x++] = &CPlugin_025;
+  ADDCPLUGIN(025)
 #endif
 
   CPluginCall(CPLUGIN_PROTOCOL_ADD, 0);
@@ -125,7 +138,7 @@ byte CPluginCall(byte Function, struct EventStruct *event)
 
  if (event == 0)
     event=&TempEvent;
-    
+
   switch (Function)
   {
     // Unconditional calls to all plugins
@@ -139,4 +152,3 @@ byte CPluginCall(byte Function, struct EventStruct *event)
 
   return false;
 }
-
