b07846fbd0b3e553f2554c6371cf6eb177764f18
Edwin Eefting <edwin@datux.nl>
2017-10-12 21:34:04 +0200

    fixes

--- test/esp.py
+++ test/esp.py
@@ -10,19 +10,25 @@ import serial.tools.miniterm
 def log(txt):
     print(txt, end="", flush=True)
 
-
 class Esp():
 
 
     def __init__(self, config):
         print("Using unit {unit} ({type}) with ip {ip}".format(**config))
         self._config=config
-        self._serial=serial.Serial(port=config['port'], baudrate=115200, timeout=1, write_timeout=1)
         self._url="http://{ip}/".format(**self._config)
+        self._serial_initialized=False
+
+    def serial_needed(self):
+        """call this at least once if you need serial stuff"""
+        if not hasattr(self,"_serial"):
+            self._serial=serial.Serial(port=self._config['port'], baudrate=115200, timeout=1, write_timeout=1)
 
 
     def pingserial(self, timeout=60):
         """waits until espeasy reponds via serial"""
+
+        self.serial_needed()
         self._serial.reset_input_buffer();
         log("Waiting for serial response: ")
         start_time=time.time()
@@ -43,6 +49,7 @@ class Esp():
 
     def reboot(self):
         '''reboot the esp via the serial DTR line'''
+        self.serial_needed()
         self._serial.setDTR(0)
         time.sleep(0.1)
         self._serial.setDTR(1)
@@ -66,6 +73,9 @@ class Esp():
     def wificonfig(self, timeout=60):
         """configure wifi via serial and make sure esp is online and pingable."""
 
+        self.serial_needed()
+
+        self.reboot()
         self.pingserial(timeout=timeout)
 
         serial_str="wifissid {ssid}\nwifikey {password}\nip {ip}\nsave\nreboot\n".format(ssid=wificonfig.ssid, password=wificonfig.password, ip=self._config['ip'])
@@ -83,6 +93,7 @@ class Esp():
     def flashserial(self):
         """flash binary to esp via serial"""
 
+        self.serial_needed()
 
         subprocess.check_call(self._config['flash_cmd'].format(**self._config), shell=True, cwd='..')
 
@@ -93,6 +104,7 @@ class Esp():
 
     def serial(self):
         """open serial terminal to esp"""
+        self.serial_needed()
         subprocess.check_call("platformio serialports monitor --baud 115200 --port {port} --echo".format(**self._config), shell=True, cwd='..')
         # print("JA")
         # term=serial.tools.miniterm.Miniterm(self._serial)
@@ -103,6 +115,7 @@ class Esp():
 
     def erase(self):
         """erase flash via serial"""
+        self.serial_needed()
         subprocess.check_call("esptool.py --port {port} -b 1500000  erase_flash".format(**self._config), shell=True, cwd='..')
 
 
