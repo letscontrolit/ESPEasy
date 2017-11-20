f853f6ef256e324fe32bf40905c9fa9f86496437
papperone <giovanni.castania@gmail.com>
2017-11-20 22:52:01 +0100

    deleted GPIO notes referring to 75HC595 not supported (#591)

--- src/_P073_7DGT.ino
+++ src/_P073_7DGT.ino
@@ -89,7 +89,6 @@ boolean Plugin_073(byte function, struct EventStruct *event, String& string)
       {
         addFormNote(string, F("TM1637:  1st=CLK-Pin, 2nd=DIO-Pin"));
         addFormNote(string, F("MAX7219: 1st=DIN-Pin, 2nd=CLK-Pin, 3rd=CS-Pin"));
-        addFormNote(string, F("74HC595: 1st=DIO-Pin, 2nd=RCLK-Pin, 3rd=SCLK-Pin"));
         String displtype[3] = { F("TM1637 - 4 digit"), F("MAX7219 - 8 digit")};
         addFormSelector(string, F("Display Type"), F("plugin_073_displtype"), 2, displtype, NULL, Settings.TaskDevicePluginConfig[event->TaskIndex][0]);
         String displout[4] = { F("Manual"), F("Clock - Blink"), F("Clock - No Blink"), F("Date")  };
