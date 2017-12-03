9cf92a1ed69a2025f0f1631b8154f874ac9058bf
Unknown <gijs.noorlander@gmail.com>
2017-12-03 12:24:01 +0100

    Move CO2 MH-Z19 plugin from "testing" to "normal"
    As discussed in #616

--- src/_P049_MHZ19.ino
+++ src/_P049_MHZ19.ino
@@ -9,11 +9,9 @@
   DevicePin2 - is TX for ESP
 */
 
-#ifdef PLUGIN_BUILD_TESTING
-
 #define PLUGIN_049
 #define PLUGIN_ID_049         49
-#define PLUGIN_NAME_049       "Gases - CO2 MH-Z19 [TESTING]"
+#define PLUGIN_NAME_049       "Gases - CO2 MH-Z19"
 #define PLUGIN_VALUENAME1_049 "PPM"
 #define PLUGIN_VALUENAME2_049 "Temperature" // Temperature in C
 #define PLUGIN_VALUENAME3_049 "U" // Undocumented, minimum measurement per time period?
@@ -358,5 +356,3 @@ boolean Plugin_049(byte function, struct EventStruct *event, String& string)
   }
   return success;
 }
-
-#endif
