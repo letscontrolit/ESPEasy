548bf8b86756759601218d7ff5eecd3deb801ea9
Edwin Eefting <edwin@datux.nl>
2018-01-11 01:49:57 +0100

    added frame oled test

--- test/espeasy.py
+++ test/espeasy.py
@@ -102,3 +102,42 @@ class EspEasy:
                 page:1
             """.format(**kwargs)
         )
+
+    def device_p036(self, **kwargs):
+        self._node.log.info("Config device plugin p036 "+str(kwargs))
+
+        self._node.http_post(
+            twice=True, # needed for controllers and devices because of the way its implemented
+            page="devices",
+
+            params="""
+                index:{index}
+            """.format(**kwargs),
+
+            data="""
+                TDNUM:36
+                TDN:
+                TDE:on
+                plugin_036_adr:60
+                plugin_036_rotate:1
+                plugin_036_nlines:1
+                plugin_036_scroll:1
+                Plugin_036_template1:espeasy
+                Plugin_036_template2:test
+                Plugin_036_template3:suite
+                Plugin_036_template4:test1
+                Plugin_036_template5:test2
+                Plugin_036_template6:test3
+                Plugin_036_template7:test4
+                Plugin_036_template8:test5
+                Plugin_036_template9:test6
+                Plugin_036_template10:test7
+                Plugin_036_template11:test8
+                Plugin_036_template12:test9
+                taskdevicepin3:-1
+                plugin_036_timer:0
+                TDT:1
+                edit:1
+                page:1
+            """.format(**kwargs)
+        )
