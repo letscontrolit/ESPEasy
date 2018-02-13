76deab593ac513caabd3c389701d64e34244269b
Bartłomiej Zimoń <cactus.list@gmail.com>
2018-02-13 22:18:50 +0100

    Update _C012.ino
    add SENSOR_TYPE_SWITCH support

--- src/_C012.ino
+++ src/_C012.ino
@@ -20,7 +20,7 @@ boolean CPlugin_012(byte function, struct EventStruct *event, String& string)
         Protocol[protocolCount].usesMQTT = false;
         Protocol[protocolCount].usesAccount = false;
         Protocol[protocolCount].usesPassword = true;
-        Protocol[protocolCount].defaultPort = 8080;
+        Protocol[protocolCount].defaultPort = 80;
         Protocol[protocolCount].usesID = true;
         break;
       }
@@ -59,6 +59,7 @@ boolean CPlugin_012(byte function, struct EventStruct *event, String& string)
             break;
 
           case SENSOR_TYPE_SWITCH:
+            success = CPlugin_012_send(event, 1);
             break;
         }
         break;
