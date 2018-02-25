d9192481c363b14eb186834898d9fd437d5d1aba
s0170071 <drk@posteo.de>
2018-02-25 15:16:20 +0100

    Webserver error page @<3k RAM

--- src/Misc.ino
+++ src/Misc.ino
@@ -14,12 +14,12 @@ extern "C" void tcp_abort (struct tcp_pcb* pcb);
  
 void tcpCleanup()   
 {
-   /*
+  
      while(tcp_tw_pcbs!=NULL)
     {
       tcp_abort(tcp_tw_pcbs);
     }
-    */
+    
  }
 #endif
 
