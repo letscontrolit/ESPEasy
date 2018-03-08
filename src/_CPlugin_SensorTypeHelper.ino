5821b0709c70fb4ea2f97999d0693634e3c3626c
Unknown <gijs.noorlander@gmail.com>
2018-03-09 00:29:52 +0100

    [issue #1015] Add other simple conversions like Celsius to Fahrenheit, etc.
    - %c_w_dir%   -  getBearing 
    - %c_c2f%   -    String(CelsiusToFahrenheit) 
    - %c_ms2Bft%   - String(m_secToBeaufort) 
    - %c_cm2imp%   - centimeterToImperialLength 
    - %c_mm2imp%   - millimeterToImperialLength 
    - %c_m2day%   -  String(minutesToDay) 
    - %c_m2dh%   -   minutesToDayHour 
    - %c_m2dhm%   -  minutesToDayHourMinute 
    - %c_s2dhms%   - secondsToDayHourMinuteSecond

--- src/_CPlugin_SensorTypeHelper.ino
+++ src/_CPlugin_SensorTypeHelper.ino
@@ -26,37 +26,3 @@ byte getValueCountFromSensorType(byte sensorType)
   addLog(LOG_LEVEL_ERROR, F("getValueCountFromSensorType: Unknown sensortype"));
   return 0;
 }
-
-/*********************************************************************************************\
-   Convert bearing in degree to bearing string
-  \*********************************************************************************************/
-
-String getBearing(int degrees)
-{
-  const __FlashStringHelper* bearing[] = {
-    F("N"),
-    F("NNE"),
-    F("NE"),
-    F("ENE"),
-    F("E"),
-    F("ESE"),
-    F("SE"),
-    F("SSE"),
-    F("S"),
-    F("SSW"),
-    F("SW"),
-    F("WSW"),
-    F("W"),
-    F("WNW"),
-    F("NW"),
-    F("NNW")
-  };
-  int nr_directions = (int) (sizeof(bearing)/sizeof(bearing[0]));
-  float stepsize = (360.0 / nr_directions);
-  if (degrees < 0) { degrees += 360; } // Allow for bearing -360 .. 359
-  int bearing_idx=int((degrees + (stepsize / 2.0)) / stepsize) % nr_directions;
-  if (bearing_idx < 0)
-    return("");
-  else
-    return(bearing[bearing_idx]);
-}
