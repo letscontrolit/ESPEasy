ec4aa627bb7fdb13f6bd18916104424e1a9eea26
TD-er <gijs.noorlander@gmail.com>
2018-08-19 23:52:09 +0200

    [MQTT] Use set controller settings on MQTT controllers

--- src/_C002.ino
+++ src/_C002.ino
@@ -33,6 +33,14 @@ boolean CPlugin_002(byte function, struct EventStruct *event, String& string)
         break;
       }
 
+    case CPLUGIN_INIT:
+      {
+        ControllerSettingsStruct ControllerSettings;
+        LoadControllerSettings(event->ControllerIndex, (byte*)&ControllerSettings, sizeof(ControllerSettings));
+        MQTTDelayHandler.configureControllerSettings(ControllerSettings);
+        break;
+      }
+
     case CPLUGIN_PROTOCOL_TEMPLATE:
       {
         event->String1 = F("domoticz/out");
