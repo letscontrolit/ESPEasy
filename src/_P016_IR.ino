d4ee74297b9dde4c53ad8ad0f6fd7c574870f08f
jimmys01 <jimdaskalakis01@msn.com>
2018-08-25 12:53:42 +0300

    IR RX updated
    The IRrecvDumpV2.ino functionality added to the RX module.
    Let me know for a good way to display the RAW values.

--- src/_P016_IR.ino
+++ src/_P016_IR.ino
@@ -9,6 +9,29 @@
 #endif
 
 #include <IRrecv.h>
+#include <IRutils.h>
+
+#if DECODE_DAIKIN
+#include <ir_Daikin.h>
+#endif
+#if DECODE_FUJITSU_AC
+#include <ir_Fujitsu.h>
+#endif
+#if DECODE_GREE
+#include <ir_Gree.h>
+#endif
+#if DECODE_HAIER_AC
+#include <ir_Haier.h>
+#endif
+#if DECODE_KELVINATOR
+#include <ir_Kelvinator.h>
+#endif
+#if DECODE_MIDEA
+#include <ir_Midea.h>
+#endif
+#if DECODE_TOSHIBA_AC
+#include <ir_Toshiba.h>
+#endif
 
 IRrecv *irReceiver;
 decode_results results;
@@ -17,6 +40,61 @@ decode_results results;
 #define PLUGIN_ID_016         16
 #define PLUGIN_NAME_016       "Communication - TSOP4838"
 #define PLUGIN_VALUENAME1_016 "IR"
+
+// ==================== start of TUNEABLE PARAMETERS ====================
+// As this program is a special purpose capture/decoder, let us use a larger
+// than normal buffer so we can handle Air Conditioner remote codes.
+#define CAPTURE_BUFFER_SIZE 1024
+
+// TIMEOUT is the Nr. of milli-Seconds of no-more-data before we consider a
+// message ended.
+// This parameter is an interesting trade-off. The longer the timeout, the more
+// complex a message it can capture. e.g. Some device protocols will send
+// multiple message packets in quick succession, like Air Conditioner remotes.
+// Air Coniditioner protocols often have a considerable gap (20-40+ms) between
+// packets.
+// The downside of a large timeout value is a lot of less complex protocols
+// send multiple messages when the remote's button is held down. The gap between
+// them is often also around 20+ms. This can result in the raw data be 2-3+
+// times larger than needed as it has captured 2-3+ messages in a single
+// capture. Setting a low timeout value can resolve this.
+// So, choosing the best TIMEOUT value for your use particular case is
+// quite nuanced. Good luck and happy hunting.
+// NOTE: Don't exceed MAX_TIMEOUT_MS. Typically 130ms.
+#if DECODE_AC
+#define TIMEOUT 50U  // Some A/C units have gaps in their protocols of ~40ms.
+// e.g. Kelvinator
+// A value this large may swallow repeats of some protocols
+#else  // DECODE_AC
+#define TIMEOUT 15U  // Suits most messages, while not swallowing many repeats.
+#endif  // DECODE_AC
+// Alternatives:
+// #define TIMEOUT 90U  // Suits messages with big gaps like XMP-1 & some aircon
+// units, but can accidentally swallow repeated messages
+// in the rawData[] output.
+// #define TIMEOUT MAX_TIMEOUT_MS  // This will set it to our currently allowed
+// maximum. Values this high are problematic
+// because it is roughly the typical boundary
+// where most messages repeat.
+// e.g. It will stop decoding a message and
+//   start sending it to serial at precisely
+//   the time when the next message is likely
+//   to be transmitted, and may miss it.
+
+// Set the smallest sized "UNKNOWN" message packets we actually care about.
+// This value helps reduce the false-positive detection rate of IR background
+// noise as real messages. The chances of background IR noise getting detected
+// as a message increases with the length of the TIMEOUT value. (See above)
+// The downside of setting this message too large is you can miss some valid
+// short messages for protocols that this library doesn't yet decode.
+//
+// Set higher if you get lots of random short UNKNOWN messages when nothing
+// should be sending a message.
+// Set lower if you are sure your setup is working, but it doesn't see messages
+// from your device. (e.g. Other IR remotes work.)
+// NOTE: Set this value very high to effectively turn off UNKNOWN detection.
+#define MIN_UNKNOWN_SIZE 12
+// ==================== end of TUNEABLE PARAMETERS ====================
 
 boolean Plugin_016(byte function, struct EventStruct *event, String& string)
 {
@@ -58,7 +136,8 @@ boolean Plugin_016(byte function, struct EventStruct *event, String& string)
         if (irReceiver == 0 && irPin != -1)
         {
           Serial.println(F("IR Init"));
-          irReceiver= new IRrecv(irPin);
+          irReceiver = new IRrecv(irPin, CAPTURE_BUFFER_SIZE, TIMEOUT, true);
+          irReceiver->setUnknownThreshold(MIN_UNKNOWN_SIZE); // Ignore messages with less than minimum on or off pulses.
           irReceiver->enableIRIn(); // Start the receiver
         }
         if (irReceiver != 0 && irPin == -1)
@@ -66,7 +145,7 @@ boolean Plugin_016(byte function, struct EventStruct *event, String& string)
           Serial.println(F("IR Removed"));
           irReceiver->disableIRIn();
           delete irReceiver;
-          irReceiver=0;
+          irReceiver = 0;
         }
         success = true;
         break;
@@ -76,18 +155,75 @@ boolean Plugin_016(byte function, struct EventStruct *event, String& string)
       {
         if (irReceiver->decode(&results))
         {
-          unsigned long IRcode = results.value;
           irReceiver->resume();
-          UserVar[event->BaseVarIndex] = (IRcode & 0xFFFF);
-          UserVar[event->BaseVarIndex + 1] = ((IRcode >> 16) & 0xFFFF);
-          String log = F("IR   : Code ");
-          log += String(IRcode, HEX);
-          log += F(" - Type: ");
-          log += results.decode_type;
-          log += F(" - Bits: ");
-          log += results.bits;
+          String log = "IR: ";
+          if (results.overflow)
+            log += F("WARNING: IR code is too big for buffer. This result shouldn't be trusted until this is resolved. Edit & increase CAPTURE_BUFFER_SIZE.\n");
+          // Display the basic output of what we found.
+          log += resultToHumanReadableBasic(&results);
+          addLog(LOG_LEVEL_INFO, log);
+          // Display any extra A/C info if we have it.
+          // Display the human readable state of an A/C message if we can.
+          log = "";
+#if DECODE_DAIKIN
+          if (results.decode_type == DAIKIN) {
+            IRDaikinESP ac(0);
+            ac.setRaw(results.state);
+            log = ac.toString();
+          }
+#endif  // DECODE_DAIKIN
+#if DECODE_FUJITSU_AC
+          if (results.decode_type == FUJITSU_AC) {
+            IRFujitsuAC ac(0);
+            ac.setRaw(results.state, results.bits / 8);
+            log = ac.toString();
+          }
+#endif  // DECODE_FUJITSU_AC
+#if DECODE_KELVINATOR
+          if (results.decode_type == KELVINATOR) {
+            IRKelvinatorAC ac(0);
+            ac.setRaw(results.state);
+            log = ac.toString();
+          }
+#endif  // DECODE_KELVINATOR
+#if DECODE_TOSHIBA_AC
+          if (results.decode_type == TOSHIBA_AC) {
+            IRToshibaAC ac(0);
+            ac.setRaw(results.state);
+            log = ac.toString();
+          }
+#endif  // DECODE_TOSHIBA_AC
+#if DECODE_GREE
+          if (results.decode_type == GREE) {
+            IRGreeAC ac(0);
+            ac.setRaw(results.state);
+            log = ac.toString();
+          }
+#endif  // DECODE_GREE
+#if DECODE_MIDEA
+          if (results.decode_type == MIDEA) {
+            IRMideaAC ac(0);
+            ac.setRaw(results.value);  // Midea uses value instead of state.
+            log = ac.toString();
+          }
+#endif  // DECODE_MIDEA
+#if DECODE_HAIER_AC
+          if (results.decode_type == HAIER_AC) {
+            IRHaierAC ac(0);
+            ac.setRaw(results.state);
+            log = ac.toString();
+          }
+#endif  // DECODE_HAIER_AC
+          // If we got a human-readable description of the message, display it.
+          if (log != "")  log += ("Mesg Desc.: " + log);
+
+          // Output RAW timing info of the result.
+          //log += resultToTimingInfo(&results);  //not showing up nicely in the web log... Maybe send them to serial?
+          // Output the results as source code
+          //  log += resultToSourceCode(&results); //not showing up nicely in the web log... Maybe send them to serial?
           addLog(LOG_LEVEL_INFO, log);
           sendData(event);
+
         }
         success = true;
         break;
