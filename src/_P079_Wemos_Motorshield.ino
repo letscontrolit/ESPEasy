b810e34b74d287e0278a8dc09dc6a8d5e46a06ee
TD-er <gijs.noorlander@gmail.com>
2018-08-25 22:39:36 +0200

    [MotorShield] Added comments in the code and fixes
    Frequency was not using the upper 4 bits of the values.
    Changed delay to yield.

--- src/_P079_Wemos_Motorshield.ino
+++ src/_P079_Wemos_Motorshield.ino
@@ -9,6 +9,11 @@
 // based on this library: https://github.com/wemos/WEMOS_Motor_Shield_Arduino_Library
 // Plugin part written by Susanne Jaeckel + TungstenE2
 
+// N.B. this shield does only accept data transmissions in sets of 4 bytes.
+// This means a simple I2C scan may mess up the module (and all other I2C communications)
+// until a full reset of the node.
+// See: https://github.com/wemos/Motor_Shield_Firmware/issues/1#issuecomment-394475451
+
 #define PLUGIN_079
 #define PLUGIN_ID_079         79
 #define PLUGIN_NAME_079       "Motor - Wemos Motorshield [TESTING]"
@@ -89,16 +94,16 @@ boolean Plugin_079(byte function, struct EventStruct *event, String& string)
 	}
 
 	case PLUGIN_WEBFORM_LOAD: {
-		addFormTextBox(F("I2C Address (Hex)"), F("plugin_079_adr"), String(F("0x")) +
-			       String(Settings.TaskDevicePluginConfig[event->TaskIndex][0], HEX), 4);
+    String i2c_addres_string = formatToHex(Settings.TaskDevicePluginConfig[event->TaskIndex][0]);
+		addFormTextBox(F("I2C Address (Hex)"), F("plugin_079_adr"), i2c_addres_string, 4);
 
 		success = true;
 		break;
 	}
 
 	case PLUGIN_WEBFORM_SAVE: {
-		String plugin1 = WebServer.arg(F("plugin_079_adr"));
-		Settings.TaskDevicePluginConfig[event->TaskIndex][0] = (int)strtol(plugin1.c_str(), 0, 16);
+		String i2c_address = WebServer.arg(F("plugin_079_adr"));
+		Settings.TaskDevicePluginConfig[event->TaskIndex][0] = (int)strtol(i2c_address.c_str(), 0, 16);
 
 		success = true;
 		break;
@@ -125,7 +130,7 @@ boolean Plugin_079(byte function, struct EventStruct *event, String& string)
 		// MotorShieldCMD,<DCMotor>,<Motornumber>,<Forward/Backward/Stop>,<Speed>
 
 		if (cmd.equalsIgnoreCase(F("WemosMotorShieldCMD"))) {
-			String paramMotor = parseString(tmpString, 2); // Motor 0 oder 1
+			String paramMotor = parseString(tmpString, 2); // Motor 0 or 1
 			String paramDirection = parseString(tmpString, 3); // Direction
 			String paramSpeed = parseString(tmpString, 4); // Speed
 
@@ -166,7 +171,7 @@ WemosMotor::WemosMotor(uint8_t address, uint8_t motor, uint32_t freq)
 	else
 		_motor = _MOTOR_B;
 
-	Wire.begin();
+	//Wire.begin();   called in ESPEasy framework
 
 	_address = address;
 
@@ -184,7 +189,7 @@ WemosMotor::WemosMotor(uint8_t address, uint8_t motor, uint32_t freq, uint8_t ST
 	else
 		_motor = _MOTOR_B;
 
-	Wire.begin();
+	//Wire.begin();   called in ESPEasy framework
 
 	_address = address;
 
@@ -200,16 +205,21 @@ WemosMotor::WemosMotor(uint8_t address, uint8_t motor, uint32_t freq, uint8_t ST
    freq:
         PWM's frequency
 
+   total 4bytes
+     |0.5byte CMD       | 3.5byte Parm|
+     |CMD								| parm        |
+     |0x0X  set freq  	| uint32  freq|
+
  */
 void WemosMotor::setfreq(uint32_t freq)
 {
 	Wire.beginTransmission(_address);
-	Wire.write(((byte)(freq >> 16)) & (byte)0x0f);
+	Wire.write(((byte)(freq >> 24)) & (byte)0x0f);
 	Wire.write((byte)(freq >> 16));
 	Wire.write((byte)(freq >> 8));
 	Wire.write((byte)freq);
 	Wire.endTransmission();     // stop transmitting
-	delay(100);
+	yield();
 }
 
 /* setmotor() -- set motor
@@ -228,6 +238,12 @@ void WemosMotor::setfreq(uint32_t freq)
    pwm_val:
         0.00 - 100.00  (%)
 
+   total 4bytes
+    |0.5byte CMD      | 3.5byte Parm         |
+    |CMD              | parm                 |
+    |0x10  set motorA | uint8 dir  uint16 pwm|
+    |0x11  set motorB | uint8 dir  uint16 pwm|
+
  */
 void WemosMotor::setmotor(uint8_t dir, float pwm_val)
 {
@@ -242,19 +258,20 @@ void WemosMotor::setmotor(uint8_t dir, float pwm_val)
 	}
 
 	Wire.beginTransmission(_address);
-	Wire.write(_motor | (byte)0x10);
+	Wire.write(_motor | (byte)0x10);  // CMD either 0x10 or 0x11
 	Wire.write(dir);
 
+  // PWM in %
 	_pwm_val = uint16_t(pwm_val * 100);
 
-	if (_pwm_val > 10000)
+	if (_pwm_val > 10000) // _pwm_val > 100.00
 		_pwm_val = 10000;
 
 	Wire.write((byte)(_pwm_val >> 8));
 	Wire.write((byte)_pwm_val);
 	Wire.endTransmission();     // stop transmitting
 
-	delay(100);
+	yield();
 }
 
 void WemosMotor::setmotor(uint8_t dir)
