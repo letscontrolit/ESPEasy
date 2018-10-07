f787a9a9a603b8eee6bdbfac40f4cf9443cc84d9
Grovkillen <jimmy.westberg@gmail.com>
2018-10-07 19:11:49 +0200

    [Sphinx] added extension "sphinx.ext.imgconverter"

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.imgmath',
+    'sphinx.ext.imgconverter',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
