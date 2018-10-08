dececf24ee362685c8a39a7986ce6e90721f6b80
TD-er <gijs.noorlander@gmail.com>
2018-10-08 15:31:48 +0200

    [Docs] Add favicon to documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -82,6 +82,8 @@ pygments_style = None
 
 # -- Options for HTML output -------------------------------------------------
 html_logo = 'ESPEasy/espeasy_logo.png'
+html_favicon = 'ESPEasy/favicon.ico'
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
