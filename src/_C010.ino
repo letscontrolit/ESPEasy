ca83983fd27d63c7396abe2334f45a83cd1c530e
TD-er <gijs.noorlander@gmail.com>
2018-10-12 01:04:46 +0200

    [#1891] Generic UPD controller - send only first value from sensor
    See #1891

--- src/_C010.ino
+++ src/_C010.ino
@@ -85,13 +85,19 @@ bool do_process_c010_delay_queue(int controller_number, const C010_queue_element
     if (element.checkDone(true))
       return true;
   }
-
-  if (!try_connect_host(controller_number, portUDP, ControllerSettings))
+  WiFiUDP C010_portUDP;
+  if (!beginWiFiUDP_randomPort(C010_portUDP)) return false;
+  if (!try_connect_host(controller_number, C010_portUDP, ControllerSettings))
     return false;
 
-  portUDP.write(
+  C010_portUDP.write(
     (uint8_t*)element.txt[element.valuesSent].c_str(),
               element.txt[element.valuesSent].length());
-  return element.checkDone(portUDP.endPacket() != 0);
+  C010_portUDP.endPacket();
+  C010_portUDP.stop();
+  // FIXME TD-er:
+  // WiFiUDP endPacket() doesn't return a value like described in the documentation,
+  // so we now consider it to be successful
+  return element.checkDone(true);
 }
 #endif
