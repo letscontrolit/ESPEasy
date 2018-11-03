dee5f6df1a5973c9e69e5b157ababc00c8836e57
TD-er <gijs.noorlander@gmail.com>
2018-11-03 22:40:57 +0100

    [Cleanup] Strip HTML templates into sections to remove duplicate strings

--- src/WebServer.ino
+++ src/WebServer.ino
@@ -542,70 +542,30 @@ void setWebserverRunning(bool state) {
 
 void getWebPageTemplateDefault(const String& tmplName, String& tmpl)
 {
+  const bool addJS = true;
+  const bool addMeta = true;
   if (tmplName == F("TmplAP"))
   {
-    tmpl += F(
-              "<!DOCTYPE html><html lang='en'>"
-              "<head>"
-              "<meta charset='utf-8'/>"
-              "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
-              "<title>{{name}}</title>"
-              "{{css}}"
-              "</head>"
-              "<body>"
+    getWebPageTemplateDefaultHead(tmpl, !addMeta, !addJS);
+    tmpl += F("<body>"
               "<header class='apheader'>"
               "<h1>Welcome to ESP Easy Mega AP</h1>"
-              "</header>"
-              "<section>"
-              "<span class='message error'>"
-              "{{error}}"
-              "</span>"
-              "{{content}}"
-              "</section>"
-              "<footer>"
-                "<br>"
-                "<h6>Powered by <a href='http://www.letscontrolit.com' style='font-size: 15px; text-decoration: none'>www.letscontrolit.com</a></h6>"
-              "</footer>"
-              "</body>"            );
+              "</header>");
+    getWebPageTemplateDefaultContentSection(tmpl);
+    getWebPageTemplateDefaultFooter(tmpl);
   }
   else if (tmplName == F("TmplMsg"))
   {
-    tmpl += F(
-              "<!DOCTYPE html><html lang='en'>"
-              "<head>"
-              "<meta charset='utf-8'/>"
-              "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
-              "<title>{{name}}</title>"
-              "{{css}}"
-              "</head>"
-              "<body>"
-              "<header class='headermenu'>"
-              "<h1>ESP Easy Mega: {{name}}</h1><BR>"
-              "</header>"
-              "<section>"
-              "<span class='message error'>"
-              "{{error}}"
-              "</span>"
-              "{{content}}"
-              "</section>"
-              "<footer>"
-                "<br>"
-                "<h6>Powered by <a href='http://www.letscontrolit.com' style='font-size: 15px; text-decoration: none'>www.letscontrolit.com</a></h6>"
-              "</footer>"
-              "</body>"
-            );
+    getWebPageTemplateDefaultHead(tmpl, !addMeta, !addJS);
+    tmpl += F("<body>");
+    getWebPageTemplateDefaultHeader(tmpl, F("{{name}}"), false);
+    getWebPageTemplateDefaultContentSection(tmpl);
+    getWebPageTemplateDefaultFooter(tmpl);
   }
   else if (tmplName == F("TmplDsh"))
   {
+    getWebPageTemplateDefaultHead(tmpl, !addMeta, addJS);
     tmpl += F(
-      "<!DOCTYPE html><html lang='en'>"
-      "<head>"
-        "<meta charset='utf-8'/>"
-        "<title>{{name}}</title>"
-        "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
-        "{{js}}"
-        "{{css}}"
-        "</head>"
         "<body>"
         "{{content}}"
         "</body></html>"
@@ -613,37 +573,55 @@ void getWebPageTemplateDefault(const String& tmplName, String& tmpl)
   }
   else   //all other template names e.g. TmplStd
   {
-    tmpl += F(
-      "<!DOCTYPE html><html lang='en'>"
-      "<head>"
-        "<meta charset='utf-8'/>"
-        "<title>{{name}}</title>"
-        "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
-        "{{meta}}"
-        "{{js}}"
-        "{{css}}"
-      "</head>"
-      "<body class='bodymenu'>"
-        "<span class='message' id='rbtmsg'></span>"
-        "<header class='headermenu'>"
-          "<h1>ESP Easy Mega: {{name}} {{logo}}</h1><BR>"
-          "{{menu}}"
-        "</header>"
-        "<section>"
-        "<span class='message error'>"
-        "{{error}}"
-        "</span>"
-        "{{content}}"
-        "</section>"
-        "<footer>"
-          "<br>"
-          "<h6>Powered by <a href='http://www.letscontrolit.com' style='font-size: 15px; text-decoration: none'>www.letscontrolit.com</a></h6>"
-        "</footer>"
-      "</body></html>"
-            );
+    getWebPageTemplateDefaultHead(tmpl, addMeta, addJS);
+    tmpl += F("<body class='bodymenu'>"
+              "<span class='message' id='rbtmsg'></span>");
+    getWebPageTemplateDefaultHeader(tmpl, F("{{name}} {{logo}}"), true);
+    getWebPageTemplateDefaultContentSection(tmpl);
+    getWebPageTemplateDefaultFooter(tmpl);
   }
 }
 
+void getWebPageTemplateDefaultHead(String& tmpl, bool addMeta, bool addJS) {
+  tmpl += F("<!DOCTYPE html><html lang='en'>"
+              "<head>"
+                "<meta charset='utf-8'/>"
+                "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
+                "<title>{{name}}</title>");
+  if (addMeta) tmpl += F("{{meta}}");
+  if (addJS) tmpl += F("{{js}}");
+
+  tmpl += F("{{css}}"
+            "</head>");
+}
+
+void getWebPageTemplateDefaultHeader(String& tmpl, const String& title, bool addMenu) {
+  tmpl += F("<header class='headermenu'>"
+            "<h1>ESP Easy Mega: ");
+  tmpl += title;
+  tmpl += F("</h1><BR>");
+  if (addMenu) tmpl += F("{{menu}}");
+  tmpl += F("</header>");
+}
+
+void getWebPageTemplateDefaultContentSection(String& tmpl) {
+  tmpl += F("<section>"
+              "<span class='message error'>"
+                "{{error}}"
+              "</span>"
+              "{{content}}"
+            "</section>"
+          );
+}
+
+void getWebPageTemplateDefaultFooter(String& tmpl) {
+  tmpl += F("<footer>"
+              "<br>"
+              "<h6>Powered by <a href='http://www.letscontrolit.com' style='font-size: 15px; text-decoration: none'>www.letscontrolit.com</a></h6>"
+            "</footer>"
+            "</body></html>"
+          );
+}
 
 void getErrorNotifications() {
   // Check number of MQTT controllers active.
