a3aa944988f2c675ee6f3bd609c89655ecd3a204
Saverio Cisternino <workgroupengineering@outlook.com>
2018-11-28 12:35:01 +0100

    [Embed Files] Append zero termination

--- embed_files.sh
+++ embed_files.sh
@@ -49,7 +49,7 @@ function ascii2hexCstyle {
 	file_name=$(constFileName $1)
 	result=$(cat /tmp/converter.temp | hexdump -ve '1/1 "0x%.2x,"')
 	result=$(echo $result | sed 's/,$//')
-	echo "const char DATA_${file_name}[] PROGMEM = {$result};"
+	echo "const char DATA_${file_name}[] PROGMEM = {$result,0};"
 }
 
 function constFileName {
