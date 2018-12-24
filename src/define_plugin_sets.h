a5aca5c5df53b567e3f175731a107614ab52f89a
Patrick Ecord <pecord@gmail.com>
2018-12-23 23:12:22 -0600

    pulled from the mq2 pr I found - https://github.com/letscontrolit/ESPEasy/pull/96 ended up pulling the values used for ppm from https://github.com/mdsiraj1992/Gassensors/blob/master/arduino%20codes/mq5_sensor/mq5_sensor.ino

--- src/define_plugin_sets.h
+++ src/define_plugin_sets.h
@@ -553,6 +553,7 @@ To create/register a plugin, you have to :
     #define USES_P079   // Wemos Motoshield
     #define USES_P080   // iButton Sensor  DS1990A
     #define USES_P081   // Cron
+    #define USES_P082   // MQ-5 gas sensor
 #endif
 
 
