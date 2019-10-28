9712a104d5f9daf6dacfb0bb40b00fb434c11963
Jean-Michel DECORET <decoretjm@gmail.com>
2019-10-28 12:58:04 +0100

    Change plugin number (242 -> 090)
    Update configuration behavior (for default values)

--- src/define_plugin_sets.h
+++ src/define_plugin_sets.h
@@ -674,7 +674,7 @@ To create/register a plugin, you have to :
     #define USES_P086   // Receiving values according Homie convention. Works together with C014 Homie controller
     //#define USES_P087   // Serial Proxy
     #define USES_P089   // Serial Proxy
-	#define USES_P242	// TFT
+	#define USES_P090	// TFT
 
 #endif
 
