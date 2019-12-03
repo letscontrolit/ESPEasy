d4c6c05e3f771c88a46c31ff1130fd4ca3956c2e
Gijs Noorlander <gijs.noorlander@gmail.com>
2019-12-03 17:25:45 +0100

    [PIO] Don't use Python 3 specific since Travis still uses Python 2.7

--- pre_default_check.py
+++ pre_default_check.py
@@ -14,13 +14,13 @@ my_flags = env.ParseFlags(env['BUILD_FLAGS'])
 my_defines = my_flags.get("CPPDEFINES")
 #defines = {k: v for (k, v) in my_defines}
 
-print("\u001b[32m Default PIO configuration check \u001b[0m", flush=True)
+print("\u001b[32m Default PIO configuration check \u001b[0m")
 # print the defines
-print("\u001b[33m CPPDEFINES: \u001b[0m  {}".format(my_defines), flush=True)
-print("\u001b[32m ------------------------------- \u001b[0m", flush=True)
+print("\u001b[33m CPPDEFINES: \u001b[0m  {}".format(my_defines))
+print("\u001b[32m ------------------------------- \u001b[0m")
 
 
 if (len(my_defines) == 0):
-  print("\u001b[31m No defines are set, probably configuration error. \u001b[0m", flush=True)
+  print("\u001b[31m No defines are set, probably configuration error. \u001b[0m")
   raise ValueError
 
