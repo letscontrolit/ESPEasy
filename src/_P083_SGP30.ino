228d940cb6c6bc704721d50e654beff47ca85d92
Gijs Noorlander <gijs.noorlander@gmail.com>
2020-02-22 19:34:50 +0100

    [P083 SGP30] Read SGP30 sensor according to specs
    The sensor was performing IAQ init for every read, while it should only be done at init of the sensor.
    The IAQ measurement should be performed every second.

--- src/_P083_SGP30.ino
+++ src/_P083_SGP30.ino
@@ -1,9 +1,10 @@
 #ifdef USES_P083
+
 /*********************************************************************************************\
- * SGP30 - Gas (TVOC - Total Volatile Organic Compounds) and Air Quality (eCO2)
- *
- *
- * I2C Address: 0x58
+* SGP30 - Gas (TVOC - Total Volatile Organic Compounds) and Air Quality (eCO2)
+*
+*
+* I2C Address: 0x58
 \*********************************************************************************************/
 #include "Adafruit_SGP30.h"
 
@@ -14,81 +15,113 @@
 #define PLUGIN_VALUENAME1_083 "TVOC"
 #define PLUGIN_VALUENAME2_083 "eCO2"
 
+Adafruit_SGP30 sgp;
+bool Plugin_083_init      = false;
+bool Plugin_083_newValues = false;
+
 boolean Plugin_083(byte function, struct EventStruct *event, String& string)
 {
   boolean success = false;
-  switch (function)
-    {
-      case PLUGIN_DEVICE_ADD:
-        {
-          Device[++deviceCount].Number = PLUGIN_ID_083;
-          Device[deviceCount].Type = DEVICE_TYPE_I2C;
-          Device[deviceCount].VType = SENSOR_TYPE_SINGLE;
-          Device[deviceCount].Ports = 0;
-          Device[deviceCount].PullUpOption = false;
-          Device[deviceCount].InverseLogicOption = false;
-          Device[deviceCount].FormulaOption = true;
-          Device[deviceCount].ValueCount = 2;
-          Device[deviceCount].SendDataOption = true;
-          Device[deviceCount].TimerOption = true;
-          Device[deviceCount].GlobalSyncOption = true;
-          break;
-        }
-  
-      case PLUGIN_GET_DEVICENAME:
-        {
-          string = F(PLUGIN_NAME_083);
-          break;
-        }
-  
-      case PLUGIN_GET_DEVICEVALUENAMES:
-        {
-          strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[0], PSTR(PLUGIN_VALUENAME1_083));
-          strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[1], PSTR(PLUGIN_VALUENAME2_083));
-          break;
-        }
-  
-      case PLUGIN_WEBFORM_LOAD:
-        {
-          success = true;
-          break;
-        }
-  
-      case PLUGIN_WEBFORM_SAVE:
-        {
-          success = true;
-          break;
-        }
-  
-      case PLUGIN_READ:
-        {
-          Adafruit_SGP30 sgp;
-          if (sgp.begin()) 
-          {
-              if (sgp.IAQmeasure()) 
-              {
-                  UserVar[event->BaseVarIndex] = sgp.TVOC;
-                  UserVar[event->BaseVarIndex + 1] = sgp.eCO2;
-                  String log = F("SGP30: TVOC: ");
-                  log += UserVar[event->BaseVarIndex];
-                  addLog(LOG_LEVEL_INFO, log);
-                  log = F("SGP30: eCO2: ");
-                  log += UserVar[event->BaseVarIndex + 1];
-                  addLog(LOG_LEVEL_INFO, log);
-                  success = true;
-                  break;
-               }else{
-                  addLog(LOG_LEVEL_ERROR, F("SGP30: Measurement failed"))
-                  break;
-               }
-           }else{
-              addLog(LOG_LEVEL_ERROR, F("SGP30: Sensor not found"))
-              break;
-           } 
-        }
-    }
-    return success;
-}
 
+  switch (function)
+  {
+    case PLUGIN_DEVICE_ADD:
+    {
+      Device[++deviceCount].Number           = PLUGIN_ID_083;
+      Device[deviceCount].Type               = DEVICE_TYPE_I2C;
+      Device[deviceCount].VType              = SENSOR_TYPE_SINGLE;
+      Device[deviceCount].Ports              = 0;
+      Device[deviceCount].PullUpOption       = false;
+      Device[deviceCount].InverseLogicOption = false;
+      Device[deviceCount].FormulaOption      = true;
+      Device[deviceCount].ValueCount         = 2;
+      Device[deviceCount].SendDataOption     = true;
+      Device[deviceCount].TimerOption        = true;
+      Device[deviceCount].GlobalSyncOption   = true;
+      break;
+    }
+
+    case PLUGIN_GET_DEVICENAME:
+    {
+      string = F(PLUGIN_NAME_083);
+      break;
+    }
+
+    case PLUGIN_GET_DEVICEVALUENAMES:
+    {
+      strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[0], PSTR(PLUGIN_VALUENAME1_083));
+      strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[1], PSTR(PLUGIN_VALUENAME2_083));
+      break;
+    }
+
+    case PLUGIN_WEBFORM_LOAD:
+    {
+      success = true;
+      break;
+    }
+
+    case PLUGIN_WEBFORM_SAVE:
+    {
+      success = true;
+      break;
+    }
+
+    case PLUGIN_INIT:
+    {
+      Plugin_083_init = sgp.begin();
+
+      if (!Plugin_083_init) {
+        addLog(LOG_LEVEL_ERROR, F("SGP30: Sensor not found"))
+      }
+
+      success = Plugin_083_init;
+      break;
+    }
+
+    case PLUGIN_ONCE_A_SECOND:
+    {
+      if (Plugin_083_init)
+      {
+        if (sgp.IAQmeasure())
+        {
+          success              = true;
+          Plugin_083_newValues = true;
+        }
+      }
+      break;
+    }
+
+    case PLUGIN_READ:
+    {
+      if (Plugin_083_init)
+      {
+        if (Plugin_083_newValues)
+        {
+          Plugin_083_newValues             = false;
+          UserVar[event->BaseVarIndex]     = sgp.TVOC;
+          UserVar[event->BaseVarIndex + 1] = sgp.eCO2;
+
+          if (loglevelActiveFor(LOG_LEVEL_INFO)) {
+            String log = F("SGP30: TVOC: ");
+            log += UserVar[event->BaseVarIndex];
+            addLog(LOG_LEVEL_INFO, log);
+            log  = F("SGP30: eCO2: ");
+            log += UserVar[event->BaseVarIndex + 1];
+            addLog(LOG_LEVEL_INFO, log);
+          }
+          success = true;
+          break;
+        } else {
+          addLog(LOG_LEVEL_ERROR, F("SGP30: Measurement failed"))
+          break;
+        }
+      } else {
+        addLog(LOG_LEVEL_ERROR, F("SGP30: Sensor not found"))
+        break;
+      }
+    }
+  }
+  return success;
+}
 
 #endif // USES_P083
