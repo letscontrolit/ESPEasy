c3f00a6f7218ea17d77f8ac0added0e748d3efa9
Gijs Noorlander <gijs.noorlander@gmail.com>
2020-06-08 16:47:51 +0200

    [Commands] Add event value source check for commands

--- src/src/DataStructs/EventValueSource.h
+++ src/src/DataStructs/EventValueSource.h
@@ -1,9 +1,16 @@
 #ifndef DATASTRUCTS_EVENT_VALUE_SOURCE_H
 #define DATASTRUCTS_EVENT_VALUE_SOURCE_H
 
-class EventValueSource {
-public:
 
+struct EventValueSourceGroup {
+  enum class Enum : byte {
+    RESTRICTED,
+    ALL
+  };
+};
+
+
+struct EventValueSource {
   // Keep the values as they can be used by other/older builds to communicate with ESPEasy
   enum class Enum : byte {
     VALUE_SOURCE_NOT_SET      = 0,
@@ -17,6 +24,26 @@ public:
 
     VALUE_SOURCE_NR_VALUES
   };
+
+  static bool partOfGroup(EventValueSource::Enum source, EventValueSourceGroup::Enum group)
+  {
+    switch (source) {
+      case EventValueSource::Enum::VALUE_SOURCE_NOT_SET:
+      case EventValueSource::Enum::VALUE_SOURCE_NR_VALUES:
+        return false;
+      case EventValueSource::Enum::VALUE_SOURCE_SYSTEM:
+      case EventValueSource::Enum::VALUE_SOURCE_SERIAL:
+      case EventValueSource::Enum::VALUE_SOURCE_UDP:
+      case EventValueSource::Enum::VALUE_SOURCE_WEB_FRONTEND:
+      case EventValueSource::Enum::VALUE_SOURCE_RULES:
+        return true;
+      case EventValueSource::Enum::VALUE_SOURCE_HTTP:
+      case EventValueSource::Enum::VALUE_SOURCE_MQTT:
+        return group == EventValueSourceGroup::Enum::ALL;
+    }
+    return false;
+  }
 };
+
 
 #endif // DATASTRUCTS_EVENT_VALUE_SOURCE_H
