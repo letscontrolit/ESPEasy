269eea6d4ced434568a056a7a200ca87ae4a0879
Ton Huisman <tonhu2004@hotmail.com>
2020-09-13 22:23:51 +0200

    [P099] Cut off x/y coordinate outside calibration

--- src/src/PluginStructs/P099_data_struct.cpp
+++ src/src/PluginStructs/P099_data_struct.cpp
@@ -185,6 +185,8 @@ void P099_data_struct::scaleRawToCalibrated(uint16_t &x, uint16_t &y) {
     uint16_t _x = x - StoredSettings.Calibration.top_left.x;
     if (_x <= 0) {
       x = 0;
+    } else if (_x > StoredSettings.Calibration.bottom_right.x) {
+      _x = StoredSettings.Calibration.bottom_right.x;
     } else {
       float x_fact = (StoredSettings.Calibration.bottom_right.x - StoredSettings.Calibration.top_left.x) / ts_x_res;
       x = int((_x * 1.0f) / x_fact);
@@ -192,6 +194,8 @@ void P099_data_struct::scaleRawToCalibrated(uint16_t &x, uint16_t &y) {
     uint16_t _y = y - StoredSettings.Calibration.top_left.y;
     if (_y <= 0) {
       y = 0;
+    } else if (_y > StoredSettings.Calibration.bottom_right.y) {
+      _y = StoredSettings.Calibration.bottom_right.y;
     } else {
       float y_fact = (StoredSettings.Calibration.bottom_right.y - StoredSettings.Calibration.top_left.y) / ts_y_res;
       y = int((_y * 1.0f) / y_fact);
