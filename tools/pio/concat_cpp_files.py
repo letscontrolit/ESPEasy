143317afce9963522ea6820a6a88c47ec3986a9a
TD-er <gijs.noorlander@gmail.com>
2020-12-01 17:05:39 +0100

    [PIO] Concat *.cpp files in ControllerQueue/ and PluginStructs/

--- tools/pio/concat_cpp_files.py
+++ tools/pio/concat_cpp_files.py
@@ -3,30 +3,36 @@ import os
 import glob
 
 
-cpp_files = []
+def concat_cpp_files(path_to_concat):
 
-cpp_path = './src/src/PluginStructs'
+    cpp_files = []
 
-cpp_path_out = './src/src/PluginStructs_tmp'
+    cpp_path = path_to_concat
 
-if not os.path.exists(cpp_path_out):
-    os.makedirs(cpp_path_out)
+    cpp_path_out = '{}_tmp'.format(path_to_concat)
 
-tmp_cpp_file = os.path.join(cpp_path_out, '__tmpfile.cpp')
+    if not os.path.exists(cpp_path_out):
+        os.makedirs(cpp_path_out)
 
-if os.path.exists(tmp_cpp_file):
-    os.remove(tmp_cpp_file)
+    tmp_cpp_file = os.path.join(cpp_path_out, '__tmpfile.cpp')
 
-for root, dirs, files in os.walk(cpp_path):
-    for file in files:
-        if file.endswith('.cpp'):
-            fullname = os.path.join(root, file)
-            cpp_files.append(fullname)
-            print("Add {}".format(fullname))
+    if os.path.exists(tmp_cpp_file):
+        os.remove(tmp_cpp_file)
+
+    for root, dirs, files in os.walk(cpp_path):
+        for file in files:
+            if file.endswith('.cpp'):
+                fullname = os.path.join(root, file)
+                cpp_files.append(fullname)
+                print("Add {}".format(fullname))
 
 
-with open(tmp_cpp_file,'wb') as newf:
-    for filename in cpp_files:
-        with open(filename,'rb') as hf:
-            newf.write(hf.read())
-            #newf.write('\n\n\n')
+    with open(tmp_cpp_file,'wb') as newf:
+        for filename in cpp_files:
+            with open(filename,'rb') as hf:
+                newf.write(hf.read())
+                #newf.write('\n\n\n')
+
+
+concat_cpp_files('./src/src/ControllerQueue')
+concat_cpp_files('./src/src/PluginStructs')
