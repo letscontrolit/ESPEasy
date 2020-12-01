35f43cd3827545f7d5b6e56608e73fbb34a783e3
TD-er <gijs.noorlander@gmail.com>
2020-12-01 17:05:39 +0100

    [PIO] Concat *.cpp files in ControllerQueue/ and PluginStructs/

--- tools/pio/remove_concat_cpp_files.py
+++ tools/pio/remove_concat_cpp_files.py
@@ -3,11 +3,20 @@ import os
 
 
 
-cpp_path = './src/src/PluginStructs'
+def clear_concat_cpp_files(path_to_concat):
+    cpp_path_out = '{}_tmp'.format(path_to_concat)
+    tmp_cpp_file = os.path.join(cpp_path_out, '__tmpfile.cpp')
 
-tmp_cpp_file = os.path.join(cpp_path, '__tmpfile.cpp')
+    if os.path.exists(tmp_cpp_file):
+        os.remove(tmp_cpp_file)
 
-#if os.path.exists(tmp_cpp_file):
-#    os.remove(tmp_cpp_file)
+    if os.path.exists(cpp_path_out):
+        os.rmdir(cpp_path_out)
 
 
+def clear_all_concat_cpp_files(source, target, env):
+    clear_concat_cpp_files('./src/src/ControllerQueue')
+    clear_concat_cpp_files('./src/src/PluginStructs')
+
+
+env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", [clear_all_concat_cpp_files])
