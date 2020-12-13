9fb6927f6d90c73324009e82a75c94bb5a2cde08
TD-er <gijs.noorlander@gmail.com>
2020-12-13 23:28:11 +0100

    [BME680] Use the selected I2C address

--- src/_P106_BME680.ino
+++ src/_P106_BME680.ino
@@ -15,10 +15,7 @@
    /******************************************************************************/
 
 
-# include <Wire.h>
-# include <SPI.h>
-# include <Adafruit_Sensor.h>
-# include <Adafruit_BME680.h>
+# include "src/PluginStructs/P106_data_struct.h"
 
 
 # define PLUGIN_106
@@ -29,19 +26,6 @@
 # define PLUGIN_VALUENAME3_106 "Pressure"
 # define PLUGIN_VALUENAME4_106 "Gas"
 
-# define BME_SCK 13
-# define BME_MISO 12
-# define BME_MOSI 11
-# define BME_CS 10
-
-# define SEALEVELPRESSURE_HPA (1013.25)
-
-Adafruit_BME680 bme; // I2C
-// Adafruit_BME680 bme(BME_CS); // hardware SPI
-// Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO,  BME_SCK);
-
-
-boolean Plugin_106_init = false;
 
 boolean Plugin_106(byte function, struct EventStruct *event, String& string)
 {
@@ -82,7 +66,6 @@ boolean Plugin_106(byte function, struct EventStruct *event, String& string)
 
     case PLUGIN_WEBFORM_SHOW_I2C_PARAMS:
     {
-
       byte choice = PCONFIG(0);
 
       /*
@@ -109,41 +92,47 @@ boolean Plugin_106(byte function, struct EventStruct *event, String& string)
     {
       PCONFIG(0) = getFormItemInt(F("i2c_addr"));
       PCONFIG(1) = getFormItemInt(F("plugin_106_BME680_elev"));
-      success                                              = true;
+      success    = true;
+      break;
+    }
+
+    case PLUGIN_INIT:
+    {
+      initPluginTaskData(event->TaskIndex, new (std::nothrow) P106_data_struct());
+      P106_data_struct *P106_data =
+        static_cast<P106_data_struct *>(getPluginTaskData(event->TaskIndex));
+
+      if (nullptr != P106_data) {
+        P106_data->bme.begin(PCONFIG(0));
+        success = P106_data->initialized;
+      }
       break;
     }
 
     case PLUGIN_READ:
     {
-      if (!Plugin_106_init)
+      P106_data_struct *P106_data =
+        static_cast<P106_data_struct *>(getPluginTaskData(event->TaskIndex));
+
+      if (nullptr != P106_data)
       {
-        addLog(LOG_LEVEL_INFO, F("BME680  : init"));
+        P106_data->bme.begin(PCONFIG(0));
 
-        Plugin_106_init = bme.begin();
+        if (!P106_data->initialized) {
+          break;
+        }
 
-        // Set up oversampling and filter initialization
-        bme.setTemperatureOversampling(BME680_OS_8X);
-        bme.setHumidityOversampling(BME680_OS_2X);
-        bme.setPressureOversampling(BME680_OS_4X);
-        bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
-        bme.setGasHeater(320, 150); // 320*C for 150 ms
-
-        success = true;
-        break;
-      }
-
-      if (Plugin_106_init)
-      {
-        if (!bme.performReading()) {
+        if (!P106_data->bme.performReading()) {
+          P106_data->initialized = false;
           addLog(LOG_LEVEL_ERROR, F("BME680 : Failed to perform reading!"));
           success = false;
           break;
         }
 
-        UserVar[event->BaseVarIndex + 0] = bme.temperature;
-        UserVar[event->BaseVarIndex + 1] = bme.humidity;
-        UserVar[event->BaseVarIndex + 2] = bme.pressure / 100.0;
-        UserVar[event->BaseVarIndex + 3] = bme.gas_resistance / 1000.0;
+        UserVar[event->BaseVarIndex + 0] = P106_data->bme.temperature;
+        UserVar[event->BaseVarIndex + 1] = P106_data->bme.humidity;
+        UserVar[event->BaseVarIndex + 2] = P106_data->bme.pressure / 100.0;
+        UserVar[event->BaseVarIndex + 3] = P106_data->bme.gas_resistance / 1000.0;
       }
 
       success = true;
