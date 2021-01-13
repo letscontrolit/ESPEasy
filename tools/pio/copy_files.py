448f8b9733f28dafeb4c481eb544485754046bb0
TD-er <gijs.noorlander@gmail.com>
2021-01-13 14:16:26 +0100

    [Build] ESP32 factory bin files are 64k larger so don't reject those

--- tools/pio/copy_files.py
+++ tools/pio/copy_files.py
@@ -20,6 +20,9 @@ def get_max_bin_size(env_name, file_suffix):
     if "4M316k" in env_name:
         # ESP32 with 1800k of sketch space.
         max_bin_size = 1900544
+        if "factory" in file_suffix:
+            # Factory bin files include a part which is not overwritten via OTA
+            max_bin_size = max_bin_size + 65536
     if "debug_" in env_name:
         # Debug env, used for analysis, not to be run on a node.
         max_bin_size = 0
