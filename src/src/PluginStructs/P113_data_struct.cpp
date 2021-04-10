00a027fba53b5e45d3ac7dfd731db9e4a187b182
Ton Huisman <tonhu2004@hotmail.com>
2021-04-10 14:15:32 +0200

    [P113] Apply uncrustify source formatting

--- src/src/PluginStructs/P113_data_struct.cpp
+++ src/src/PluginStructs/P113_data_struct.cpp
@@ -4,11 +4,10 @@
 
 P113_data_struct::P113_data_struct(uint8_t i2c_addr, int timing, bool range) : i2cAddress(i2c_addr), timing(timing), range(range) {}
 
-//**************************************************************************/
+// **************************************************************************/
 // Initialize VL53L1X
-//**************************************************************************/
+// **************************************************************************/
 bool P113_data_struct::begin() {
-
   initState = true;
 
   sensor.setI2CAddress(i2cAddress); // Initialize for configured address
@@ -36,30 +35,32 @@ bool P113_data_struct::startRead() {
   if (initState && !readActive) {
     sensor.startRanging();
     readActive = true;
-    distance = -1;
+    distance   = -1;
   }
   return readActive;
 }
 
 bool P113_data_struct::readAvailable() {
   bool ready = sensor.checkForDataReady();
+
   if (ready) {
     distance = sensor.getDistance();
     sensor.clearInterrupt();
     sensor.stopRanging();
+
     // readActive = false;
   }
   return ready;
 }
 
 uint16_t P113_data_struct::readDistance() {
-
   success = false;
 
-  #if defined(P113_DEBUG) || defined (P113_DEBUG_DEBUG)
+  # if defined(P113_DEBUG) || defined(P113_DEBUG_DEBUG)
   String log;
-  #endif
-  #ifdef P113_DEBUG_DEBUG
+  # endif // if defined(P113_DEBUG) || defined(P113_DEBUG_DEBUG)
+  # ifdef P113_DEBUG_DEBUG
+
   if (loglevelActiveFor(LOG_LEVEL_DEBUG)) {
     log  = F("VL53L1X  : idx: 0x");
     log += String(i2cAddress, HEX);
@@ -67,20 +68,20 @@ uint16_t P113_data_struct::readDistance() {
     log += String(initState, BIN);
     addLog(LOG_LEVEL_DEBUG, log);
   }
-  #endif // P113_DEBUG_DEBUG
+  # endif // P113_DEBUG_DEBUG
 
-  success = true;
+  success    = true;
   readActive = false;
 
-  if ( distance >= 8190 ) {
-    #ifdef P113_DEBUG_DEBUG
+  if (distance >= 8190) {
+    # ifdef P113_DEBUG_DEBUG
     addLog(LOG_LEVEL_DEBUG, "VL53L1X: NO MEASUREMENT");
-    #endif // P113_DEBUG_DEBUG
+    # endif // P113_DEBUG_DEBUG
     success = false;
   }
 
-  #ifdef P113_DEBUG
-  log = F("VL53L1X: Address: 0x");
+  # ifdef P113_DEBUG
+  log  = F("VL53L1X: Address: 0x");
   log += String(i2cAddress, HEX);
   log += F(" / Timing: ");
   log += String(timing, DEC);
@@ -89,10 +90,10 @@ uint16_t P113_data_struct::readDistance() {
   log += F(" / Distance: ");
   log += distance;
   addLog(LOG_LEVEL_INFO, log);
-  #endif // P113_DEBUG
+  # endif // P113_DEBUG
 
   return distance;
-};
+}
 
 uint16_t P113_data_struct::readAmbient() {
   return sensor.getAmbientRate();
@@ -100,6 +101,6 @@ uint16_t P113_data_struct::readAmbient() {
 
 bool P113_data_struct::isReadSuccessful() {
   return success;
-};
+}
 
 #endif // ifdef USES_P113
--- src/src/PluginStructs/P113_data_struct.h
+++ src/src/PluginStructs/P113_data_struct.h
@@ -4,22 +4,24 @@
 #include "../../_Plugin_Helper.h"
 #ifdef USES_P113
 
-#define P113_DEBUG        // Enable debugging output (INFO loglevel)
-#define P113_DEBUG_DEBUG  // Enable extended debugging output (DEBUG loglevel)
+# define P113_DEBUG       // Enable debugging output (INFO loglevel)
+# define P113_DEBUG_DEBUG // Enable extended debugging output (DEBUG loglevel)
 
-#ifdef LIMIT_BUILD_SIZE
-  #ifdef P113_DEBUG_DEBUG
-    #undef P113_DEBUG_DEBUG
-  #endif
-#endif
+# ifdef LIMIT_BUILD_SIZE
+  #  ifdef P113_DEBUG_DEBUG
+    #   undef P113_DEBUG_DEBUG
+  #  endif // ifdef P113_DEBUG_DEBUG
+# endif    // ifdef LIMIT_BUILD_SIZE
 
-#include <Wire.h>
-#include <SparkFun_VL53L1X.h>
+# include <Wire.h>
+# include <SparkFun_VL53L1X.h>
 
 struct P113_data_struct : public PluginTaskData_base {
 public:
 
-  P113_data_struct(uint8_t i2c_addr, int timing, bool range);
+  P113_data_struct(uint8_t i2c_addr,
+                   int     timing,
+                   bool    range);
 
   bool     begin();
   bool     startRead();
@@ -33,7 +35,7 @@ private:
   SFEVL53L1X sensor;
 
   uint8_t  i2cAddress;
-  bool     initState  = false;
+  bool     initState = false;
   int      timing;
   bool     range;
   bool     success    = false;
