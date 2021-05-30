437c29f3980cefdc2a87c9097c32ca4c8e99b6a3
TD-er <gijs.noorlander@gmail.com>
2021-05-30 11:29:48 +0200

    [P003 Pulse] Fix compile issue when plugin not included in build.

--- src/src/PluginStructs/P003_data_struct.cpp
+++ src/src/PluginStructs/P003_data_struct.cpp
@@ -1,6 +1,7 @@
 #include "../PluginStructs/P003_data_struct.h"
 
 
+#ifdef USES_P003
 P003_data_struct::P003_data_struct(const Internal_GPIO_pulseHelper::pulseCounterConfig& config)
   :
   pulseHelper(config)
@@ -8,3 +9,4 @@ P003_data_struct::P003_data_struct(const Internal_GPIO_pulseHelper::pulseCounter
 
 P003_data_struct::~P003_data_struct()
 {}
+#endif
