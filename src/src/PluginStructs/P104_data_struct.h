c67e3690646b7d8c494cc0008078eaeae34b37fa
Ton Huisman <tonhu2004@hotmail.com>
2021-06-19 17:00:41 +0200

    [P104] Try to make build fit in ESP8266 display configuration

--- src/src/PluginStructs/P104_data_struct.h
+++ src/src/PluginStructs/P104_data_struct.h
@@ -20,11 +20,11 @@
 # define P104_USE_FULL_DOUBLEHEIGHT_FONT // Enables the use of a full (lower ascii only) set double height font
 # define P104_USE_VERTICAL_FONT          // Enables the use of a vertical font
 # define P104_USE_EXT_ASCII_FONT         // Enables the use of a extended ascii font
-// # ifdef PLUGIN_DISPLAY_COLLECTION
-# define P104_USE_ARABIC_FONT            // Enables the use of a Arabic font (see usage in MD_Parola examples)
-# define P104_USE_GREEK_FONT             // Enables the use of a Greek font (see usage in MD_Parola examples)
-# define P104_USE_KATAKANA_FONT          // Enables the use of a Katakana font (see usage in MD_Parola examples)
-// # endif // ifdef PLUGIN_DISPLAY_COLLECTION
+# if !defined(PLUGIN_DISPLAY_COLLECTION) && !defined(ESP8266) // To make it fit in the ESP8266 build, a few fonts are disabled
+#  define P104_USE_ARABIC_FONT           // Enables the use of a Arabic font (see usage in MD_Parola examples)
+#  define P104_USE_GREEK_FONT            // Enables the use of a Greek font (see usage in MD_Parola examples)
+#  define P104_USE_KATAKANA_FONT         // Enables the use of a Katakana font (see usage in MD_Parola examples)
+# endif // ifdef PLUGIN_DISPLAY_COLLECTION
 
 // # define P104_MINIMAL_ANIMATIONS            // disable most animations
 // # define P104_MEDIUM_ANIMATIONS             // disable some complex animations
