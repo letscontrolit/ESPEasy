9def20bd2cd5561945435f57be36f57c58f15cae
Ton Huisman <tonhu2004@hotmail.com>
2021-06-21 20:56:53 +0200

    [P104] Adjust Custom-sample.h

--- src/Custom-sample.h
+++ src/Custom-sample.h
@@ -363,8 +363,8 @@ static const char DATA_ESPEASY_DEFAULT_MIN_CSS[] PROGMEM = {
 // #define USES_P101   // WakeOnLan
 // #define USES_P102   // PZEM004Tv3
 // #define USES_P103   // Atlas_EZO_pH
-// #define USES_P104   // Atlas_EZO_EC
-// #define USES_P105   // Atlas_EZO_ORP
+// #define USES_P104   // MAX7219 dotmatrix
+// #define USES_P105   //
 // #define USES_P106   // BME680
 // #define USES_P107   // Si1145
 // #define USES_P110   // VL53L0X Time of Flight sensor
