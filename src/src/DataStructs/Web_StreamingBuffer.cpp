ef052d9124f74c6d10281df406f50c202f51e96a
TD-er <gijs.noorlander@gmail.com>
2021-06-25 16:37:20 +0200

    [Webserver]  Fix unneeded copy of object when serving data(#3693)
    Fixes: #3693

--- src/src/DataStructs/Web_StreamingBuffer.cpp
+++ src/src/DataStructs/Web_StreamingBuffer.cpp
@@ -23,16 +23,16 @@ Web_StreamingBuffer::Web_StreamingBuffer(void) : lowMemorySkip(false),
 }
 
 /*
-Web_StreamingBuffer Web_StreamingBuffer::operator=(String& a)                 {
+Web_StreamingBuffer& Web_StreamingBuffer::operator=(String& a)                 {
   flush(); return addString(a);
 }
 
-Web_StreamingBuffer Web_StreamingBuffer::operator=(const String& a)           {
+Web_StreamingBuffer& Web_StreamingBuffer::operator=(const String& a)           {
   flush(); return addString(a);
 }
 */
 
-Web_StreamingBuffer Web_StreamingBuffer::operator+=(char a)                   {
+Web_StreamingBuffer& Web_StreamingBuffer::operator+=(char a)                   {
   if (CHUNKED_BUFFER_SIZE > (this->buf.length() + 1)) {
     this->buf += a;
     return *this;
@@ -40,35 +40,35 @@ Web_StreamingBuffer Web_StreamingBuffer::operator+=(char a)                   {
   return addString(String(a));
 }
 
-Web_StreamingBuffer Web_StreamingBuffer::operator+=(long unsigned int a)     {
+Web_StreamingBuffer& Web_StreamingBuffer::operator+=(long unsigned int a)     {
   return addString(String(a));
 }
 
-Web_StreamingBuffer Web_StreamingBuffer::operator+=(float a)                  {
+Web_StreamingBuffer& Web_StreamingBuffer::operator+=(float a)                  {
   return addString(String(a));
 }
 
-Web_StreamingBuffer Web_StreamingBuffer::operator+=(int a)                    {
+Web_StreamingBuffer& Web_StreamingBuffer::operator+=(int a)                    {
   return addString(String(a));
 }
 
-Web_StreamingBuffer Web_StreamingBuffer::operator+=(uint32_t a)               {
+Web_StreamingBuffer& Web_StreamingBuffer::operator+=(uint32_t a)               {
   return addString(String(a));
 }
 
-Web_StreamingBuffer Web_StreamingBuffer::operator+=(const String& a)          {
+Web_StreamingBuffer& Web_StreamingBuffer::operator+=(const String& a)          {
   return addString(a);
 }
 
-Web_StreamingBuffer Web_StreamingBuffer::operator+=(PGM_P str) {
+Web_StreamingBuffer& Web_StreamingBuffer::operator+=(PGM_P str) {
   return addFlashString(str);
 }
 
-Web_StreamingBuffer Web_StreamingBuffer::operator+=(const __FlashStringHelper* str) {
+Web_StreamingBuffer& Web_StreamingBuffer::operator+=(const __FlashStringHelper* str) {
   return addFlashString((PGM_P)str);
 }
 
-Web_StreamingBuffer Web_StreamingBuffer::addFlashString(PGM_P str) {
+Web_StreamingBuffer& Web_StreamingBuffer::addFlashString(PGM_P str) {
   #ifdef USE_SECOND_HEAP
   HeapSelectDram ephemeral;
   #endif
@@ -112,7 +112,7 @@ Web_StreamingBuffer Web_StreamingBuffer::addFlashString(PGM_P str) {
   return *this;
 }
 
-Web_StreamingBuffer Web_StreamingBuffer::addString(const String& a) {
+Web_StreamingBuffer& Web_StreamingBuffer::addString(const String& a) {
   if (lowMemorySkip) { return *this; }
   int flush_step = CHUNKED_BUFFER_SIZE - this->buf.length();
 
