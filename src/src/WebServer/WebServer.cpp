53450f6d30b94d06604a38100470ad9f22ab23aa
Ton Huisman <tonhu2004@hotmail.com>
2021-07-30 20:17:14 +0200

    [UI] Build name/date is link to Github Releases page

--- src/src/WebServer/WebServer.cpp
+++ src/src/WebServer/WebServer.cpp
@@ -548,7 +548,7 @@ void getWebPageTemplateDefaultHeader(String& tmpl, const String& title, bool add
   #ifndef WEBPAGE_TEMPLATE_DEFAULT_HEADER
     tmp = F("<header class='headermenu'><h1>ESP Easy Mega: {{title}}"
             #if BUILD_IN_WEBHEADER
-            "<div style='float:right;font-size:10pt'>Build: {{build}}</div>"
+            "<div style='float:right;font-size:10pt'>Build: <a href='https://github.com/letscontrolit/ESPEasy/releases' style='font-size: 15px; text-decoration: none'>{{build}}</a></div>"
             #endif // #if BUILD_IN_WEBHEADER
             "</h1><BR>"
             );
@@ -583,7 +583,7 @@ void getWebPageTemplateDefaultFooter(String& tmpl) {
             "<br>"
             "<h6>Powered by <a href='http://www.letscontrolit.com' style='font-size: 15px; text-decoration: none'>Let's Control It</a> community"
             #if BUILD_IN_WEBFOOTER
-            "<div style='float: right;font-size:10pt;'>Build: {{build}} {{date}}</div>"
+            "<div style='float: right'>Build: <a href='https://github.com/letscontrolit/ESPEasy/releases' style='font-size: 15px; text-decoration: none'>{{build}} {{date}}</a></div>"
             #endif // #if BUILD_IN_WEBFOOTER
             "</h6>"
             "</footer>"
