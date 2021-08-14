7641a9a3625566cf512f95632aeb3ed8583eaeb4
Ton Huisman <tonhu2004@hotmail.com>
2021-08-14 19:37:46 +0200

    [P105] Fix I2Cscanner merge issue

--- src/_P105_AHT.ino
+++ src/_P105_AHT.ino
@@ -77,23 +77,20 @@ boolean Plugin_105(uint8_t function, struct EventStruct *event, String& string)
       break;
     }
 
+    case PLUGIN_I2C_HAS_ADDRESS:
     case PLUGIN_WEBFORM_SHOW_I2C_PARAMS:
     {
-      int Plugin_105_i2c_addresses[2] = { 0x38, 0x39 };
-      addFormSelectorI2C(F("i2c_addr"), 2, Plugin_105_i2c_addresses, PCONFIG(0));
-      addFormNote(F("SDO Low=0x38, High=0x39. NB: Only available on AHT10 sensors."));
+      const int i2cAddressValues[2] = { 0x38, 0x39 };
+
+      if (function == PLUGIN_WEBFORM_SHOW_I2C_PARAMS) {
+        addFormSelectorI2C(F("i2c_addr"), 2, i2cAddressValues, PCONFIG(0));
+        addFormNote(F("SDO Low=0x38, High=0x39. NB: Only available on AHT10 sensors."));
+      } else {
+        success = intArrayContains(2, i2cAddressValues, event->Par1);
+      }
 
       break;
     }
-
-    #if USE_I2C_DEVICE_SCAN
-    case PLUGIN_I2C_GET_ADDRESSES_HEX:
-    {
-      string = F("38,39"); // List of device addresses, hex, comma separated, _no_ 0x prefix
-      success = true;
-      break;
-    }
-    #endif // if USE_I2C_DEVICE_SCAN
 
     case PLUGIN_WEBFORM_LOAD:
     {
