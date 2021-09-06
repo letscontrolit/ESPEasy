745ff17ef3e03f2715e5d675a565cb34af3368a0
TD-er <gijs.noorlander@gmail.com>
2021-09-06 21:02:24 +0200

    [ESP32] Allow to compile using staging branch ESP32

--- lib/TinyGPSPlus-1.0.2/src/TinyGPS++.cpp
+++ lib/TinyGPSPlus-1.0.2/src/TinyGPS++.cpp
@@ -27,6 +27,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include <ctype.h>
 #include <stdlib.h>
 
+#include <time.h>
+
 TinyGPSPlus::TinyGPSPlus()
   :  parity(0)
   ,  isChecksumTerm(false)
