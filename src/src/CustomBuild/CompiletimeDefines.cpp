0d10401c1df1ab11ccf2a200fa22a3cc296d42f1
TD-er <gijs.noorlander@gmail.com>
2021-10-24 18:14:59 +0200

    [Build] Fix CompiletimeDefines
    The patched files were still in `src/Helpers/` but that's now concatenated, thus the `CompiletimeDefines.cpp` file was not compiled anymore.

--- src/src/CustomBuild/CompiletimeDefines.cpp
+++ src/src/CustomBuild/CompiletimeDefines.cpp
@@ -1,4 +1,4 @@
-#include "../Helpers/CompiletimeDefines.h"
+#include "../CustomBuild/CompiletimeDefines.h"
 
 // This file will be "patched" at compiletime by 
 // tools/pio/generate-compiletime-defines.py
@@ -22,7 +22,7 @@
 // End of defines being patched by the Python build script.
 
 const __FlashStringHelper * get_binary_filename() {
- #if !defined(CORE_POST_2_5_0) && !defined(ESP32) || defined(ESP32_STAGE)
+ #if !defined(CORE_POST_2_5_0) && !defined(ESP32)
    return F("firmware.bin");
  #else
     return F(SET_BUILD_BINARY_FILENAME);
@@ -48,7 +48,7 @@ const __FlashStringHelper * get_build_origin() {
 }
 
 const __FlashStringHelper * get_build_platform() {
- #if !defined(CORE_POST_2_5_0) && !defined(ESP32) || defined(ESP32_STAGE)
+ #if !defined(CORE_POST_2_5_0) && !defined(ESP32)
     return F("");
   #else
     return F(SET_BUILD_PLATFORM);
@@ -56,7 +56,7 @@ const __FlashStringHelper * get_build_platform() {
 }
 
 const __FlashStringHelper * get_git_head() {
- #if !defined(CORE_POST_2_5_0) && !defined(ESP32) || defined(ESP32_STAGE)
+ #if !defined(CORE_POST_2_5_0) && !defined(ESP32)
    return F("");
  #else
     return F(SET_BUILD_GIT_HEAD);
