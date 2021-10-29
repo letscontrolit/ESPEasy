041095ec1a890521cb34aad12d2fc020c508d585
Ton Huisman <tonhu2004@hotmail.com>
2021-10-29 20:31:28 +0200

    [P119] Add plugin for ITG3205 gyro sensor

---
+++ src/_P119_ITG3205_Gyro.ino
@@ -0,0 +1,193 @@
+#include "_Plugin_Helper.h"
+
+#ifdef USES_P119
+
+// #######################################################################################################
+// #################################### Plugin 119: Gyro - ITG3205 #######################################
+// #######################################################################################################
+
+/**
+ * Gyro plugin for ITG3205 chip. This chip is often combined with ADXL345 and HMC5883L on a single board, and then will
+ * support only the default I2C address. For a 'standalone' chip/board, often an AD0 pin is available to select the
+ * secondary I2C address.
+ */
+
+/** Changelog:
+ *
+ * 2021-10-28, tonhuisman: Tested reading (chip) temperature measurement, but it isn't useful, so removed again.
+ * 2021-10-26, tonhuisman: Add averaging and frequency features
+ * 2021-10-24, tonhuisman: Initial plugin created from template, using ITG3205 library https://github.com/ikiselev/ITG3205
+ *
+ *************************************************************************************************************************/
+
+// #include section
+# include "src/PluginStructs/P119_data_struct.h"
+
+# define PLUGIN_119
+# define PLUGIN_ID_119          119 // plugin id
+# define PLUGIN_NAME_119        "Gyro - ITG3205 [DEVELOPMENT]"
+# define PLUGIN_VALUENAME1_119  "X"
+# define PLUGIN_VALUENAME2_119  "Y"
+# define PLUGIN_VALUENAME3_119  "Z"
+
+// Make accessing specific parameters more readable in the code
+# define P119_RAW_DATA        PCONFIG(0)
+# define P119_I2C_ADDR        PCONFIG(1) // Kept (1) from template
+# define P119_AVERAGE_BUFFER  PCONFIG(2)
+# define P119_FREQUENCY       PCONFIG(3)
+# define P119_FREQUENCY_10    0          // 10x per second
+# define P119_FREQUENCY_50    1          // 50x per second
+
+boolean Plugin_119(uint8_t function, struct EventStruct *event, String& string)
+{
+  boolean success = false;
+
+  switch (function)
+  {
+    case PLUGIN_DEVICE_ADD:
+    {
+      Device[++deviceCount].Number       = PLUGIN_ID_119;
+      Device[deviceCount].Type           = DEVICE_TYPE_I2C;
+      Device[deviceCount].VType          = Sensor_VType::SENSOR_TYPE_TRIPLE;
+      Device[deviceCount].Ports          = 0;
+      Device[deviceCount].ValueCount     = 3;
+      Device[deviceCount].FormulaOption  = true;
+      Device[deviceCount].SendDataOption = true;
+      Device[deviceCount].TimerOption    = true;
+      Device[deviceCount].TimerOptional  = true;
+
+      break;
+    }
+
+    case PLUGIN_GET_DEVICENAME:
+    {
+      string = F(PLUGIN_NAME_119);
+      break;
+    }
+
+    case PLUGIN_GET_DEVICEVALUENAMES:
+    {
+      strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[0], PSTR(PLUGIN_VALUENAME1_119));
+      strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[1], PSTR(PLUGIN_VALUENAME2_119));
+      strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[2], PSTR(PLUGIN_VALUENAME3_119));
+      break;
+    }
+
+    case PLUGIN_I2C_HAS_ADDRESS:
+    case PLUGIN_WEBFORM_SHOW_I2C_PARAMS:
+    {
+      const uint8_t i2cAddressValues[] = { 0x68, 0x69 };
+
+      if (function == PLUGIN_WEBFORM_SHOW_I2C_PARAMS) {
+        addFormSelectorI2C(F("i2c_addr"), 2, i2cAddressValues, P119_I2C_ADDR);
+        addFormNote(F("AD0 Low=0x68, High=0x69"));
+      } else {
+        success = intArrayContains(2, i2cAddressValues, event->Par1);
+      }
+      break;
+    }
+
+    case PLUGIN_SET_DEFAULTS:
+    {
+      P119_I2C_ADDR       = 0x68; // Default I2C Address
+      P119_AVERAGE_BUFFER = 10;   // Average averaging ;-)
+
+      // No decimals plausible, as the outputs from the sensor are of type int
+      for (uint8_t i = 0; i < VARS_PER_TASK; ++i) {
+        ExtraTaskSettings.TaskDeviceValueDecimals[i] = 0;
+      }
+
+      success = true;
+      break;
+    }
+
+
+    case PLUGIN_WEBFORM_LOAD:
+    {
+      addFormCheckBox(F("Read 'raw' values from Gyro"), F("p119_rawData"), P119_RAW_DATA == 1);
+
+      addFormNumericBox(F("Averaging buffer size"), F("p119_average_buf"), P119_AVERAGE_BUFFER, 1, 100);
+      addUnit(F("1..100"));
+
+      const __FlashStringHelper *frequencyOptions[] = {
+        F("10x per second"),
+        F("50x per second") };
+      int frequencyValues[] = { P119_FREQUENCY_10, P119_FREQUENCY_50 };
+      addFormSelector(F("Measuring frequency"), F("p119_frequency"), 2, frequencyOptions, frequencyValues, P119_FREQUENCY);
+
+      success = true;
+      break;
+    }
+
+    case PLUGIN_WEBFORM_SAVE:
+    {
+      P119_I2C_ADDR       = getFormItemInt(F("i2c_addr"));
+      P119_RAW_DATA       = isFormItemChecked(F("p119_rawData")) ? 1 : 0;
+      P119_AVERAGE_BUFFER = getFormItemInt(F("p119_average_buf"));
+      P119_FREQUENCY      = getFormItemInt(F("p119_frequency"));
+
+      success = true;
+      break;
+    }
+
+    case PLUGIN_INIT:
+    {
+      initPluginTaskData(event->TaskIndex, new (std::nothrow) P119_data_struct(P119_I2C_ADDR, P119_RAW_DATA, P119_AVERAGE_BUFFER));
+      P119_data_struct *P119_data = static_cast<P119_data_struct *>(getPluginTaskData(event->TaskIndex));
+
+      if (nullptr != P119_data) {
+        success = true;
+      }
+
+      break;
+    }
+
+    case PLUGIN_READ:
+    {
+      P119_data_struct *P119_data = static_cast<P119_data_struct *>(getPluginTaskData(event->TaskIndex));
+
+      if (nullptr != P119_data) {
+        success = P119_data->initialized();
+      }
+
+      break;
+    }
+
+    case PLUGIN_ONCE_A_SECOND:
+    {
+      P119_data_struct *P119_data = static_cast<P119_data_struct *>(getPluginTaskData(event->TaskIndex));
+
+      if (nullptr != P119_data) {
+        int X, Y, Z;
+
+        if (P119_data->read_data(X, Y, Z)) {
+          UserVar[event->BaseVarIndex]     = X;
+          UserVar[event->BaseVarIndex + 1] = Y;
+          UserVar[event->BaseVarIndex + 2] = Z;
+
+          success = true;
+        }
+      }
+
+      break;
+    }
+
+    case PLUGIN_TEN_PER_SECOND:
+    case PLUGIN_FIFTY_PER_SECOND:
+    {
+      if (((function == PLUGIN_TEN_PER_SECOND) && (P119_FREQUENCY == P119_FREQUENCY_10)) ||
+          ((function == PLUGIN_FIFTY_PER_SECOND) && (P119_FREQUENCY == P119_FREQUENCY_50))) {
+        P119_data_struct *P119_data = static_cast<P119_data_struct *>(getPluginTaskData(event->TaskIndex));
+
+        if (nullptr != P119_data) {
+          success = P119_data->read_sensor();
+        }
+      }
+
+      break;
+    }
+  } // switch
+  return success;
+}   // function
+
+#endif // ifdef USES_P119
---
+++ src/src/PluginStructs/P119_data_struct.h
@@ -0,0 +1,54 @@
+#ifndef PLUGINSTRUCTS_P119_DATA_STRUCT_H
+#define PLUGINSTRUCTS_P119_DATA_STRUCT_H
+
+#include "../../_Plugin_Helper.h"
+
+#ifdef USES_P119
+
+# define PLUGIN_119_DEBUG  false // set to true for extra log info in the debug
+
+# include <vector>
+# include "ITG3205.h"
+
+struct P119_data_struct : public PluginTaskData_base {
+public:
+
+  P119_data_struct(uint8_t i2c_addr,
+                   bool    rawData,
+                   uint8_t aSize);
+
+  P119_data_struct() = delete;
+
+  ~P119_data_struct();
+
+  bool read_sensor();
+
+  bool read_data(int& X,
+                 int& Y,
+                 int& Z);
+
+  bool initialized() {
+    return _initialized;
+  }
+
+private:
+
+  ITG3205 *itg3205 = nullptr;
+
+  bool init_sensor();
+
+  uint8_t _i2cAddress;
+  bool    _rawData;
+  uint8_t _aSize;
+
+  std::vector<int>_XA;
+  std::vector<int>_YA;
+  std::vector<int>_ZA;
+  uint8_t         _aUsed = 0;
+  uint8_t         _aMax  = 0;
+
+  bool _initialized = false;
+};
+
+#endif // ifdef USES_P119
+#endif // ifndef PLUGINSTRUCTS_P119_DATA_STRUCT_H
