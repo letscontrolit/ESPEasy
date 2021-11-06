40d17e04037701aaf21bf21b52e24b79fceca105
TD-er <gijs.noorlander@gmail.com>
2021-11-06 17:21:22 +0100

    [TLS] Add ALPN protocol

--- src/src/Helpers/ESPEasy_ssl_client.h
+++ src/src/Helpers/ESPEasy_ssl_client.h
@@ -38,12 +38,13 @@ typedef struct ESPEasy_sslclient_context {
 
 
 void ssl_init(ESPEasy_sslclient_context *ssl_client);
-int start_ssl_client(ESPEasy_sslclient_context *ssl_client, const char *host, uint32_t port, int timeout, const char *rootCABuff, const char *cli_cert, const char *cli_key, const char *pskIdent, const char *psKey, bool insecure);
+int start_ssl_client(ESPEasy_sslclient_context *ssl_client, const char *host, uint32_t port, int timeout, const char *rootCABuff, const char *cli_cert, const char *cli_key, const char *pskIdent, const char *psKey, bool insecure, const char **alpn_protos);
 void stop_ssl_socket(ESPEasy_sslclient_context *ssl_client, const char *rootCABuff, const char *cli_cert, const char *cli_key);
 int data_to_read(ESPEasy_sslclient_context *ssl_client);
 int send_ssl_data(ESPEasy_sslclient_context *ssl_client, const uint8_t *data, uint16_t len);
 int get_ssl_receive(ESPEasy_sslclient_context *ssl_client, uint8_t *data, int length);
 bool verify_ssl_fingerprint(ESPEasy_sslclient_context *ssl_client, const char* fp, const char* domain_name);
 bool verify_ssl_dn(ESPEasy_sslclient_context *ssl_client, const char* domain_name);
+bool get_peer_fingerprint(ESPEasy_sslclient_context *ssl_client, uint8_t sha256[32]);
 
 #endif
