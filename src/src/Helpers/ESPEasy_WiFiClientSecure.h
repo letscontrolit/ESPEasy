97a4594fa93895ed3396e28baa6f3006152ff026
TD-er <gijs.noorlander@gmail.com>
2021-11-06 18:01:27 +0100

    [TLS] Add fix for WiFiClientSecure connection timeout

--- src/src/Helpers/ESPEasy_WiFiClientSecure.h
+++ src/src/Helpers/ESPEasy_WiFiClientSecure.h
@@ -57,7 +57,7 @@ public:
     int connect(const char *host, uint16_t port, const char *rootCABuff, const char *cli_cert, const char *cli_key);
     int connect(IPAddress ip, uint16_t port, const char *pskIdent, const char *psKey);
     int connect(const char *host, uint16_t port, const char *pskIdent, const char *psKey);
-	int peek();
+    int peek();
     size_t write(uint8_t data);
     size_t write(const uint8_t *buf, size_t size);
     int available();
