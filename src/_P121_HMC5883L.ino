64744303c1066de5fe7dd176fcfe3c752073524b
svn2208 <s.briesen@gmail.com>
2021-11-19 12:43:17 +0100

    Usage of proper storage of a float conf value

--- src/_P121_HMC5883L.ino
+++ src/_P121_HMC5883L.ino
@@ -79,7 +79,7 @@ boolean Plugin_121(uint8_t function, struct EventStruct *event, String &string)
   }
   case PLUGIN_WEBFORM_LOAD:
   {
-    addFormFloatNumberBox(F("DeclinationAngle"), F("plugin_121_HMC5883L_decl"), PCONFIG(0), -2.0f,2.0f ,5, 0.00001f);
+    addFormFloatNumberBox(F("DeclinationAngle"), F("plugin_121_HMC5883L_decl"), PCONFIG_FLOAT(0), -2.0f,2.0f ,5, 0.00001f);
     addUnit(F("degree"));
     success = true;
     break;
@@ -87,7 +87,7 @@ boolean Plugin_121(uint8_t function, struct EventStruct *event, String &string)
 
   case PLUGIN_WEBFORM_SAVE:
   {
-    PCONFIG(0) = getFormItemFloat(F("plugin_121_HMC5883L_decl"));
+    PCONFIG_FLOAT(0) = getFormItemFloat(F("plugin_121_HMC5883L_decl"));
     success = true;
     break;
   }
@@ -127,7 +127,7 @@ boolean Plugin_121(uint8_t function, struct EventStruct *event, String &string)
 
       float heading = atan2(s_event.magnetic.y, s_event.magnetic.x);
 
-      const float decl = PCONFIG(0);
+      const float decl = PCONFIG_FLOAT(0);
       addLog(LOG_LEVEL_DEBUG, "Declination Degree:" + String(decl));
       if (decl != 0)
       {
