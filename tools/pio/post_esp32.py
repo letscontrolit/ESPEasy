840f706fc711f0de1fd86e399e9f6c0e17b77002
TD-er <gijs.noorlander@gmail.com>
2022-01-02 02:16:46 +0100

    [PIO build ESP32] Attempt to fix boot loop on ESP32 with IDF4.4
    16M flash ESP32 units keep rebooting when built with a partitioning schema for 16M flash units.
    This does not happen on older IDF versions, but does happen on IDF 4.4

--- tools/pio/post_esp32.py
+++ tools/pio/post_esp32.py
@@ -7,15 +7,20 @@ def esp32_create_factory_bin(source, target, env):
     new_file_name = env.subst("$BUILD_DIR/${PROGNAME}-factory.bin")
     sections = env.subst(env.get('FLASH_EXTRA_IMAGES'))
     new_file = open(new_file_name,"wb")
+    print("    {} | {}".format("Offset", "File"))
     for section in sections:
       sect_adr,sect_file = section.split(" ",1)
+      print(" -  {} | {}".format(sect_adr,sect_file))
       source = open(sect_file,"rb")
       new_file.seek(int(sect_adr,0)-offset)
       new_file.write(source.read());
       source.close()
     
-    firmware = open(env.subst("$BUILD_DIR/${PROGNAME}.bin"),"rb")
-    new_file.seek(0x10000-offset)
+    firmware_name = env.subst("$BUILD_DIR/${PROGNAME}.bin")
+    firmware_start = 0x10000-offset
+    firmware = open(firmware_name,"rb")
+    print(" - {} | {}".format(hex(firmware_start),firmware_name))
+    new_file.seek(firmware_start)
     new_file.write(firmware.read())
     new_file.close()
     firmware.close()
