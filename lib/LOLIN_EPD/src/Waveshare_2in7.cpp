e477a8f5c215d94a6d82777b7f82ff9635015601
Ton Huisman <tonhu2004@hotmail.com>
2022-03-27 15:58:33 +0200

    [P096] WS2in7: Prefix all compile-time defines, remove unused constructor

--- lib/LOLIN_EPD/src/Waveshare_2in7.cpp
+++ lib/LOLIN_EPD/src/Waveshare_2in7.cpp
@@ -67,32 +67,14 @@ static const unsigned char EPD_2in7_gray_lut_bb[] = {
 #define BUSY_WAIT 1000
 
 /*!
-    @brief constructor if using on-chip RAM and software SPI
+    @brief constructor if using on-chip RAM and hardware SPI
     @param width the width of the display in pixels
     @param height the height of the display in pixels
-    @param SID the SID pin to use
-    @param SCLK the SCLK pin to use
     @param DC the data/command pin to use
     @param RST the reset pin to use
     @param CS the chip select pin to use
     @param BUSY the busy pin to use
  */
-
-/**************************************************************************/
-Waveshare_2in7::Waveshare_2in7(int width, int height, int8_t SID, int8_t SCLK, int8_t DC, int8_t RST, int8_t CS, int8_t BUSY)
-  : LOLIN_EPD(width, height, SID, SCLK, DC, RST, CS, BUSY) {
-  if ((height % 8) > 0) {
-    _height_8bit = (height / 8 + 1) * 8;
-  } else {
-    _height_8bit = height;
-  }
-
-  bw_bufsize = width * _height_8bit / 8;
-  bw_buf = (uint8_t *)malloc(bw_bufsize);
-
-  //   red_buf     = (uint8_t *)malloc(width * _height_8bit / 8); //
-  //   red_bufsize = bw_bufsize;
-}
 
 Waveshare_2in7::Waveshare_2in7(int width, int height, int8_t DC, int8_t RST, int8_t CS, int8_t BUSY)
   : LOLIN_EPD(width, height, DC, RST, CS, BUSY) {
@@ -103,10 +85,7 @@ Waveshare_2in7::Waveshare_2in7(int width, int height, int8_t DC, int8_t RST, int
   }
 
   bw_bufsize = width * _height_8bit / 8;
-  bw_buf = (uint8_t *)malloc(bw_bufsize);
-
-  //   red_buf     = (uint8_t *)malloc(width * _height_8bit / 8);
-  //   red_bufsize = bw_bufsize;
+  bw_buf     = (uint8_t *)malloc(bw_bufsize);
 }
 
 /**************************************************************************/
@@ -124,62 +103,62 @@ void Waveshare_2in7::begin(bool reset) {
   sendCmd(0x12);
   readBusy();
 
-  sendCmd(POWER_SETTING);
+  sendCmd(WS2IN7_POWER_SETTING);
   sendData(0x03); // VDS_EN, VDG_EN
   sendData(0x00); // VCOM_HV, VGHL_LV[1], VGHL_LV[0]
   sendData(0x2b); // VDH
   sendData(0x2b); // VDL
   sendData(0x09); // VDHR
-  sendCmd(BOOSTER_SOFT_START);
+  sendCmd(WS2IN7_BOOSTER_SOFT_START);
   sendData(0x07);
   sendData(0x07);
   sendData(0x17);
 
   // Power optimization
-  sendCmd(POWER_OPTIMIZATION);
+  sendCmd(WS2IN7_POWER_OPTIMIZATION);
   sendData(0x60);
   sendData(0xA5);
 
   // Power optimization
-  sendCmd(POWER_OPTIMIZATION);
+  sendCmd(WS2IN7_POWER_OPTIMIZATION);
   sendData(0x89);
   sendData(0xA5);
 
   // Power optimization
-  sendCmd(POWER_OPTIMIZATION);
+  sendCmd(WS2IN7_POWER_OPTIMIZATION);
   sendData(0x90);
   sendData(0x00);
 
   // Power optimization
-  sendCmd(POWER_OPTIMIZATION);
+  sendCmd(WS2IN7_POWER_OPTIMIZATION);
   sendData(0x93);
   sendData(0x2A);
 
   // Power optimization
-  sendCmd(POWER_OPTIMIZATION);
+  sendCmd(WS2IN7_POWER_OPTIMIZATION);
   sendData(0xA0);
   sendData(0xA5);
 
   // Power optimization
-  sendCmd(POWER_OPTIMIZATION);
+  sendCmd(WS2IN7_POWER_OPTIMIZATION);
   sendData(0xA1);
   sendData(0x00);
 
   // Power optimization
-  sendCmd(POWER_OPTIMIZATION);
+  sendCmd(WS2IN7_POWER_OPTIMIZATION);
   sendData(0x73);
   sendData(0x41);
 
-  sendCmd(PARTIAL_DISPLAY_REFRESH);
+  sendCmd(WS2IN7_PARTIAL_DISPLAY_REFRESH);
   sendData(0x00);
   sendCmd(WS2IN7_POWER_ON);
   readBusy();
 
-  sendCmd(PANEL_SETTING);
+  sendCmd(WS2IN7_PANEL_SETTING);
   sendData(0xAF); // KW-BF   KWR-AF    BWROTP 0f
-  sendCmd(PLL_CONTROL);
+  sendCmd(WS2IN7_PLL_CONTROL);
   sendData(0x3A); // 3A 100HZ   29 150Hz 39 200HZ    31 171HZ
-  sendCmd(VCM_DC_SETTING_REGISTER);
+  sendCmd(WS2IN7_VCM_DC_SETTING_REGISTER);
   sendData(0x12);
   delay(2);
   setLut();
@@ -194,31 +173,31 @@ void Waveshare_2in7::begin(bool reset) {
 void Waveshare_2in7::setLut(void) {
   unsigned int count;
 
-  sendCmd(LUT_FOR_VCOM); // vcom
+  sendCmd(WS2IN7_LUT_FOR_VCOM); // vcom
 
   for (count = 0; count < 44; count++) {
     sendData(lut_vcom_dc[count]);
   }
 
-  sendCmd(LUT_WHITE_TO_WHITE); // ww --
+  sendCmd(WS2IN7_LUT_WHITE_TO_WHITE); // ww --
 
   for (count = 0; count < 42; count++) {
     sendData(lut_ww[count]);
   }
 
-  sendCmd(LUT_BLACK_TO_WHITE); // bw r
+  sendCmd(WS2IN7_LUT_BLACK_TO_WHITE); // bw r
 
   for (count = 0; count < 42; count++) {
     sendData(lut_bw[count]);
   }
 
-  sendCmd(LUT_WHITE_TO_BLACK); // wb w
+  sendCmd(WS2IN7_LUT_WHITE_TO_BLACK); // wb w
 
   for (count = 0; count < 42; count++) {
     sendData(lut_bb[count]);
   }
 
-  sendCmd(LUT_BLACK_TO_BLACK); // bb b
+  sendCmd(WS2IN7_LUT_BLACK_TO_BLACK); // bb b
 
   for (count = 0; count < 42; count++) {
     sendData(lut_wb[count]);
@@ -234,7 +213,7 @@ void Waveshare_2in7::display() {
   // }
   // delay(2);
 
-  sendCmd(DATA_START_TRANSMISSION_2); // write RAM for black(0)/white (1)
+  sendCmd(WS2IN7_DATA_START_TRANSMISSION_2); // write RAM for black(0)/white (1)
   delay(2);
 
   for (uint16_t i = 0; i < bw_bufsize; i++) {
@@ -246,7 +225,7 @@ void Waveshare_2in7::display() {
 }
 
 void Waveshare_2in7::update() {
-  sendCmd(DISPLAY_REFRESH);
+  sendCmd(WS2IN7_DISPLAY_REFRESH);
   delay(200);
   readBusy();
 }
@@ -367,7 +346,7 @@ void Waveshare_2in7::selectLUT(uint8_t *wave_data) {
 }
 
 void Waveshare_2in7::deepSleep() {
-  sendCmd(DEEP_SLEEP);
+  sendCmd(WS2IN7_DEEP_SLEEP);
   sendData(0xa5);
 }
 
--- lib/LOLIN_EPD/src/Waveshare_2in7.h
+++ lib/LOLIN_EPD/src/Waveshare_2in7.h
@@ -12,44 +12,44 @@
 #include "LOLIN_EPD.h"
 
 // EPD2IN7 commands
-#define PANEL_SETTING                               0x00
-#define POWER_SETTING                               0x01
+#define WS2IN7_PANEL_SETTING                        0x00
+#define WS2IN7_POWER_SETTING                        0x01
 #define WS2IN7_POWER_OFF                            0x02
-#define POWER_OFF_SEQUENCE_SETTING                  0x03
+#define WS2IN7_POWER_OFF_SEQUENCE_SETTING           0x03
 #define WS2IN7_POWER_ON                             0x04
-#define POWER_ON_MEASURE                            0x05
-#define BOOSTER_SOFT_START                          0x06
-#define DEEP_SLEEP                                  0x07
-#define DATA_START_TRANSMISSION_1                   0x10
-#define DATA_STOP                                   0x11
-#define DISPLAY_REFRESH                             0x12
-#define DATA_START_TRANSMISSION_2                   0x13
-#define PARTIAL_DATA_START_TRANSMISSION_1           0x14 
-#define PARTIAL_DATA_START_TRANSMISSION_2           0x15 
-#define PARTIAL_DISPLAY_REFRESH                     0x16
-#define LUT_FOR_VCOM                                0x20 
-#define LUT_WHITE_TO_WHITE                          0x21
-#define LUT_BLACK_TO_WHITE                          0x22
-#define LUT_WHITE_TO_BLACK                          0x23
-#define LUT_BLACK_TO_BLACK                          0x24
-#define PLL_CONTROL                                 0x30
-#define TEMPERATURE_SENSOR_COMMAND                  0x40
-#define TEMPERATURE_SENSOR_CALIBRATION              0x41
-#define TEMPERATURE_SENSOR_WRITE                    0x42
-#define TEMPERATURE_SENSOR_READ                     0x43
-#define VCOM_AND_DATA_INTERVAL_SETTING              0x50
-#define LOW_POWER_DETECTION                         0x51
-#define TCON_SETTING                                0x60
-#define TCON_RESOLUTION                             0x61
-#define SOURCE_AND_GATE_START_SETTING               0x62
-#define GET_STATUS                                  0x71
-#define AUTO_MEASURE_VCOM                           0x80
-#define VCOM_VALUE                                  0x81
-#define VCM_DC_SETTING_REGISTER                     0x82
-#define PROGRAM_MODE                                0xA0
-#define ACTIVE_PROGRAM                              0xA1
-#define READ_OTP_DATA                               0xA2
-#define POWER_OPTIMIZATION                          0xF8
+#define WS2IN7_POWER_ON_MEASURE                     0x05
+#define WS2IN7_BOOSTER_SOFT_START                   0x06
+#define WS2IN7_DEEP_SLEEP                           0x07
+#define WS2IN7_DATA_START_TRANSMISSION_1            0x10
+#define WS2IN7_DATA_STOP                            0x11
+#define WS2IN7_DISPLAY_REFRESH                      0x12
+#define WS2IN7_DATA_START_TRANSMISSION_2            0x13
+#define WS2IN7_PARTIAL_DATA_START_TRANSMISSION_1    0x14 
+#define WS2IN7_PARTIAL_DATA_START_TRANSMISSION_2    0x15 
+#define WS2IN7_PARTIAL_DISPLAY_REFRESH              0x16
+#define WS2IN7_LUT_FOR_VCOM                         0x20 
+#define WS2IN7_LUT_WHITE_TO_WHITE                   0x21
+#define WS2IN7_LUT_BLACK_TO_WHITE                   0x22
+#define WS2IN7_LUT_WHITE_TO_BLACK                   0x23
+#define WS2IN7_LUT_BLACK_TO_BLACK                   0x24
+#define WS2IN7_PLL_CONTROL                          0x30
+#define WS2IN7_TEMPERATURE_SENSOR_COMMAND           0x40
+#define WS2IN7_TEMPERATURE_SENSOR_CALIBRATION       0x41
+#define WS2IN7_TEMPERATURE_SENSOR_WRITE             0x42
+#define WS2IN7_TEMPERATURE_SENSOR_READ              0x43
+#define WS2IN7_VCOM_AND_DATA_INTERVAL_SETTING       0x50
+#define WS2IN7_LOW_POWER_DETECTION                  0x51
+#define WS2IN7_TCON_SETTING                         0x60
+#define WS2IN7_TCON_RESOLUTION                      0x61
+#define WS2IN7_SOURCE_AND_GATE_START_SETTING        0x62
+#define WS2IN7_GET_STATUS                           0x71
+#define WS2IN7_AUTO_MEASURE_VCOM                    0x80
+#define WS2IN7_VCOM_VALUE                           0x81
+#define WS2IN7_VCM_DC_SETTING_REGISTER              0x82
+#define WS2IN7_PROGRAM_MODE                         0xA0
+#define WS2IN7_ACTIVE_PROGRAM                       0xA1
+#define WS2IN7_READ_OTP_DATA                        0xA2
+#define WS2IN7_POWER_OPTIMIZATION                   0xF8
 
 extern const unsigned char lut_vcom_dc[];
 extern const unsigned char lut_ww[];
@@ -67,14 +67,6 @@ extern const unsigned char lut_wb[];
 class Waveshare_2in7 : public LOLIN_EPD {
 public:
 
-  Waveshare_2in7(int    width,
-                 int    height,
-                 int8_t SID,
-                 int8_t SCLK,
-                 int8_t DC,
-                 int8_t RST,
-                 int8_t CS,
-                 int8_t BUSY = -1);
   Waveshare_2in7(int    width,
                  int    height,
                  int8_t DC,
