193401484dc302a52b1d90ff69016c2f141cee50
Ton Huisman <tonhu2004@hotmail.com>
2022-03-27 15:54:47 +0200

    [P096] Correction to rotation calculation by kretzp for Waveshare 2.7"

--- lib/LOLIN_EPD/src/Waveshare_2in7.cpp
+++ lib/LOLIN_EPD/src/Waveshare_2in7.cpp
@@ -264,19 +264,22 @@ void Waveshare_2in7::update() {
 void Waveshare_2in7::drawPixel(int16_t x, int16_t y, uint16_t color) {
   uint8_t *pBuf;
 
+  // Corrections by @kretzp (Peter Kretz) 2022-03-27
   // check rotation, move pixel around if necessary
   switch (getRotation()) {
-    case 1:
+    case 0:
       EPD_swap(x, y);
-      x = WIDTH - x - 1;
+      y = WIDTH - y - 1;
+      break;
+    case 1:
       break;
     case 2:
-      x = WIDTH - x - 1;
-      y = HEIGHT - y - 1;
+      EPD_swap(x, y);
+      x = HEIGHT - x - 1;
       break;
     case 3:
-      EPD_swap(x, y);
-      y = HEIGHT - y - 1;
+      x = HEIGHT - x - 1;
+      y = WIDTH - y - 1;
       break;
   }
 
