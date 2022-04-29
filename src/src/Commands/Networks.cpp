fdcf8b7e810c1ad707f17fe08ce5585a62f4f0cd
TD-er <gijs.noorlander@gmail.com>
2022-04-29 13:29:55 +0200

    [Ethernet] Fix IP related commands showing WiFi IP values

--- src/src/Commands/Networks.cpp
+++ src/src/Commands/Networks.cpp
@@ -2,6 +2,7 @@
 
 #include "../../ESPEasy_common.h"
 #include "../Commands/Common.h"
+#include "../ESPEasyCore/ESPEasyNetwork.h"
 #include "../Globals/Settings.h"
 #include "../WebServer/AccessControl.h"
 
@@ -25,22 +26,22 @@ String Command_AccessInfo_Clear (struct EventStruct *event, const char* Line)
 
 String Command_DNS (struct EventStruct *event, const char* Line)
 {
-  return Command_GetORSetIP(event, F("DNS:"), Line, Settings.DNS,WiFi.dnsIP(0),1);
+  return Command_GetORSetIP(event, F("DNS:"), Line, Settings.DNS, NetworkDnsIP(0), 1);
 }
 
 String Command_Gateway (struct EventStruct *event, const char* Line)
 {
-  return Command_GetORSetIP(event, F("Gateway:"), Line, Settings.Gateway,WiFi.gatewayIP(),1);
+  return Command_GetORSetIP(event, F("Gateway:"), Line, Settings.Gateway, NetworkGatewayIP(),1);
 }
 
 String Command_IP (struct EventStruct *event, const char* Line)
 {
-  return Command_GetORSetIP(event, F("IP:"), Line, Settings.IP,WiFi.localIP(),1);
+  return Command_GetORSetIP(event, F("IP:"), Line, Settings.IP, NetworkLocalIP(),1);
 }
 
 String Command_Subnet (struct EventStruct *event, const char* Line)
 {
-  return Command_GetORSetIP(event, F("Subnet:"), Line, Settings.Subnet,WiFi.subnetMask(),1);
+  return Command_GetORSetIP(event, F("Subnet:"), Line, Settings.Subnet, NetworkSubnetMask(), 1);
 }
 
 #ifdef HAS_ETHERNET
