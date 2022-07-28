2d574f776ed5c4dcfcb3f4d45c9becedcf493df2
Ton Huisman <tonhu2004@hotmail.com>
2022-07-28 20:51:39 +0200

    [Build] Rename USES_MODBUS to FEATURE_MODBUS and use 0/1 state

--- src/src/CustomBuild/check_defines_custom.h
+++ src/src/CustomBuild/check_defines_custom.h
@@ -132,6 +132,12 @@
 # undef USES_NOTIFIER
 #endif // ifdef USES_NOTIFIER
 
+#ifdef USES_MODBUS
+# warning "Custom.h has '#define USES_MODBUS' to be replaced with '#define FEATURE_MODBUS 1', see https://github.com/letscontrolit/ESPEasy/pull/4153"
+# define FEATURE_MODBUS  1
+# undef USES_MODBUS
+#endif // ifdef USES_MODBUS
+
 /* *INDENT-ON* */
 
 #endif // ifndef CUSTOMBUILD_CHECK_DEFINES_CUSTOM_H
