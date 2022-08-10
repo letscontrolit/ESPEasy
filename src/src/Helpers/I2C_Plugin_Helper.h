acbcfc2f3a2485d8fb9e6f719fa5ef870e4fad81
Ton Huisman <tonhu2004@hotmail.com>
2022-08-10 13:58:19 +0200

    [I2C] invert bool argument name and value for readability

--- src/src/Helpers/I2C_Plugin_Helper.h
+++ src/src/Helpers/I2C_Plugin_Helper.h
@@ -11,6 +11,6 @@
 #include "../WebServer/HTML_wrappers.h"
 
 bool checkI2CConfigValid_toHtml(taskIndex_t taskIndex,
-                                bool        onlyCheck = false);
+                                bool        outputToHtml = true);
 
 #endif // ifndef _SRC_HELPERS_I2C_PLUGIN_HELPER_H
