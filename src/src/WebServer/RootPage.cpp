515b43e0e17b62b7d727d89c9c9b0795b6f5fd0e
TD-er <gijs.noorlander@gmail.com>
2022-09-02 22:18:36 +0200

    [Cleanup] Uncrustify WebServer/RootPage.cpp

--- src/src/WebServer/RootPage.cpp
+++ src/src/WebServer/RootPage.cpp
@@ -3,75 +3,75 @@
 
 #ifdef WEBSERVER_ROOT
 
-#include "../WebServer/ESPEasy_WebServer.h"
-#include "../WebServer/HTML_wrappers.h"
-#include "../WebServer/LoadFromFS.h"
-#include "../WebServer/Markup.h"
-#include "../WebServer/Markup_Buttons.h"
-#include "../WebServer/Markup_Forms.h"
+# include "../WebServer/ESPEasy_WebServer.h"
+# include "../WebServer/HTML_wrappers.h"
+# include "../WebServer/LoadFromFS.h"
+# include "../WebServer/Markup.h"
+# include "../WebServer/Markup_Buttons.h"
+# include "../WebServer/Markup_Forms.h"
 
-#include "../Commands/InternalCommands.h"
-#include "../ESPEasyCore/ESPEasyNetwork.h"
-#include "../Globals/ESPEasy_time.h"
-#include "../Globals/ESPEasyWiFiEvent.h"
-#include "../Globals/MainLoopCommand.h"
-#include "../Globals/NetworkState.h"
-#include "../Globals/Nodes.h"
-#include "../Globals/Settings.h"
-#include "../Globals/Statistics.h"
-#include "../Helpers/ESPEasy_Storage.h"
-#include "../Helpers/Memory.h"
-#include "../Helpers/Misc.h"
-#include "../Helpers/StringGenerator_System.h"
-#include "../Helpers/WebServer_commandHelper.h"
-
-
-#include "../../ESPEasy-Globals.h"
-
-#if FEATURE_MQTT
-# include "../Globals/MQTT.h"
-# include "../Helpers/PeriodicalActions.h" // For finding enabled MQTT controller
-#endif
+# include "../Commands/InternalCommands.h"
+# include "../ESPEasyCore/ESPEasyNetwork.h"
+# include "../Globals/ESPEasy_time.h"
+# include "../Globals/ESPEasyWiFiEvent.h"
+# include "../Globals/MainLoopCommand.h"
+# include "../Globals/NetworkState.h"
+# include "../Globals/Nodes.h"
+# include "../Globals/Settings.h"
+# include "../Globals/Statistics.h"
+# include "../Helpers/ESPEasy_Storage.h"
+# include "../Helpers/Memory.h"
+# include "../Helpers/Misc.h"
+# include "../Helpers/StringGenerator_System.h"
+# include "../Helpers/WebServer_commandHelper.h"
 
 
-#ifndef MAIN_PAGE_SHOW_BASIC_INFO_NOT_LOGGED_IN
-  #define MAIN_PAGE_SHOW_BASIC_INFO_NOT_LOGGED_IN false
-#endif
+# include "../../ESPEasy-Globals.h"
+
+# if FEATURE_MQTT
+#  include "../Globals/MQTT.h"
+#  include "../Helpers/PeriodicalActions.h" // For finding enabled MQTT controller
+# endif // if FEATURE_MQTT
+
+
+# ifndef MAIN_PAGE_SHOW_BASIC_INFO_NOT_LOGGED_IN
+  #  define MAIN_PAGE_SHOW_BASIC_INFO_NOT_LOGGED_IN false
+# endif // ifndef MAIN_PAGE_SHOW_BASIC_INFO_NOT_LOGGED_IN
 
 // Define main page elements present
-#ifndef MAIN_PAGE_SHOW_SYSINFO_BUTTON
-  #define MAIN_PAGE_SHOW_SYSINFO_BUTTON    true
-#endif
+# ifndef MAIN_PAGE_SHOW_SYSINFO_BUTTON
+  #  define MAIN_PAGE_SHOW_SYSINFO_BUTTON    true
+# endif // ifndef MAIN_PAGE_SHOW_SYSINFO_BUTTON
 
-#ifndef MAIN_PAGE_SHOW_WiFi_SETUP_BUTTON
-  #define MAIN_PAGE_SHOW_WiFi_SETUP_BUTTON   false
-#endif
+# ifndef MAIN_PAGE_SHOW_WiFi_SETUP_BUTTON
+  #  define MAIN_PAGE_SHOW_WiFi_SETUP_BUTTON   false
+# endif // ifndef MAIN_PAGE_SHOW_WiFi_SETUP_BUTTON
 
-#ifndef MAIN_PAGE_SHOW_NODE_LIST_BUILD
-  #define MAIN_PAGE_SHOW_NODE_LIST_BUILD   true
-#endif
-#ifndef MAIN_PAGE_SHOW_NODE_LIST_TYPE
-  #define MAIN_PAGE_SHOW_NODE_LIST_TYPE    true
-#endif
+# ifndef MAIN_PAGE_SHOW_NODE_LIST_BUILD
+  #  define MAIN_PAGE_SHOW_NODE_LIST_BUILD   true
+# endif // ifndef MAIN_PAGE_SHOW_NODE_LIST_BUILD
+# ifndef MAIN_PAGE_SHOW_NODE_LIST_TYPE
+  #  define MAIN_PAGE_SHOW_NODE_LIST_TYPE    true
+# endif // ifndef MAIN_PAGE_SHOW_NODE_LIST_TYPE
 
 
 // ********************************************************************************
 // Web Interface root page
 // ********************************************************************************
 void handle_root() {
-  #ifndef BUILD_NO_RAM_TRACKER
+  # ifndef BUILD_NO_RAM_TRACKER
   checkRAM(F("handle_root"));
-  #endif
+  # endif // ifndef BUILD_NO_RAM_TRACKER
 
- if (captivePortal()) { // If captive portal redirect instead of displaying the page.
-   return;
- }
+  if (captivePortal()) { // If captive portal redirect instead of displaying the page.
+    return;
+  }
 
   // if Wifi setup, launch setup wizard if AP_DONT_FORCE_SETUP is not set.
- if (WiFiEventData.wifiSetup && !Settings.ApDontForceSetup())
+  if (WiFiEventData.wifiSetup && !Settings.ApDontForceSetup())
   {
     web_server.send(200, F("text/html"), F("<meta HTTP-EQUIV='REFRESH' content='0; url=/setup'>"));
-   return;
+    return;
   }
 
   if (!MAIN_PAGE_SHOW_BASIC_INFO_NOT_LOGGED_IN) {
@@ -84,12 +84,13 @@ void handle_root() {
 
   // if index.htm exists on FS serve that one (first check if gziped version exists)
   if (loadFromFS(F("/index.htm.gz"))) { return; }
+
   if (loadFromFS(F("/index.htm"))) { return; }
 
   TXBuffer.startStream();
 
   boolean rebootCmd = false;
-  String sCommand  = webArg(F("cmd"));
+  String  sCommand  = webArg(F("cmd"));
   rebootCmd = strcasecmp_P(sCommand.c_str(), PSTR("reboot")) == 0;
   sendHeadandTail_stdtemplate(_HEAD, rebootCmd);
 
@@ -151,93 +152,94 @@ void handle_root() {
     }
     addRowLabel(LabelType::LOAD_PCT);
 
-      if (wdcounter > 0)
-      {
-        addHtmlFloat(getCPUload());
-        addHtml(F("% (LC="));
-        addHtmlInt(getLoopCountPerSec());
-        addHtml(')');
-      }
-      {
-        addRowLabel(LabelType::FREE_MEM);
-        addHtmlInt(freeMem);
-        #ifndef BUILD_NO_RAM_TRACKER
-        addHtml(F(" ("));
-        addHtmlInt(lowestRAM);
-        addHtml(F(" - "));
-        addHtml(lowestRAMfunction);
-        addHtml(')');
-        #endif
-      }
-      {
-        #ifdef USE_SECOND_HEAP
+    if (wdcounter > 0)
+    {
+      addHtmlFloat(getCPUload());
+      addHtml(F("% (LC="));
+      addHtmlInt(getLoopCountPerSec());
+      addHtml(')');
+    }
+    {
+      addRowLabel(LabelType::FREE_MEM);
+      addHtmlInt(freeMem);
+        # ifndef BUILD_NO_RAM_TRACKER
+      addHtml(F(" ("));
+      addHtmlInt(lowestRAM);
+      addHtml(F(" - "));
+      addHtml(lowestRAMfunction);
+      addHtml(')');
+        # endif // ifndef BUILD_NO_RAM_TRACKER
+    }
+    {
+        # ifdef USE_SECOND_HEAP
       addRowLabelValue(LabelType::FREE_HEAP_IRAM);
-      #endif
-      }
-      {
-        addRowLabel(LabelType::FREE_STACK);
-        addHtmlInt(getCurrentFreeStack());
-        #ifndef BUILD_NO_RAM_TRACKER
-        addHtml(F(" ("));
-        addHtmlInt(lowestFreeStack);
-        addHtml(F(" - "));
-        addHtml(lowestFreeStackfunction);
-        addHtml(')');
-        #endif
-      }
+      # endif // ifdef USE_SECOND_HEAP
+    }
+    {
+      addRowLabel(LabelType::FREE_STACK);
+      addHtmlInt(getCurrentFreeStack());
+        # ifndef BUILD_NO_RAM_TRACKER
+      addHtml(F(" ("));
+      addHtmlInt(lowestFreeStack);
+      addHtml(F(" - "));
+      addHtml(lowestFreeStackfunction);
+      addHtml(')');
+        # endif // ifndef BUILD_NO_RAM_TRACKER
+    }
 
-  #if FEATURE_ETHERNET
-      addRowLabelValue(LabelType::ETH_WIFI_MODE);
-  #endif // if FEATURE_ETHERNET
+  # if FEATURE_ETHERNET
+    addRowLabelValue(LabelType::ETH_WIFI_MODE);
+  # endif // if FEATURE_ETHERNET
 
-      if (!WiFiEventData.WiFiDisconnected())
-      {
-        addRowLabelValue(LabelType::IP_ADDRESS);
-        addRowLabel(LabelType::WIFI_RSSI);
-        addHtmlInt(WiFi.RSSI());
-        addHtml(F(" dBm ("));
-        addHtml(WiFi.SSID());
-        addHtml(')');
-      }
+    if (!WiFiEventData.WiFiDisconnected())
+    {
+      addRowLabelValue(LabelType::IP_ADDRESS);
+      addRowLabel(LabelType::WIFI_RSSI);
+      addHtmlInt(WiFi.RSSI());
+      addHtml(F(" dBm ("));
+      addHtml(WiFi.SSID());
+      addHtml(')');
+    }
 
-  #if FEATURE_ETHERNET
-    if(active_network_medium == NetworkMedium_t::Ethernet) {
-        addRowLabelValue(LabelType::ETH_SPEED_STATE);
-        addRowLabelValue(LabelType::ETH_IP_ADDRESS);
-      }
-  #endif // if FEATURE_ETHERNET
+  # if FEATURE_ETHERNET
 
-      #if FEATURE_MDNS
-      {
-        addRowLabel(LabelType::M_DNS);
-        addHtml(F("<a href='http://"));
-        addHtml(getValue(LabelType::M_DNS));
-        addHtml(F("'>"));
-        addHtml(getValue(LabelType::M_DNS));
-        addHtml(F("</a>"));
-      }
-      #endif // if FEATURE_MDNS
+    if (active_network_medium == NetworkMedium_t::Ethernet) {
+      addRowLabelValue(LabelType::ETH_SPEED_STATE);
+      addRowLabelValue(LabelType::ETH_IP_ADDRESS);
+    }
+  # endif // if FEATURE_ETHERNET
 
-    #if FEATURE_MQTT
+      # if FEATURE_MDNS
+    {
+      addRowLabel(LabelType::M_DNS);
+      addHtml(F("<a href='http://"));
+      addHtml(getValue(LabelType::M_DNS));
+      addHtml(F("'>"));
+      addHtml(getValue(LabelType::M_DNS));
+      addHtml(F("</a>"));
+    }
+      # endif // if FEATURE_MDNS
+
+    # if FEATURE_MQTT
     {
       if (validControllerIndex(firstEnabledMQTT_ControllerIndex())) {
         addRowLabel(F("MQTT Client Connected"));
         addEnabled(MQTTclient_connected);
       }
     }
-    #endif
+    # endif // if FEATURE_MQTT
 
 
-    #if MAIN_PAGE_SHOW_SYSINFO_BUTTON
+    # if MAIN_PAGE_SHOW_SYSINFO_BUTTON
     html_TR_TD();
     html_TD();
     addButton(F("sysinfo"), F("More info"));
-    #endif
-    #if MAIN_PAGE_SHOW_WiFi_SETUP_BUTTON
+    # endif // if MAIN_PAGE_SHOW_SYSINFO_BUTTON
+    # if MAIN_PAGE_SHOW_WiFi_SETUP_BUTTON
     html_TR_TD();
     html_TD();
     addButton(F("setup"), F("WiFi Setup"));
-    #endif
+    # endif // if MAIN_PAGE_SHOW_WiFi_SETUP_BUTTON
 
     if (loggedIn) {
       if (printWebString.length() > 0)
@@ -248,37 +250,42 @@ void handle_root() {
         addRowLabel(F("Command"));
         addHtml(sCommand);
 
-          addHtml(F("<TR><TD colspan='2'>Command Output<BR><textarea readonly rows='10' wrap='on'>"));
-          addHtml(printWebString);
-          addHtml(F("</textarea>"));
-          printWebString = String();
-        }
+        addHtml(F("<TR><TD colspan='2'>Command Output<BR><textarea readonly rows='10' wrap='on'>"));
+        addHtml(printWebString);
+        addHtml(F("</textarea>"));
+        printWebString = String();
       }
-      html_end_table();
-      
-    #if FEATURE_ESPEASY_P2P
-      html_BR();
-      if (Settings.Unit == 0 && Settings.UDPPort != 0) addFormNote(F("Warning: Unit number is 0, please change it if you want to send data to other units."));
-      html_BR();
+    }
+    html_end_table();
+
+    # if FEATURE_ESPEASY_P2P
+    html_BR();
+
+    if ((Settings.Unit == 0) &&
+        (Settings.UDPPort != 0)) { addFormNote(F("Warning: Unit number is 0, please change it if you want to send data to other units.")); }
+    html_BR();
     html_table_class_multirow_noborder();
     html_TR();
     html_table_header(F("Node List"));
     html_table_header(F("Name"));
+
     if (MAIN_PAGE_SHOW_NODE_LIST_BUILD) {
       html_table_header(getLabel(LabelType::BUILD_DESC));
     }
+
     if (MAIN_PAGE_SHOW_NODE_LIST_TYPE) {
       html_table_header(F("Type"));
     }
     html_table_header(F("IP"), 160); // Should fit "255.255.255.255"
     html_table_header(F("Load"));
     html_table_header(F("Age (s)"));
-    #ifdef USES_ESPEASY_NOW
+    #  ifdef USES_ESPEASY_NOW
+
     if (Settings.UseESPEasyNow()) {
       html_table_header(F("Dist"));
       html_table_header(F("Peer Info"), 160);
     }
-    #endif
+    #  endif // ifdef USES_ESPEASY_NOW
 
     for (auto it = Nodes.begin(); it != Nodes.end(); ++it)
     {
@@ -311,10 +318,12 @@ void handle_root() {
           }
           html_TD();
         }
+
         if (MAIN_PAGE_SHOW_NODE_LIST_TYPE) {
           addHtml(it->second.getNodeTypeDisplayString());
           html_TD();
         }
+
         if (it->second.ip[0] != 0)
         {
           html_add_wide_button_prefix();
@@ -322,28 +331,33 @@ void handle_root() {
           addHtml(F("http://"));
           addHtml(it->second.IP().toString());
           uint16_t port = it->second.webgui_portnumber;
-          if (port !=0 && port != 80) {
+
+          if ((port != 0) && (port != 80)) {
             addHtml(':');
             addHtmlInt(port);
           }
           addHtml('\'', '>');
           addHtml(it->second.IP().toString());
           addHtml(F("</a>"));
-        } 
+        }
         html_TD();
         const float load = it->second.getLoad();
+
         if (load > 0.1) {
           addHtmlFloat(load);
         }
         html_TD();
-        addHtmlInt(static_cast<uint32_t>(it->second.getAge()/1000)); // time in seconds
-        #ifdef USES_ESPEASY_NOW
+        addHtmlInt(static_cast<uint32_t>(it->second.getAge() / 1000)); // time in seconds
+        #  ifdef USES_ESPEASY_NOW
+
         if (Settings.UseESPEasyNow()) {
           html_TD();
+
           if (it->second.distance != 255) {
-            addHtmlInt(it->second.distance); 
+            addHtmlInt(it->second.distance);
           }
           html_TD();
+
           if (it->second.ESPEasyNowPeer) {
             addHtml(F(ESPEASY_NOW_NAME));
             addHtml(' ');
@@ -351,24 +365,26 @@ void handle_root() {
             addHtml(F(" (ch: "));
             addHtmlInt(it->second.channel);
             int8_t rssi = it->second.getRSSI();
+
             if (rssi < 0) {
               addHtml(' ');
               addHtmlInt(rssi);
             }
             addHtml(')');
-            const ESPEasy_now_traceroute_struct* trace = Nodes.getDiscoveryRoute(it->second.unit);
+            const ESPEasy_now_traceroute_struct *trace = Nodes.getDiscoveryRoute(it->second.unit);
+
             if (trace != nullptr) {
               addHtml(' ');
               addHtml(trace->toString());
-            }          
+            }
           }
         }
-        #endif
+        #  endif // ifdef USES_ESPEASY_NOW
       }
     }
 
     html_end_table();
-  #endif
+  # endif // if FEATURE_ESPEASY_P2P
     html_end_form();
 
     printWebString = String();
