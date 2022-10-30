20e60d65c0908a66e33cb772cfefa7c110909c95
Ton Huisman <tonhu2004@hotmail.com>
2022-10-30 22:24:42 +0100

    [UI] Add `disabled` feature for numeric input fields

--- src/src/WebServer/Markup.cpp
+++ src/src/WebServer/Markup.cpp
@@ -642,15 +642,16 @@ void addCheckBox(const String& id, bool    checked, bool disabled
 // ********************************************************************************
 // Add a numeric box
 // ********************************************************************************
-void addNumericBox(const __FlashStringHelper *id, int value, int min, int max)
+void addNumericBox(const __FlashStringHelper *id, int value, int min, int max, bool disabled)
 {
-  addNumericBox(String(id), value, min, max);
+  addNumericBox(String(id), value, min, max, disabled);
 }
 
 void addNumericBox(const String& id, int value, int min, int max
                    #if FEATURE_TOOLTIPS
                    , const __FlashStringHelper * classname, const String& tooltip
                    #endif // if FEATURE_TOOLTIPS
+                   , bool disabled
                    )
 {
   addHtml(F("<input "));
@@ -668,6 +669,10 @@ void addNumericBox(const String& id, int value, int min, int max
     addHtmlAttribute(F("title"), tooltip);
   }
   #endif // if FEATURE_TOOLTIPS
+
+  if (disabled) {
+    addDisabled();
+  }
 
   if (value < min) {
     value = min;
@@ -691,9 +696,9 @@ void addNumericBox(const String& id, int value, int min, int max
 }
 
 #if FEATURE_TOOLTIPS
-void addNumericBox(const String& id, int value, int min, int max)
+void addNumericBox(const String& id, int value, int min, int max, bool disabled)
 {
-  addNumericBox(id, value, min, max, F("widenumber"));
+  addNumericBox(id, value, min, max, F("widenumber"), EMPTY_STRING, disabled);
 }
 
 #endif // if FEATURE_TOOLTIPS
