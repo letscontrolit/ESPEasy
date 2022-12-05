86baf37da904c7bb30eb3299b06d5ac2ea388ca1
oiiooiiooiio <102457370+oiiooiiooiio@users.noreply.github.com>
2022-12-05 22:43:38 +0800

    Update P087_data_struct.cpp
    [P087] Add serialproxy_writehex command and support serial read hex

--- src/src/PluginStructs/P087_data_struct.cpp
+++ src/src/PluginStructs/P087_data_struct.cpp
@@ -33,6 +33,8 @@ bool P087_data_struct::init(ESPEasySerialPort port, const int16_t serial_rx, con
   reset();
   easySerial = new (std::nothrow) ESPeasySerial(port, serial_rx, serial_tx);
 
+  tx_gpio_serial = serial_tx;
+
   if (isInitialized()) {
     # if defined(ESP8266)
     easySerial->begin(baudrate, (SerialConfig)config);
@@ -45,12 +47,79 @@ bool P087_data_struct::init(ESPEasySerialPort port, const int16_t serial_rx, con
 }
 
 void P087_data_struct::post_init() {
+  # ifndef BUILD_NO_DEBUG
+  String log = F("P087_data_init:");
+  #endif
+  hex_head_match   = 0;
+  hex_head_match_a = false;
+  hex_head_match_b = false;
+  sentence_part    = String();
+  hex_head_empty   = _lines[P087_HEX_HEADER_POS].isEmpty();
+  hex_data_length  = _lines[P087_HEX_DATA_LEN_POS].toInt();
+  # ifndef BUILD_NO_DEBUG
+  log += F(" HEX Data Length: ");
+  log += String(hex_data_length);
+  #endif
+  if (hex_data_length > 0) {
+    hex_data_length  = _lines[P087_HEX_DATA_LEN_POS].toInt() * 2;
+    max_length       = 2048;
+  }
+  if (hex_data_length < 0) {
+    hex_data_add_length  = _lines[P087_HEX_DATA_LEN_ADD_POS].toInt();
+    # ifndef BUILD_NO_DEBUG
+    log += F(" HEX Data Adding Length: ");
+    log += String(hex_data_add_length);
+    #endif
+    hex_data_add_length  = _lines[P087_HEX_DATA_LEN_ADD_POS].toInt() * 2;
+    max_length       = 2048;
+  }
+  if (hex_data_length != 0 && !hex_head_empty) {
+    String buf = _lines[P087_HEX_HEADER_POS];
+    # ifndef BUILD_NO_DEBUG
+    log += F(" HEX Header: ");
+    log += buf;
+    #endif
+    uint16_t len = buf.length();
+    head_sentence_length = (len / 2);
+    # ifndef BUILD_NO_DEBUG
+    log += F(" HEX Header length: ");
+    log += String(head_sentence_length);
+    #endif
+    for(int i = 0; i < len / 2 && !hex_head_empty; i++) {
+      uint8_t uc = buf[i * 2], dc = buf[i * 2 + 1];
+      if(uc >= 48 && uc <= 57) {
+        uc -= 48;
+      } else if(uc >= 65 && uc <= 70) {
+        uc -= 55;
+      } else if(uc >= 97 && uc <= 102) {
+        uc -= 87;
+      } else {
+        hex_head_empty = true;
+      }
+
+      if(dc >= 48 && dc <= 57) {
+        dc -= 48;
+      } else if(dc >= 65 && dc <= 70) {
+        dc -= 55;
+      } else if(dc >= 97 && dc <= 102) {
+        dc -= 87;
+      } else {
+        hex_head_empty = true;
+      }
+      if(!hex_head_empty) {
+        head_sentence[i] = (uc << 4 | dc);
+      }
+    }
+  }
+  # ifndef BUILD_NO_DEBUG
+  addLogMove(LOG_LEVEL_DEBUG, log);
+  #endif
   for (uint8_t i = 0; i < P87_MAX_CAPTURE_INDEX; ++i) {
     capture_index_used[i] = false;
   }
   regex_empty = _lines[P087_REGEX_POS].isEmpty();
   # ifndef BUILD_NO_DEBUG
-  String log = F("P087_post_init:");
+  log = F("P087_post_init:");
   #endif
 
   for (uint8_t i = 0; i < P087_NR_FILTERS; ++i) {
@@ -94,11 +163,52 @@ void P087_data_struct::sendString(const String& data) {
   }
 }
 
+void P087_data_struct::sendStringhex(const String& data) {
+  if (isInitialized()) {
+    if (data.length() > 0) {
+      setDisableFilterWindowTimer();
+      unsigned int len = data.length();
+      for(unsigned int i=0; i < len / 2; i++){
+        uint8_t uc = data[i * 2], dc = data[i * 2 + 1];
+        if(uc >= 48 && uc <= 57) {
+          uc -= 48;
+        } else if(uc >= 65 && uc <= 70) {
+          uc -= 55;
+        } else if(uc >= 97 && uc <= 102) {
+          uc -= 87;
+        } else {
+          continue;
+        }
+
+        if(dc >= 48 && dc <= 57) {
+          dc -= 48;
+        } else if(dc >= 65 && dc <= 70) {
+          dc -= 55;
+        } else if(dc >= 97 && dc <= 102) {
+          dc -= 87;
+        } else {
+          continue;
+        }
+        easySerial->write(uc << 4 | dc);
+      }
+
+      if (loglevelActiveFor(LOG_LEVEL_INFO)) {
+        String log = F("Proxy: SendingHEX: ");
+        log += data;
+        addLogMove(LOG_LEVEL_INFO, log);
+      }
+    }
+  }
+}
+
 bool P087_data_struct::loop() {
   if (!isInitialized()) {
     return false;
   }
   bool fullSentenceReceived = false;
+  bool valid                = false;
+  sentence_part_length      = 0;
+  char temp_c[3];
 
   if (easySerial != nullptr) {
     int available = easySerial->available();
@@ -112,14 +222,75 @@ bool P087_data_struct::loop() {
         available = easySerial->available();
         delay(0);
       }
-
+// - 00 01 02 03 04 05 06 07
+// - 08 09 0A 0B 0C 0D 0E 0F
+// - 10 11 12 13 14 FE FF
+      if (hex_data_length != 0 && !hex_head_empty) {
+        if (!hex_head_empty) {
+          valid = false;
+          if (!hex_head_match_a && !hex_head_match_b && head_sentence[hex_head_match] == c) {
+            ++hex_head_match;
+            if (hex_head_match >= head_sentence_length) {
+              hex_head_match_a = true; // First match head, the next char start reading
+              hex_head_match = 0;
+              if (max_length_reached()) { fullSentenceReceived = true; }
+              continue;
+            }
+          }
+          if (hex_head_match >= head_sentence_length) {
+              hex_head_match = 0;
+          }
+          if (hex_head_match_a || hex_head_match_b) { // match head
+            sprintf(temp_c, "%02x", c);
+            sentence_part += String(temp_c);
+            sentence_part_length = sentence_part.length();
+            if (hex_data_read_length == 0 && hex_data_length < 0 && sentence_part_length / 2 == hex_data_length * -1) {
+              hex_data_read_length = c * 2 + hex_data_add_length;
+            }
+            if ((hex_data_length < 0 && hex_data_read_length > 0 && sentence_part_length >= hex_data_read_length) || (hex_data_length > 0 && sentence_part_length >= hex_data_length)) { // full hex data ending read
+              valid = true;
+              if (hex_head_match_b) {
+                hex_head_match_b = false;
+              }
+              if (hex_head_match_a) {
+                hex_head_match_a = false;
+              }
+            }
+            if (head_sentence[hex_head_match] == c) {
+              ++hex_head_match;
+              if (hex_head_match >= head_sentence_length) {
+                hex_head_match_b = true; // second match head, send sentence, the next char start reading
+                hex_head_match = 0;
+                valid = true;
+              }
+            }
+          }
+        } else if (hex_head_empty) {
+          sprintf(temp_c, "%02x", c);
+          sentence_part += String(temp_c);
+          sentence_part_length = sentence_part.length();
+          if (hex_data_read_length == 0 && hex_data_length < 0 && sentence_part_length / 2 == hex_data_length * -1) {
+            hex_data_read_length = c * 2 + hex_data_add_length;
+          }
+          if ((hex_data_length < 0 && hex_data_read_length > 0 && sentence_part_length >= hex_data_read_length) || (hex_data_length > 0 && sentence_part_length >= hex_data_length)) { // full hex data ending read
+            valid = true;
+          }
+        }
+        if (valid) { // send sentence
+          fullSentenceReceived = true;
+          last_sentence = sentence_part;
+          sentence_part = String();
+          hex_data_read_length = 0;
+        }
+      } else {
+// - ASCII 32 - 217
       switch (c) {
         case 13:
         {
-          const size_t length = sentence_part.length();
-          bool valid          = length > 0;
+          sentence_part_length = sentence_part.length();
+          valid                = sentence_part_length > 0;
 
-          for (size_t i = 0; i < length && valid; ++i) {
+          for (size_t i = 0; i < sentence_part_length && valid; ++i) {
             if ((sentence_part[i] > 127) || (sentence_part[i] < 32)) {
               sentence_part = String();
               ++sentences_received_error;
@@ -142,6 +313,7 @@ bool P087_data_struct::loop() {
           sentence_part += c;
           break;
       }
+      }
 
       if (max_length_reached()) { fullSentenceReceived = true; }
     }
@@ -150,6 +322,7 @@ bool P087_data_struct::loop() {
   if (fullSentenceReceived) {
     ++sentences_received;
     length_last_received = last_sentence.length();
+    hex_data_read_length = 0;
   }
   return fullSentenceReceived;
 }
@@ -161,6 +334,12 @@ bool P087_data_struct::getSentence(String& string) {
   }
   last_sentence = String();
   return true;
+}
+
+void P087_data_struct::getSentencePart(String& string) const {
+  if (hex_data_length == 0 || hex_head_empty) {
+    string      = sentence_part;
+  }
 }
 
 void P087_data_struct::getSentencesReceived(uint32_t& succes, uint32_t& error, uint32_t& length_last) const {
@@ -177,6 +356,22 @@ void P087_data_struct::setLine(uint8_t varNr, const String& line) {
   if (varNr < P87_Nlines) {
     _lines[varNr] = line;
   }
+}
+
+int16_t P087_data_struct::getTXGpioSerial() const {
+  return tx_gpio_serial;
+}
+
+String P087_data_struct::getHEXHeader() const {
+  return _lines[P087_HEX_HEADER_POS];
+}
+
+int16_t P087_data_struct::getHEXDataLength() const {
+  return _lines[P087_HEX_DATA_LEN_POS].toInt();
+}
+
+uint16_t P087_data_struct::getHEXDataAddLength() const {
+  return _lines[P087_HEX_DATA_LEN_ADD_POS].toInt();
 }
 
 String P087_data_struct::getRegEx() const {
@@ -227,7 +422,7 @@ String P087_data_struct::getFilter(uint8_t lineNr, uint8_t& capture, P087_Filter
 {
   uint8_t varNr = lineNr * 3 + P087_FIRST_FILTER_POS;
 
-  if ((varNr + 3) > P87_Nlines) { return ""; }
+  if ((varNr + 3) > P087_NR_FILTERS_N) { return ""; }
 
   capture    = _lines[varNr++].toInt();
   comparator = _lines[varNr++] == "1" ? P087_Filter_Comp::NotEqual : P087_Filter_Comp::Equal;
@@ -280,7 +475,7 @@ bool P087_data_struct::matchRegexp(String& received) const {
   }
 
 
-  uint32_t regexp_match_length = getRegExpMatchLength();
+  uint16_t regexp_match_length = getRegExpMatchLength();
 
   if ((regexp_match_length > 0) && (strlength > regexp_match_length)) {
     strlength = regexp_match_length;
