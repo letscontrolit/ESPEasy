bb9b2b3abf7b0a3dec60b980e8f4add9fb0da228
TD-er <gijs.noorlander@gmail.com>
2023-02-13 16:24:34 +0100

    [TaskRun] Reschedule task to set Interval

--- src/src/Commands/Tasks.cpp
+++ src/src/Commands/Tasks.cpp
@@ -218,6 +218,7 @@ const __FlashStringHelper * Command_Task_ValueSetAndRun(struct EventStruct *even
   const __FlashStringHelper * returnvalue = taskValueSet(event, Line, taskIndex, success);
   if (success)
   {
+    Scheduler.reschedule_task_device_timer(taskIndex, millis());
     START_TIMER;
     SensorSendTask(taskIndex);
     STOP_TIMER(SENSOR_SEND_TASK);
@@ -264,7 +265,7 @@ const __FlashStringHelper * Command_Task_Run(struct EventStruct *event, const ch
   if (GetArgv(Line, par2, 2)) {
     validUIntFromString(par2, unixTime);
   }
-
+  Scheduler.reschedule_task_device_timer(taskIndex, millis());
   START_TIMER;
   SensorSendTask(taskIndex, unixTime);
   STOP_TIMER(SENSOR_SEND_TASK);
