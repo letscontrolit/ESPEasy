4c9af89912261ea2570639bc899ebc125730167b
TD-er <gijs.noorlander@gmail.com>
2023-03-08 12:25:33 +0100

    [Build] Fix missing network medium options

--- src/src/DataTypes/NetworkMedium.h
+++ src/src/DataTypes/NetworkMedium.h
@@ -3,10 +3,16 @@
 
 #include <Arduino.h>
 
+#include "../../ESPEasy_common.h"
+
 // Is stored in settings
-enum class NetworkMedium_t : unsigned char {
-  WIFI     = 0,
-  Ethernet = 1
+enum class NetworkMedium_t : uint8_t {
+  WIFI            = 0,
+  Ethernet        = 1,
+#ifdef USES_ESPEASY_NOW
+  ESPEasyNOW_only = 2,
+#endif
+  NotSet          = 255
 };
 
 bool   isValid(NetworkMedium_t medium);
