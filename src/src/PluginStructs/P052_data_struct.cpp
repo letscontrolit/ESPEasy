a533dcc698d42e5131d72593d42140d4e0829f31
TD-er <gijs.noorlander@gmail.com>
2023-03-16 09:59:57 +0100

    [SenseAir] Add more sensor info + begin implementing setting ABC

--- src/src/PluginStructs/P052_data_struct.cpp
+++ src/src/PluginStructs/P052_data_struct.cpp
@@ -19,19 +19,77 @@ bool P052_data_struct::isInitialized() const {
 }
 
 const __FlashStringHelper * P052_data_struct::Plugin_052_valuename(uint8_t value_nr, bool displayString) {
-  switch (value_nr) {
-    case 0:  return displayString ? F("Empty") : F("");
-    case 1:  return displayString ? F("Carbon Dioxide") : F("co2");
-    case 2:  return displayString ? F("Temperature") : F("T");
-    case 3:  return displayString ? F("Humidity") : F("H");
-    case 4:  return displayString ? F("Relay Status") : F("rel");
-    case 5:  return displayString ? F("Temperature Adjustment") : F("Tadj");
-    case 6:  return displayString ? F("ABC period") : F("abc_per");
-    case 7:  return displayString ? F("Error Status") : F("err");
-    default:
-      break;
+  const __FlashStringHelper *strings[] {
+    F("Empty"),                  F(""),
+    F("Carbon Dioxide"),         F("co2"),
+    F("Temperature"),            F("T"),
+    F("Humidity"),               F("H"),
+    F("Relay Status"),           F("rel"),
+    F("Temperature Adjustment"), F("Tadj"),
+    F("ABC period"),             F("abc_per"),
+    F("Error Status"),           F("err")
+  };
+  const size_t index         = 2 * value_nr + (displayString ? 0 : 1);
+  constexpr size_t nrStrings = sizeof(strings) / sizeof(strings[0]);
+
+  if (index < nrStrings) {
+    return strings[index];
   }
   return F("");
+}
+
+void P052_data_struct::setABCperiod(int hours)
+{
+  // Enable: write 1 ... 65534 to HR14 and bit1 set to 0 at HR19
+  // Disable: write 0 or 65535 to HR14 and bit1 set to 1 at HR19
+
+
+  // Read HR19
+  uint8_t errorcode = 0;
+  int     value     = modbus.readHoldingRegister(P052_HR19_METER_CONTROL, errorcode);
+
+  // Clear bit 1 in register and write back HR19
+  if (bitRead(value, 1)) {
+    bitClear(value, 1);
+    modbus.writeSingleRegister(P052_HR19_METER_CONTROL, value, errorcode);
+  }
+
+  // Read HR14 and verify desired ABC period
+  value = modbus.readHoldingRegister(P052_HR14_ABC_PERIOD, errorcode);
+
+  // If HR14 (ABC period) is not the desired period,
+  // write desired ABC period to HR14
+  if (value != hours) {
+    modbus.writeSingleRegister(P052_HR14_ABC_PERIOD, hours, errorcode);
+  }
+}
+
+uint32_t P052_data_struct::getSensorID()
+{
+  uint8_t errorcode       = 0;
+  const uint32_t sensorId = (modbus.readInputRegister(P052_IR30_SENSOR_ID_HIGH, errorcode) << 16) |
+                            modbus.readInputRegister(P052_IR31_SENSOR_ID_LOW, errorcode);
+
+  if (errorcode == 0) {
+    return sensorId;
+  }
+  return 0;
+}
+
+bool P052_data_struct::readInputRegister(short addr, int& value)
+{
+  uint8_t errorcode = 0;
+
+  value = modbus.readInputRegister(addr, errorcode);
+  return errorcode == 0;
+}
+
+bool P052_data_struct::readHoldingRegister(short addr, int& value)
+{
+  uint8_t errorcode = 0;
+
+  value = modbus.readHoldingRegister(addr, errorcode);
+  return errorcode == 0;
 }
 
 #endif // ifdef USES_P052
