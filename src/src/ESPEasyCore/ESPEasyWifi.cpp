a81d5638ae52d47f3c7dd96ff2e176d696629f0e
TD-er <gijs.noorlander@gmail.com>
2023-10-30 11:23:43 +0100

    [WiFi] Fix force B/G mode on ESP8266
    Also add automatic fallback to a mode supported by the access point when settings force a mode not supported by the access point.  (only on ESP32 or ESP8266 SDK3.x)

--- src/src/ESPEasyCore/ESPEasyWifi.cpp
+++ src/src/ESPEasyCore/ESPEasyWifi.cpp
@@ -1386,15 +1386,24 @@ bool wifiAPmodeActivelyUsed()
 
 void setConnectionSpeed() {
   #ifdef ESP8266
-  WiFiPhyMode_t phyMode = WIFI_PHY_MODE_11G;
+  // ESP8266 only supports 802.11g mode when running in STA+AP
   const bool forcedByAPmode = WifiIsAP(WiFi.getMode());
+  WiFiPhyMode_t phyMode = (Settings.ForceWiFi_bg_mode() || forcedByAPmode) ? WIFI_PHY_MODE_11G : WIFI_PHY_MODE_11N;
   if (!forcedByAPmode) {
-    // ESP8266 only supports 802.11g mode when running in STA+AP
-//    const WiFi_AP_Candidate candidate = WiFi_AP_Candidates.getCurrent();
-
-    bool useAlternate = WiFi_AP_Candidates.attemptsLeft == 0;
-    if (Settings.ForceWiFi_bg_mode() == useAlternate) {
-      phyMode = WIFI_PHY_MODE_11N;
+    const WiFi_AP_Candidate candidate = WiFi_AP_Candidates.getCurrent();
+    if (candidate.phy_known() && (candidate.phy_11g != candidate.phy_11n)) {
+      if ((WIFI_PHY_MODE_11G == phyMode) && !candidate.phy_11g) {
+        phyMode = WIFI_PHY_MODE_11N;
+        addLog(LOG_LEVEL_INFO, F("WIFI : AP is set to 802.11n only"));
+      } else if ((WIFI_PHY_MODE_11N == phyMode) && !candidate.phy_11n) {
+        phyMode = WIFI_PHY_MODE_11G;
+        addLog(LOG_LEVEL_INFO, F("WIFI : AP is set to 802.11g only"));
+      }      
+    } else {
+      bool useAlternate = WiFiEventData.connectionFailures > 10;
+      if (useAlternate) {
+        phyMode = (WIFI_PHY_MODE_11G == phyMode) ? WIFI_PHY_MODE_11N : WIFI_PHY_MODE_11G;
+      }
     }
   } else {
     // No need to perform a next attempt.
@@ -1427,6 +1436,19 @@ void setConnectionSpeed() {
     // Set to use BGN
     protocol |= WIFI_PROTOCOL_11N;
   }
+
+  const WiFi_AP_Candidate candidate = WiFi_AP_Candidates.getCurrent();
+  if (candidate.phy_known()) {
+    // Check to see if the access point is set to "N-only"
+    if ((protocol & WIFI_PROTOCOL_11N) == 0) {
+      if (!candidate.phy_11b && !candidate.phy_11g && candidate.phy_11n) {
+        // Set to use BGN
+        protocol |= WIFI_PROTOCOL_11N;
+        addLog(LOG_LEVEL_INFO, F("WIFI : AP is set to 802.11n only"));
+      }
+    }
+  }
+
 
   if (WifiIsSTA(WiFi.getMode())) {
     esp_wifi_set_protocol(WIFI_IF_STA, protocol);
