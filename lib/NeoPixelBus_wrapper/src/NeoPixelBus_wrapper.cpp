38865542fbd33109d5e670808a3b9846398cb3b5
Ton Huisman <tonhu2004@hotmail.com>
2023-12-27 15:30:31 +0100

    [Lib] NeoPixelBus_Wrapper: Allow fall-back to Adafruit_NeoPixel lib for ESP8266

--- lib/NeoPixelBus_wrapper/src/NeoPixelBus_wrapper.cpp
+++ lib/NeoPixelBus_wrapper/src/NeoPixelBus_wrapper.cpp
@@ -5,36 +5,49 @@
 NeoPixelBus_wrapper::NeoPixelBus_wrapper(uint16_t     _maxPixels,
                                          int16_t      _gpioPin,
                                          neoPixelType _stripType)
-  : numLEDs(_maxPixels) {
+  #ifndef NEOPIXEL_WRAPPER_USE_ADAFRUIT
+  : numLEDs(_maxPixels)
+  #endif // ifndef NEOPIXEL_WRAPPER_USE_ADAFRUIT
+{
+  #ifdef NEOPIXEL_WRAPPER_USE_ADAFRUIT
+  neopixels = new (std::nothrow) Adafruit_NeoPixel(_maxPixels, _gpioPin, _stripType);
+  #else // ifdef NEOPIXEL_WRAPPER_USE_ADAFRUIT
+
   if (NEO_GRBW == (_stripType & NEO_GRBW)) {
-    #ifdef ESP8266
+    # ifdef ESP8266
     neopixels_grbw = new (std::nothrow) NEOPIXEL_LIB<NeoGrbwFeature, METHOD>(_maxPixels);
-    #endif // ifdef ESP8266
-    #ifdef ESP32
+    # endif // ifdef ESP8266
+    # ifdef ESP32
     neopixels_grbw = new (std::nothrow) NEOPIXEL_LIB<NeoGrbwFeature, METHOD>(_maxPixels, _gpioPin);
-    #endif // ifdef ESP32
+    # endif // ifdef ESP32
   } else if (NEO_GRB == (_stripType & NEO_GRB)) {
-    #ifdef ESP8266
+    # ifdef ESP8266
     neopixels_grb = new (std::nothrow) NEOPIXEL_LIB<NeoGrbFeature, METHOD>(_maxPixels);
-    #endif // ifdef ESP8266
-    #ifdef ESP32
+    # endif // ifdef ESP8266
+    # ifdef ESP32
     neopixels_grb = new (std::nothrow) NEOPIXEL_LIB<NeoGrbFeature, METHOD>(_maxPixels, _gpioPin);
-    #endif // ifdef ESP32
+    # endif // ifdef ESP32
   }
+  #endif // ifdef NEOPIXEL_WRAPPER_USE_ADAFRUIT
 }
 
 NeoPixelBus_wrapper::~NeoPixelBus_wrapper() {
+  #ifdef NEOPIXEL_WRAPPER_USE_ADAFRUIT
+  delete neopixels;
+  neopixels = nullptr;
+  #else // ifdef NEOPIXEL_WRAPPER_USE_ADAFRUIT
   delete neopixels_grb;
   neopixels_grb = nullptr;
   delete neopixels_grbw;
   neopixels_grbw = nullptr;
+  #endif // ifdef NEOPIXEL_WRAPPER_USE_ADAFRUIT
 }
 
+#ifndef NEOPIXEL_WRAPPER_USE_ADAFRUIT
 void NeoPixelBus_wrapper::begin() {
   if (nullptr != neopixels_grb) {
     neopixels_grb->Begin();
-  }
-
+  } else
   if (nullptr != neopixels_grbw) {
     neopixels_grbw->Begin();
   }
@@ -43,8 +56,7 @@ void NeoPixelBus_wrapper::begin() {
 void NeoPixelBus_wrapper::show(void) {
   if (nullptr != neopixels_grb) {
     neopixels_grb->Show();
-  }
-
+  } else
   if (nullptr != neopixels_grbw) {
     neopixels_grbw->Show();
   }
@@ -53,8 +65,7 @@ void NeoPixelBus_wrapper::show(void) {
 void NeoPixelBus_wrapper::setBrightness(uint8_t b) {
   if (nullptr != neopixels_grb) {
     neopixels_grb->SetBrightness(b);
-  }
-
+  } else
   if (nullptr != neopixels_grbw) {
     neopixels_grbw->SetBrightness(b);
   }
@@ -66,8 +77,7 @@ void NeoPixelBus_wrapper::setPixelColor(uint16_t pxl,
                                         uint8_t  b) {
   if (nullptr != neopixels_grb) {
     neopixels_grb->SetPixelColor(pxl, RgbColor(r, g, b));
-  }
-
+  } else
   if (nullptr != neopixels_grbw) {
     neopixels_grbw->SetPixelColor(pxl, RgbwColor(r, g, b));
   }
@@ -80,8 +90,7 @@ void NeoPixelBus_wrapper::setPixelColor(uint16_t pxl,
                                         uint8_t  w) {
   if (nullptr != neopixels_grb) {
     neopixels_grb->SetPixelColor(pxl, RgbColor(r, g, b));
-  }
-
+  } else
   if (nullptr != neopixels_grbw) {
     neopixels_grbw->SetPixelColor(pxl, RgbwColor(r, g, b, w));
   }
@@ -91,8 +100,7 @@ void NeoPixelBus_wrapper::setPixelColor(uint16_t pxl,
                                         uint32_t c) {
   if (nullptr != neopixels_grb) {
     neopixels_grb->SetPixelColor(pxl, RgbColor((c >> 16) & 0xFF, (c >> 8) & 0xFF, c & 0xFF)); // Unfold the Color(r,g,b,w) static
-  }
-
+  } else
   if (nullptr != neopixels_grbw) {
     neopixels_grbw->SetPixelColor(pxl, RgbwColor((c >> 16) & 0xFF, (c >> 8) & 0xFF, c & 0xFF, (c >> 24) & 0xFF));
   }
@@ -102,8 +110,7 @@ uint32_t NeoPixelBus_wrapper::getPixelColor(uint16_t n) {
   if (nullptr != neopixels_grb) {
     const RgbColor color = neopixels_grb->GetPixelColor(n);
     return Color(color.R, color.G, color.B);
-  }
-
+  } else
   if (nullptr != neopixels_grbw) {
     const RgbwColor color = neopixels_grbw->GetPixelColor(n);
     return Color(color.R, color.G, color.B, color.W);
@@ -112,4 +119,5 @@ uint32_t NeoPixelBus_wrapper::getPixelColor(uint16_t n) {
   return 0u; // Fall-through value...
 }
 
+#endif // ifndef NEOPIXEL_WRAPPER_USE_ADAFRUIT
 #endif // ifndef _NEOPIXELBUS_WRAPPER_CPP
