bfe5926589cb009b7dc5ed4b68f841946c5f2008
TD-er <gijs.noorlander@gmail.com>
2024-02-18 01:43:18 +0100

    [ESP32-C3/C6] Fix stalling on concurrent connections

--- src/src/DataStructs/Web_StreamingBuffer.cpp
+++ src/src/DataStructs/Web_StreamingBuffer.cpp
@@ -17,7 +17,7 @@
 #ifdef ESP8266
 #define CHUNKED_BUFFER_SIZE         512
 #else 
-#define CHUNKED_BUFFER_SIZE         1400
+#define CHUNKED_BUFFER_SIZE         1200
 #endif
 
 Web_StreamingBuffer::Web_StreamingBuffer(void) : lowMemorySkip(false),
