a2cba3178b6cd765566dd7646cab09ae80fda36f
Ton Huisman <tonhu2004@hotmail.com>
2024-04-10 22:29:37 +0200

    [P162] Add required include

--- src/src/PluginStructs/P162_data_struct.cpp
+++ src/src/PluginStructs/P162_data_struct.cpp
@@ -4,6 +4,8 @@
 
 # include "../PluginStructs/P162_data_struct.h"
 
+#include <SPI.h>
+
 // Needed also here for PlatformIO's library finder as the .h file
 // is in a directory which is excluded in the src_filter
 
