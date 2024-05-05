3d4c5d45b75ba276d46a01015cf4678d67762e53
Ton Huisman <tonhu2004@hotmail.com>
2024-05-05 17:01:15 +0200

    [P167] Add command `sen5x,techlog,<0|1>`

--- src/_P167_Vindstyrka.ino
+++ src/_P167_Vindstyrka.ino
@@ -6,6 +6,7 @@
 // #######################################################################################################
 
 /** Changelog:
+ * 2024-05-05 tonhuisman: Add subcommand sen5x,techlog,<1|0> to enable/disable Technical logging option. 0 = Off, any other value is on
  * 2024-04-20 tonhuisman: Replace dewpoint calculation by standard calculation, fix issue with status bits, reduce strings
  *                        Remove unneeded code and variables, move most defines to P167_data_struct.h
  *                        Implement Get Config Value to retrieve all available values from a single instance
@@ -364,6 +365,11 @@ boolean Plugin_167(uint8_t function, struct EventStruct *event, String& string) 
 
           if (equals(subcmd, F("startclean"))) {
             Plugin_167_SEN->startCleaning();
+            success = true;
+          } else
+          if (equals(subcmd, F("techlog"))) {
+            P167_ENABLE_LOG = event->Par2 == 0 ? 0 : 1;
+            Plugin_167_SEN->setLogging(P167_ENABLE_LOG);
             success = true;
           }
         }
