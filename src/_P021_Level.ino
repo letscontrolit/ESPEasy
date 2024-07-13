c749ad71232421bc3de527178b689e5badde4c43
flashmark <15078748+flashmark@users.noreply.github.com>
2024-07-13 21:57:51 +0200

    Fix backwards compatibility issue and tidy up

--- src/_P021_Level.ino
+++ src/_P021_Level.ino
@@ -32,7 +32,11 @@
 
 # define P021_GPIO_RELAY            CONFIG_PIN1
 
-//  Input signal is read from another task (plugin) by accessing TASK & VALUE
+// Define the configuration options using ESPeasy standard structures
+// For identification on the HTML form use P021_GUID_<option>
+// ------------------------------------------------------------------
+
+// Input signal is read from another task (plugin) by accessing its TASK & VALUE
 # define P021_CHECK_TASK            PCONFIG(0)
 # define P021_GUID_CHECK_TASK       "f0"
 # define P021_CHECK_VALUE           PCONFIG(1)
@@ -46,20 +50,20 @@
 // Use P021_getConfigBit() and P021_setConfigBit() to access the individual bits
 // For backwards compatibility first bit shall be equal original P021_INVERT_OUTPUT
 # define P021_FLAGS                 PCONFIG(3)
-# define P021_INV_OUTPUT            1 // Invert output signal
-# define P021_GUID_INV_OUTPUT       "b1"
-# define P021_EXT_FUNCT             2 // Extended functionality on setup screen
-# define P021_GUID_EXT_FUNCT        "b2"
-# define P021_INV_INPUT             3 // Invert input comparator
-# define P021_GUID_INV_INPUT        "b3"
-# define P021_SYM_HYSTERESIS        4 // Use symetrical hysteresis
-# define P021_GUID_SYM_HYSTERESIS   "b4"
-# define P021_SLOW_EVAL             5 // 1Hz evaluation i.s.o. 10Hz
-# define P021_GUID_SLOW_EVAL        "b5"
-# define P021_STATE_OUTP            6 // Add state as additional output value
-# define P021_GUID_STATE_OUTP       "b6"
-# define P021_EXTEND_END            7 // Extend state timed from start
-# define P021_GUID_EXTEND_END       "b7"
+# define P021_INV_OUTPUT            0 // Invert output signal
+# define P021_GUID_INV_OUTPUT       "b0"
+# define P021_EXT_FUNCT             1 // Extended functionality on setup screen
+# define P021_GUID_EXT_FUNCT        "b1"
+# define P021_INV_INPUT             2 // Invert input comparator
+# define P021_GUID_INV_INPUT        "b2"
+# define P021_SYM_HYSTERESIS        3 // Use symetrical hysteresis
+# define P021_GUID_SYM_HYSTERESIS   "b3"
+# define P021_SLOW_EVAL             4 // 1Hz evaluation i.s.o. 10Hz
+# define P021_GUID_SLOW_EVAL        "b4"
+# define P021_STATE_OUTP            5 // Add state as additional output value
+# define P021_GUID_STATE_OUTP       "b5"
+# define P021_EXTEND_END            6 // Extend state timed from start
+# define P021_GUID_EXTEND_END       "b6"
 
 // Minimum time output shall be active [min]
 # define P021_MIN_TIME              PCONFIG(4)
@@ -77,10 +81,15 @@
 # define P021_OPMODE                PCONFIG(7)
 # define P021_GUID_OPMODE           "f7"
 
+// Setpoint (set level)
 # define P021_SETPOINT              PCONFIG_FLOAT(0)
 # define P021_GUID_SETPOINT         "f8"
+
+// Hysteresis
 # define P021_HYSTERESIS            PCONFIG_FLOAT(1)
 # define P021_GUID_HYSTERESIS       "f9"
+
+// SETP_LAST_STORED is not available on form. Used as storage for autosave function
 # define P021_SETP_LAST_STORED      PCONFIG_FLOAT(2)
 # define P021_GUID_SETP_LAST_STORED "f10"
 # define P021_AUTOSAVE_TIMER        PCONFIG_ULONG(0)
@@ -88,12 +97,12 @@
 
 // Positions in userVar array for the output values of this plugin
 // For now we only advertise output & state. Autosave bookkeeping is hidden and used as static storage
-#define P021_VALUE_OUTPUT           0
-#define P021_VALUE_STATE            1
-#define P021_VALUE_AUTOSAVE_FLAG    2
-#define P021_VALUE_AUTOSAVE_TIME    3
+#define P021_VALUE_OUTPUT           0 // Switch output, logical state [inactive/active]
+#define P021_VALUE_STATE            1 // Control state, see P021_control_state
+#define P021_VALUE_AUTOSAVE_FLAG    2 // Autosave bookkeeping
+#define P021_VALUE_AUTOSAVE_TIME    3 // Autosave bookkeeping
 
-// Make time interpretation easier by converting millis to longer periods we want to check
+// Make time interpretation easier by converting millis to the longer periods we want to check
 #define millis2seconds(x) ((x) / ((ulong)1000))
 #define millis2minutes(x) ((x) / ((ulong)1000 * 60))
 #define millis2hours(x) ((x) / ((ulong)1000 * 60 * 24))
@@ -371,15 +380,6 @@ boolean Plugin_021(uint8_t function, struct EventStruct *event, String& string)
           }
           success = true;
         }
-        else {
-          String log = F("Invalid config parameter for task: ");
-          log += event->TaskIndex;
-          log += F(" command: ");
-          log += command;
-          log += F(" value: ");
-          log += value;
-          addLogMove(LOG_LEVEL_INFO, log);
-        }
       }
       break;
     }
@@ -590,7 +590,7 @@ void P021_evaluate(struct EventStruct *event)
 
       if (old_control_state != P021_STATE_IDLE)
       {
-        timestamp = millis();
+        timestamp         = millis();
         new_control_state = P021_STATE_IDLE;
       }
       break;
@@ -608,7 +608,7 @@ void P021_evaluate(struct EventStruct *event)
     // Control is switched off with maintnenance interval enabled
     case P021_OPMODE_STANDBY:
 
-      if (old_control_state == P021_STATE_IDLE) // Pump was idling
+      if (old_control_state == P021_STATE_IDLE) // Output was idling
       {
         if ((millis2hours(timePassedSince(timestamp)) >= (ulong)P021_INTERVAL_TIME) && ((ulong)P021_FORCE_TIME >= 1))
         {
@@ -764,6 +764,7 @@ void P021_evaluate(struct EventStruct *event)
   // - The logical state of the output signal [on,OFF]
   // - The state of the internal state machine
   // - Timestamp for last change of the output signal
+  // Note: the actual state of the output is not stored, it can be calculated from the control_state
   UserVar.setFloat(event->TaskIndex, P021_VALUE_OUTPUT, (float)(((P021_control_state)new_control_state == P021_STATE_IDLE) ? 0 : 1));
   UserVar.setFloat(event->TaskIndex, P021_VALUE_STATE,  (float)new_control_state);
   P021_timestamp[event->TaskIndex] = timestamp;
