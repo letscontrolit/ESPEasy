623664d99b15afa62d004510e8158a64008dd7b0
Ton Huisman <tonhu2004@hotmail.com>
2025-08-21 13:23:36 +0200

    [Storage] Show Task Values in EEPROM by using tasks=1 parameter for Eeprom Values page

--- src/src/WebServer/EpromVarPage.cpp
+++ src/src/WebServer/EpromVarPage.cpp
@@ -8,8 +8,10 @@
 #include "../WebServer/HTML_wrappers.h"
 
 #include "../Globals/Settings.h"
+#include "../Globals/ExtraTaskSettings.h"
 
 #include "../Helpers/EEPROMExternal.h"
+#include "../Helpers/ESPEasy_Storage.h"
 
 #include "../Helpers/StringConverter.h"
 
@@ -21,16 +23,57 @@
 
 void handle_eepromvars() {
   if (!isLoggedIn()) { return; }
+
+  const bool showTasks = getFormItemInt(F("tasks"), 0) != 0;
+
   TXBuffer.startStream();
   sendHeadandTail_stdtemplate(_HEAD);
 
-  if ((nullptr != EEPROMExternal) && (Settings.EEPROMExternalI2CAddress() > 0)) {
+  if (checkEEPROMEnabled() > 0) {
     // the table header
     html_table_class_normal();
     html_TR();
     html_table_header(F("External EEPROM"),                                                             300);
     html_table_header(getEEPROMName(static_cast<EEPROMExternal_Type_e>(Settings.EEPROMExternalType())), 400);
+    html_table_header(F(""),                                                                            400);
     html_table_header(F(""));
+
+    if (showTasks) {
+      html_TR();
+      html_table_header(F("Task"),    300);
+      html_table_header(F("Value"),   400);
+      html_table_header(F("Content"), 400);
+      html_table_header(F(""));
+
+      for (taskIndex_t tsk = 0; tsk < TASKS_MAX; ++tsk) {
+        LoadTaskSettings(tsk);
+        html_TR_TD();
+        addHtmlInt(tsk + 1);
+        addHtml(' ');
+        addHtml(getTaskDeviceName(tsk));
+
+        for (taskVarIndex_t var = 0; var < VARS_PER_TASK; ++var) {
+          if (var != 0) {
+            html_TR_TD();
+          }
+          html_TD();
+          addHtmlInt(var + 1);
+          addHtml(' ');
+          addHtml(getTaskValueName(tsk, var));
+          html_TD();
+          const uint32_t addr  = getEEPROMAddressForTaskValue(tsk, var);
+          const float    value = EEPROMExternal->readFloat(addr);
+          const uint32_t data  = EEPROMExternal->readLong(addr);
+
+          if (isnan(value) || (addr == std::numeric_limits<uint32_t>::max())) {
+            addHtml('-');
+          } else {
+            addHtml(strformat(F("%s (0x%04x)"), floatToString(value, ExtraTaskSettings.TaskDeviceValueDecimals[var]), data));
+          }
+        }
+        delay(0);
+      }
+    }
     html_TR();
     html_table_header(F("Slot"),                         300);
     html_table_header(F("Value (only non-zero values)"), 400);
@@ -44,7 +87,7 @@ void handle_eepromvars() {
 
       if (slot % 50 == 0) { delay(0); }
 
-      if (!essentiallyZero(value)) {
+      if (!isnan(value) && !essentiallyZero(value)) {
         ++count;
         html_TR_TD();
         addHtmlInt(slot);
