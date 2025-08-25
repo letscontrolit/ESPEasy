11e96346a6f380464919efa6e5a419c428ea8b58
Ton Huisman <tonhu2004@hotmail.com>
2025-08-25 12:44:10 +0200

    [Storage] Move Settings attributes to next available bits

--- src/src/DataStructs/SettingsStruct.h
+++ src/src/DataStructs/SettingsStruct.h
@@ -604,11 +604,11 @@ public:
     uint32_t DisableSaveConfigAsTar           : 1; // Bit 05
     uint32_t PassiveWiFiScan                  : 1; // Bit 06  // inverted
     uint32_t ShowUnitOfMeasureOnDevicesPage   : 1; // Bit 07  // inverted
-    uint32_t RestoreUserVarsFromEEPROMOnColdBoot : 1; // Bit 08
-    uint32_t RestoreUserVarsFromEEPROMOnWarmBoot : 1; // Bit 09
+    uint32_t unused_08                        : 1; // Bit 08
+    uint32_t unused_09                        : 1; // Bit 09
     uint32_t unused_10                        : 1; // Bit 10
-    uint32_t unused_11                        : 1; // Bit 11
-    uint32_t unused_12                        : 1; // Bit 12
+    uint32_t RestoreUserVarsFromEEPROMOnColdBoot : 1; // Bit 11
+    uint32_t RestoreUserVarsFromEEPROMOnWarmBoot : 1; // Bit 12
     uint32_t unused_13                        : 1; // Bit 13
     uint32_t unused_14                        : 1; // Bit 14
     uint32_t unused_15                        : 1; // Bit 15
