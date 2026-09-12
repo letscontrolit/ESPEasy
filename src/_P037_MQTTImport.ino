ca1e43f52f9c5d1335017d3fd179987d54497033
Ton Huisman <tonhu2004@hotmail.com>
2026-09-12 17:13:27 +0200

    [P037] Only available when String variables are included

--- src/_P037_MQTTImport.ino
+++ src/_P037_MQTTImport.ino
@@ -557,7 +557,9 @@ boolean Plugin_037(uint8_t function, struct EventStruct *event, String& string)
 
                 if (!jsonAttribute.isEmpty()) {
                   key = jsonAttribute;
+                  #  if FEATURE_STRING_VARIABLES
                   key = parseTemplate_padded(key, 21 * 2); // Process variables to allow for longer JSON attribute names
+                  #  endif // if FEATURE_STRING_VARIABLES
 
                   #  if FEATURE_JSON_PARSE
                   Payload = getJsonValue(P037_data->root, key, false);
